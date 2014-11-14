fbd9d399461af8aca86e55e301f7c5de623acb7b
Jim Dowling <jdowling@sics.se>
2014-11-14 09:34:28 +0100

    updated serverspec to version 2

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -1,21 +1,17 @@
 require 'spec_helper'
 
+#describe service('kagent') do  
+#  it { should be_running   }
+#end 
+
+
+describe service('collectd-server') do  
+  it { should be_enabled   }
+  it { should be_running   }
+end 
+
 describe service('ASMain') do  
   it { should be_running   }
 end 
 
-describe service('kagent') do  
-  it { should be_running   }
-end 
 
-describe service('collectd') do  
-  it { should be_running   }
-end 
-
-describe service('collectd-server') do  
-  it { should be_running   }
-end 
-
-describe command("cat /var/lib/kagent/agent.log") do
-  it { should return_stdout /Sending heartbeat.../ }
-end
