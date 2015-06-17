2a78851f39e94930ebddc0f9b36949167ac88d2f
Jim Dowling <jdowling@kth.se>
2015-06-17 07:07:40 +0200

    updated unit test to run as glassfish user

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -34,12 +34,12 @@ describe command("/var/lib/mysql-cluster/ndb/scripts/mgm-client.sh -e \"show\"")
 end
 
 
-describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -ls /\"") do
+describe command("su glassfish -l -c \"/srv/hadoop/bin/hdfs dfs -ls /\"") do
   its (:stdout) { should match /mr-history/ }
 end
 
 
-describe command("su yarn -l -c \"/srv/hadoop/bin/yarn jar /srv/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar pi 1 1000 \"") do
+describe command("su glassfish -l -c \"/srv/hadoop/bin/yarn jar /srv/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar pi 1 1000 \"") do
   its (:stdout) { should match /Estimated value of Pi is/ }
 end
 
