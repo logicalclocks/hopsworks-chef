b0582cbbd893553dedc573494067b7b4f9229a33
Jim Dowling <jdowling@sics.se>
2015-10-08 07:45:01 +0200

    script to start vagrant added

--- run-vagrant.sh
+++ run-vagrant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Removing old vendored cookbooks"
-rm -rf cookbooks > /dev/null/ 2>&1
-rm -f Berksfile.lock nohup.out > /dev/null/ 2>&1
+rm -rf cookbooks > /dev/null 2>&1
+rm -f Berksfile.lock nohup.out > /dev/null 2>&1
 echo "Vendoring cookbooks using 'berks vendor cookbooks'"
 berks vendor cookbooks
 echo "Running the Vagrantfile using 'vagrant up'"
