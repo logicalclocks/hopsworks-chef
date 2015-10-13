1bb9f2d3221463d4501632a5eff5e2cecd4c884c
Jim Dowling <jdowling@sics.se>
2015-10-13 10:40:05 +0200

    glassfish should use jdk 1.7

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,13 @@
 require 'json'
-
+node.override = {
+  'java' => {
+    'install_flavor' => 'oracle',
+    'jdk_version' => 7,
+    'oracle' => {
+      'accept_oracle_download_terms' => true
+    }
+  }
+}
 include_recipe 'glassfish::default'
 
 bash 'fix_java_path_for_glassfish_cookbook' do
