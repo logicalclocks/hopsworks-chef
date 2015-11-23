8b7e261137623440531d03d50827236287f97b5f
Jim Dowling <jdowling@sics.se>
2015-11-23 23:31:08 +0100

    fixed foreign keys problem

--- providers/grants.rb
+++ providers/grants.rb
@@ -8,8 +8,8 @@ notifying_action :create_tables do
   bash 'create_hopsworks_tables' do
     user "root"
     code <<-EOF
-      #{exec} -e \"CREATE DATABASE IF NOT EXISTS hopsworks CHARACTER SET latin1 COLLATE latin1_swedish_ci\"
-      #{exec} -e \"CREATE DATABASE IF NOT EXISTS glassfish_timers CHARACTER SET latin1 COLLATE latin1_swedish_ci\"
+      #{exec} -e \"CREATE DATABASE IF NOT EXISTS hopsworks CHARACTER SET latin1\"
+      #{exec} -e \"CREATE DATABASE IF NOT EXISTS glassfish_timers CHARACTER SET latin1\"
       #{exec} #{db} -e \"source #{new_resource.tables_path}\"
     EOF
     not_if "#{exec} -e 'show databases' | grep hopsworks"
