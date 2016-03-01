2568e9412bef100995e67cd3037a2af77606ce53
%github_username% <jdowling@sics.se>
2016-03-01 18:02:41 +0100

    release script

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cb=$(grep "^name\s*" metadata.rb | perl -p -e 's/"//g' |  perl -p -e "s/name\s*//g")
+
+echo "Releasing cookbook: $cb to Chef supermarket"
+if [ $cb == "" ] ; then
+ echo "Couldnt determine cookbook name. Exiting..."
+fi
+
+rm -rf /tmp/cookbooks
+berks vendor /tmp/cookbooks
+cp metadata.rb /tmp/cookbooks/$cb/
+knife cookbook site share $cb Applications
+
