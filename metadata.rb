1ac9acbf43f2ca6e1d8182630b93ed2ad14599fe
Jim Dowling <jdowling@sics.se>
2016-03-02 09:42:03 +0100

    removed hamid's graphs, as they have invalid utf8 sequences preventing release to chef supermarket

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "hopsworks"
 maintainer       "Jim Dowling"
 maintainer_email "jdowling@kth.se"
 license          "Apache v2.0"
-description      "Installs/Configures the HopsHub Dashboard"
+description      "Installs/Configures HopsWorks, the UI for Hops Hadoop."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
--- recipes/letsencrypt.rb
+++ recipes/letsencrypt.rb
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 package 'openssl'
 
 myHost=node.fqdn
