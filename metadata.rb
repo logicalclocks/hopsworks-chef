c80164d5e59eb48c6b9d42fa8996abc68a05107c
misdess <misdess@gmail.com>
2016-04-26 10:36:56 +0200

    adding default numbers of replication factor and partition for kafka topic

--- metadata.rb
+++ metadata.rb
@@ -200,4 +200,12 @@ attribute "hadoop_spark/dir",
           :description => "Installation directory.",
           :type => 'string'
 
+attribute "hopsworks.kafka_num_replicas",
+          :description => "Default number of replicas for Kafka Topics.",
+          :type => 'string'
+
+attribute "hopsworks.kafka_num_partitions",
+          :description => "Default number of partitions for Kafka Topics.",
+          :type => 'string'
+
 
