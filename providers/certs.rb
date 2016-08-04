28bb11b10c63955d7e9b6659d76251676631531a
Jim Dowling <jdowling@sics.se>
2016-08-05 00:35:40 +0200

    cleanup cert generation

--- providers/certs.rb
+++ providers/certs.rb
@@ -16,15 +16,13 @@ bash 'certificateauthority' do
 	echo 1000 > serial
 
 	#2 Create the root key
-	openssl genrsa -aes256 -out ${BASEDIR}/private/ca.key.pem -passout pass:$KEYSTOREPW 4096 2>&1 > ${BASEDIR}/certificateuauthority.log
+	openssl genrsa -aes256 -out ${BASEDIR}/private/ca.key.pem -passout pass:${KEYSTOREPW} 4096 2>&1 > ${BASEDIR}/certificateuauthority.log
 
 	chmod 400 private/ca.key.pem
 
 	#3 Create the root certificate
 	openssl req -subj "/C=SE/ST=Sweden/L=Stockholm/O=SICS/CN=HopsRootCA" \
-	  -passin pass:$KEYSTOREPW -passout pass:$KEYSTOREPW \
-      -key ${BASEDIR}/private/ca.key.pem \
-      -new -x509 -days 7300 -sha256 -extensions v3_ca \
+	  -passin pass:${KEYSTOREPW} -passout pass:${KEYSTOREPW} -key ${BASEDIR}/private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca \
       -out ${BASEDIR}/certs/ca.cert.pem 2>&1 >> ${BASEDIR}/certificateuauthority.log
 
 	chmod 444 certs/ca.cert.pem
@@ -36,33 +34,27 @@ bash 'certificateauthority' do
 	echo 1000 > intermediate/crlnumber
 
 	#5 Create the intermediate key
-	openssl genrsa -aes256 -out ${BASEDIR}/intermediate/private/intermediate.key.pem -passout pass:$KEYSTOREPW 4096 2>&1 >> ${BASEDIR}/certificateuauthority.log
+	openssl genrsa -aes256 -out ${BASEDIR}/intermediate/private/intermediate.key.pem -passout pass:${KEYSTOREPW} 4096 2>&1 >> ${BASEDIR}/certificateuauthority.log
 
 	chown -R #{node.glassfish.user}:#{node.glassfish.group} intermediate/private/intermediate.key.pem
 	chmod 440 intermediate/private/intermediate.key.pem
 
 	#6 Create the intermediate certificate 
-	openssl req -new -sha256 \
-	  -subj "/C=SE/ST=Sweden/L=Stockholm/O=SICS/CN=HopsIntermedtiateCA" \
-      -key ${BASEDIR}/intermediate/private/intermediate.key.pem \
-      -passin pass:$KEYSTOREPW -passout pass:$KEYSTOREPW \
+	openssl req -new -sha256 -subj "/C=SE/ST=Sweden/L=Stockholm/O=SICS/CN=HopsIntermedtiateCA" \
+      -key ${BASEDIR}/intermediate/private/intermediate.key.pem -passin pass:${KEYSTOREPW} -passout pass:${KEYSTOREPW} \
       -out ${BASEDIR}/intermediate/csr/intermediate.csr.pem 2>&1 >> ${BASEDIR}/certificateuauthority.log
 
-	openssl ca -policy policy_loose -batch -config openssl.cnf -extensions v3_intermediate_ca \
-      -days 3650 -notext -md sha256 \
-      -passin pass:$KEYSTOREPW \
-      -in ${BASEDIR}/intermediate/csr/intermediate.csr.pem \
+	openssl ca -policy policy_loose -batch -config ${BASEDIR}/intermediate/openssl.cnf -extensions v3_intermediate_ca \
+      -days 3650 -notext -md sha256 -passin pass:${KEYSTOREPW} -in ${BASEDIR}/intermediate/csr/intermediate.csr.pem \
       -out ${BASEDIR}/intermediate/certs/intermediate.cert.pem 2>&1 >> ${BASEDIR}/certificateuauthority.log
 
 	chmod 444 intermediate/certs/intermediate.cert.pem
 
 	#7 Verify the intermediate certificate
-	openssl verify -CAfile ${BASEDIR}/certs/ca.cert.pem \
-    	  ${BASEDIR}/intermediate/certs/intermediate.cert.pem 2>&1 >> ${BASEDIR}/certificateuauthority.log
+	openssl verify -CAfile ${BASEDIR}/certs/ca.cert.pem ${BASEDIR}/intermediate/certs/intermediate.cert.pem 2>&1 >> ${BASEDIR}/certificateuauthority.log
 
 	#8 Create the certificate chain file
-	cat intermediate/certs/intermediate.cert.pem \
-    	  certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
+	cat intermediate/certs/intermediate.cert.pem certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
 
 	chmod 444 intermediate/certs/ca-chain.cert.pem
     EOF
