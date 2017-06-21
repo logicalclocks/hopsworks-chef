1cd5a7f6da5c524e88d954d36df2d95797e1f23a
Jim Dowling <jdowling@sics.se>
2017-06-21 18:53:25 +0200

    change autologin script

--- recipes/image.rb
+++ recipes/image.rb
@@ -4,27 +4,28 @@
 #
 
 package 'ubuntu-desktop'
+package "mingetty"
 
 template "/home/#{node['glassfish']['user']}/.config/autostart/google-chrome.desktop" do
     source "virtualbox/google-chrome.desktop.erb"
     owner node["glassfish"]["user"]
     mode 0774
     action :create
-end 
+end
 
 
   
-template "/etc/init/tty7.conf" do
-    source "virtualbox/tty7.conf.erb"
-    owner node["glassfish"]["user"]
-    mode 0774
+template "/etc/init/tty1.conf" do
+    source "virtualbox/tty.conf.erb"
+    owner "root"
+    mode 0644
     action :create
 end 
   
 template "/etc/lightdm/lightdm.conf" do
     source "virtualbox/lightdm.conf.erb"
-    owner node["glassfish"]["user"]
-    mode 0774
+    owner "root"
+    mode 0644
     action :create
 end 
 
