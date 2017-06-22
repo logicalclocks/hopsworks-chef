959a0033925883846095f01843ea20c5a50b7e08
Jim Dowling <jdowling@sics.se>
2017-06-22 15:16:49 +0200

    fix image.rb

--- recipes/image.rb
+++ recipes/image.rb
@@ -6,6 +6,14 @@
 package 'ubuntu-desktop'
 package "mingetty"
 
+bash 'mkdir_autostart' do
+  user node["glassfish"]["user"]
+  code <<-EOF
+       mkdir -p /home/#{node["glassfish"]["user"]}/.config/autostart
+    EOF
+end
+
+  
 template "/home/#{node['glassfish']['user']}/.config/autostart/google-chrome.desktop" do
     source "virtualbox/google-chrome.desktop.erb"
     owner node["glassfish"]["user"]
