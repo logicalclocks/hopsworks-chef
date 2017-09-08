fd92a3f2298975349dfb2d82b3cface47d3769d1
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-09-08 09:03:00 +0200

    dela v1
    added new hopsworks/dela attributes
    insert dela variables rows
    adding dela table changes
    sign req to hops-site
    default hops-site changed
    fix isEmpty not found
    fix missing .erb on csr-ca.py
    fixes cert signing
    install python-openssl
    fix missing end
    remove unused var
    added max retries
    added retries to config, and use hopsworks email instead of hopssite user
    create variables table inserts for dela_enabled and hops_site_host
    tag
    added global to csr-ca.py.erb
    added manual register flag for cert signing and dela enabled for dela
    fix certs for hopssite

--- recipes/default.rb
+++ recipes/default.rb
@@ -309,6 +309,8 @@ template "#{rows_path}" do
 		:file_preview_txt_size => node["hopsworks"]["file_preview_txt_size"],
                 :zk_ip => zk_ip,
                 :dela_ip => dela_ip,
+                :dela_cluster_http_port => node["hopsworks"]["dela"]["cluster_http_port"],
+                :dela_hopsworks_public_port => node["hopsworks"]["dela"]["public_hopsworks_port"],
                 :java_home => node["java"]["java_home"],
                 :dela_port => node["dela"]["http_port"],
                 :kafka_ip => kafka_ip,                
@@ -335,7 +337,8 @@ template "#{rows_path}" do
                 :org_city => node["hopsworks"]["org_city"],                
                 :vagrant_enabled => vagrant_enabled,
                 :public_ip => public_ip,
-                :monitor_max_status_poll_try => node["hopsworks"]["monitor_max_status_poll_try"]
+                :monitor_max_status_poll_try => node["hopsworks"]["monitor_max_status_poll_try"],
+                :dela_enabled => node["hopsworks"]["dela"]["enabled"]
               })
    notifies :insert_rows, 'hopsworks_grants[hopsworks_tables]', :immediately
 end
--- recipes/install.rb
+++ recipes/install.rb
@@ -23,7 +23,7 @@ bash "systemd_reload_for_glassfish_failures" do
 end
 
 
-if node["hopsworks"]["systemd"] === "true" 
+if node["hopsworks"]["systemd"] === "true"
   systemd = true
 else
   systemd = false
@@ -113,7 +113,7 @@ end
 
 case node["platform_family"]
 when "debian"
-  
+
   if node.platform_version.to_f <= 14.04
     node.override["hopsworks"]["systemd"] = "false"
   end
@@ -122,7 +122,7 @@ when "debian"
 
 when "rhel"
   package "krb5-libs"
-  
+
   remote_file "#{Chef::Config[:file_cache_path]}/dtrx.tar.gz" do
     user node["glassfish"]["user"]
     group node["glassfish"]["group"]
@@ -130,7 +130,7 @@ when "rhel"
     mode 0755
     action :create
   end
-  
+
   bash "unpack_dtrx" do
     user "root"
     code <<-EOF
@@ -168,7 +168,7 @@ node.override = {
           'min_memory' => node["glassfish"]["min_mem"],
           'max_memory' => node["glassfish"]["max_mem"],
           'max_perm_size' => node["glassfish"]["max_perm_size"],
-          'max_stack_size' => node["glassfish"]["max_stack_size"], 
+          'max_stack_size' => node["glassfish"]["max_stack_size"],
           'port' => web_port,
           'admin_port' => admin_port,
           'username' => username,
@@ -220,7 +220,7 @@ node.override = {
             'corepoolsize' => 12,
             'description' => 'Hopsworks Executor Service'
           }
-        },        
+        },
         'jdbc_connection_pools' => {
           'hopsworksPool' => {
             'config' => {
@@ -310,7 +310,7 @@ template "#{theDomain}/docroot/404.html" do
               :org_name => node["hopsworks"]["org_name"]
             })
   action :create
-end 
+end
 
 cookbook_file"#{theDomain}/docroot/obama-smoked-us.gif" do
   source 'obama-smoked-us.gif'
@@ -335,7 +335,7 @@ when "rhel"
   # Needed by sparkmagic
   package "krb5-libs"
   package "krb5-devel"
-  
+
   service_name = "glassfish-#{domain_name}"
   file "/etc/systemd/system/#{service_name}.service" do
     owner "root"
@@ -370,14 +370,14 @@ if systemd == true
     owner "root"
     mode 0774
     action :create
-  end 
+  end
 
   hopsworks_grants "reload_systemd" do
     tables_path  ""
     views_path ""
     rows_path  ""
     action :reload_systemd
-  end 
+  end
 
 end
 
@@ -392,7 +392,7 @@ end
 
 dirs = %w{certs crl newcerts private intermediate}
 
-for d in dirs 
+for d in dirs
   directory "#{ca_dir}/#{d}" do
     owner node["glassfish"]["user"]
     group node["glassfish"]["group"]
@@ -403,7 +403,7 @@ end
 
 int_dirs = %w{certs crl csr newcerts private}
 
-for d in int_dirs 
+for d in int_dirs
   directory "#{ca_dir}/intermediate/#{d}" do
     owner node["glassfish"]["user"]
     group node["glassfish"]["group"]
@@ -420,7 +420,7 @@ template "#{ca_dir}/openssl-ca.cnf" do
               :ca_dir =>  "#{ca_dir}"
             })
   action :create
-end 
+end
 
 template "#{ca_dir}/intermediate/openssl-intermediate.cnf" do
   source "intermediateopenssl.cnf.erb"
@@ -430,7 +430,7 @@ template "#{ca_dir}/intermediate/openssl-intermediate.cnf" do
               :int_ca_dir =>  "#{ca_dir}/intermediate"
             })
   action :create
-end 
+end
 
 template "#{ca_dir}/intermediate/createusercerts.sh" do
   source "createusercerts.sh.erb"
@@ -543,10 +543,10 @@ template "/etc/sudoers.d/glassfish" do
               :delete_projectcert =>  "#{ca_dir}/intermediate/deleteprojectcerts.sh",
               :ndb_backup =>  "#{theDomain}/bin/ndb_backup.sh",
               :jupyter =>  "#{theDomain}/bin/jupyter.sh",
-              :jupyter_cleanup =>  "#{theDomain}/bin/jupyter-project-cleanup.sh"                                
+              :jupyter_cleanup =>  "#{theDomain}/bin/jupyter-project-cleanup.sh"
             })
   action :create
-end  
+end
 
 # Replace sysv with our version. It increases the max number of open files limit (ulimit -n)
 case node.platform
@@ -566,7 +566,7 @@ when "ubuntu"
                 :password_file => password_file
               })
 
-  end 
+  end
 
 end
 
@@ -575,8 +575,6 @@ end
 #
 # Jupyter Configuration
 #
-
-
 
 # group node["kagent"]["certs_group"] do
 #   action :modify
@@ -589,6 +587,48 @@ end
 # Hopsworks will use a sudoer script to launch jupyter as the 'jupyter' user.
 # The jupyter user will be able to read the files and write to the directories due to group permissions
 
+user node["jupyter"]["user"] do
+  home node["jupyter"]["base_dir"]
+  gid node["jupyter"]["group"]  
+  action :create
+  shell "/bin/bash"
+  manage_home true
+  not_if "getent passwd #{node["jupyter"]["user"]}"
+end
+
+#update permissions of base_dir to 770
+directory node["jupyter"]["base_dir"]  do
+  owner node["jupyter"]["user"]  
+  group node["jupyter"]["group"]
+  mode "770"
+  action :create
+end
+
+case node["platform_family"]
+  when "debian"
+   apt_package "python-openssl" do
+     action :install
+   end
+
+  when "rhel"
+   python_package "pyOpenSSL" do
+     action :install
+   end
+end
+
+
+directory node["hopssite"]["certs_dir"] do
+  owner node["glassfish"]["user"]
+  mode "750"
+  action :create
+end
+
+directory node["hopssite"]["keystore_dir"] do
+  owner node["glassfish"]["user"]
+  mode "750"
+  action :create
+end
+
 template "#{theDomain}/config/ca.ini" do
   source "ca.ini.erb"
   owner node["glassfish"]["user"]
@@ -596,21 +636,26 @@ template "#{theDomain}/config/ca.ini" do
   action :create
 end
 
-# template "#{theDomain}/bin/csr-ca.py" do
-#   source "csr-ca.py"
-#   owner node["glassfish"]["user"]
-#   mode 0750
-#   action :create
-# end
-# template "#{theDomain}/bin/ca-keystore.sh" do
-#   source "ca-keystore.sh.erb"
-#   owner node["glassfish"]["user"]
-#   mode 0750
-#   action :create
-# end
+template "#{theDomain}/bin/csr-ca.py" do
+  source "csr-ca.py.erb"
+  owner node["glassfish"]["user"]
+  mode 0750
+  action :create
+end
 
-# if node['hopssite']['user'].isEmpty? == false
-#   hopsworks_certs "sign-ca-with-root-hopssite-ca" do
-#     action :sign_hopssite
-#   end
-# end
+template "#{theDomain}/bin/ca-keystore.sh" do
+  source "ca-keystore.sh.erb"
+  owner node["glassfish"]["user"]
+  mode 0750
+  action :create
+  variables({
+         :directory => node["hopssite"]["keystore_dir"],
+         :keystorepass => node["hopsworks"]["master"]["password"]
+  })
+end
+
+if node['hopssite']['manual_register'].empty? || node['hopssite']['manual_register'] == "false"
+  hopsworks_certs "sign-ca-with-root-hopssite-ca" do
+    action :sign_hopssite
+  end
+end
