428843e0d781f5f5e5f72ff63dd4156e272e2315
Jim Dowling <jdowling@sics.se>
2017-09-12 10:23:24 +0200

    hotfix

--- recipes/default.rb
+++ recipes/default.rb
@@ -902,7 +902,7 @@ EOF
 end
 
 
-if node['hopsworks']['pixiedust'].eql("true") 
+if node['hopsworks']['pixiedust'].eql?("true") 
   cloudant="cloudant-spark-v2.0.0-185.jar"
   # Pixiedust is a visualization library for Jupyter
   pixiedust_home="#{node['jupyter']['base_dir']}/pixiedust"
