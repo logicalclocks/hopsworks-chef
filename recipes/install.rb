7cf9ee38d3704c4fecb61de6bafd071b2026e2ad
Fabio Buso <dev.siroibaf@gmail.com>
2017-09-19 12:58:47 +0200

    [HOPSWORKS-194] Change chef cookbooks from dot notation to bracket notation

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,15 +2,15 @@ require 'json'
 require 'base64'
 
 private_ip=my_private_ip()
-username=node["hopsworks"]["admin"]["user"]
-password=node["hopsworks"]["admin"]["password"]
+username=node['hopsworks']['admin']['user']
+password=node['hopsworks']['admin']['password']
 domain_name="domain1"
-domains_dir = node["hopsworks"]["domains_dir"]
+domains_dir = node['hopsworks']['domains_dir']
 theDomain="#{domains_dir}/#{domain_name}"
-admin_port = node["glassfish"]["admin"]["port"]
-web_port = node["glassfish"]["port"]
-mysql_user=node["mysql"]["user"]
-mysql_password=node["mysql"]["password"]
+admin_port = node['glassfish']['admin']['port']
+web_port = node['glassfish']['port']
+mysql_user=node['mysql']['user']
+mysql_password=node['mysql']['password']
 mysql_host = my_private_ip()
 password_file = "#{theDomain}_admin_passwd"
 
@@ -23,18 +23,18 @@ bash "systemd_reload_for_glassfish_failures" do
 end
 
 
-if node["hopsworks"]["systemd"] === "true" 
+if node['hopsworks']['systemd'] === "true" 
   systemd = true
 else
   systemd = false
 end
 
-group node["hopsworks"]["group"] do
+group node['hopsworks']['group'] do
   action :create
   not_if "getent group #{node['hopsworks']['group']}"
 end
 
-group node["jupyter"]["group"] do
+group node['jupyter']['group'] do
   action :create
   not_if "getent group #{node['jupyter']['group']}"
 end
@@ -42,67 +42,67 @@ end
 #
 # hdfs superuser group is 'hdfs'
 #
-group node.hops.hdfs.user do
+group node['hops']['hdfs']['user'] do
   action :create
-  not_if "getent group #{node.hops.hdfs.user}"
+  not_if "getent group #{node['hops']['hdfs']['user']}"
 end
 
-user node["hopsworks"]["user"] do
+user node['hopsworks']['user'] do
   home "/home/#{node['hopsworks']['user']}"
-  gid node["hopsworks"]["group"]
+  gid node['hopsworks']['group']
   action :create
   shell "/bin/bash"
   manage_home true
   not_if "getent passwd #{node['hopsworks']['user']}"
 end
 
-group node["jupyter"]["group"] do
+group node['jupyter']['group'] do
   action :modify
   members ["#{node['hopsworks']['user']}"]
   append true
 end
 
 # Add to the hdfs superuser group
-group node["hops"]["hdfs"]["user"] do
+group node['hops']['hdfs']['user'] do
   action :modify
   members ["#{node['hopsworks']['user']}"]
   append true
 end
 
-user node["jupyter"]["user"] do
-  home node["jupyter"]["base_dir"]
-  gid node["jupyter"]["group"]  
+user node['jupyter']['user'] do
+  home node['jupyter']['base_dir']
+  gid node['jupyter']['group']  
   action :create
   shell "/bin/bash"
   manage_home true
-  not_if "getent passwd #{node["jupyter"]["user"]}"
+  not_if "getent passwd #{node['jupyter']['user']}"
 end
 
-group node["hops"]["group"] do
+group node['hops']['group'] do
   action :modify
   members ["#{node['hopsworks']['user']}", "#{node['jupyter']['user']}"]
   append true
 end
 
 #update permissions of base_dir to 770
-directory node["jupyter"]["base_dir"]  do
-  owner node["jupyter"]["user"]  
-  group node["jupyter"]["group"]
+directory node['jupyter']['base_dir']  do
+  owner node['jupyter']['user']  
+  group node['jupyter']['group']
   mode "770"
   action :create
 end
 
-directory node["hopsworks"]["dir"]  do
-  owner node["hopsworks"]["user"]
-  group node["hopsworks"]["group"]
+directory node['hopsworks']['dir']  do
+  owner node['hopsworks']['user']
+  group node['hopsworks']['group']
   mode "755"
   action :create
   not_if "test -d #{node['hopsworks']['dir']}"
 end
 
 directory domains_dir  do
-  owner node["hopsworks"]["user"]
-  group node["hopsworks"]["group"]
+  owner node['hopsworks']['user']
+  group node['hopsworks']['group']
   mode "750"
   action :create
   not_if "test -d #{domains_dir}"
@@ -111,11 +111,11 @@ end
 
 # For unzipping files
 
-case node["platform_family"]
+case node['platform_family']
 when "debian"
   
-  if node.platform_version.to_f <= 14.04
-    node.override["hopsworks"]["systemd"] = "false"
+  if node['platform_version'].to_f <= 14.04
+    node.override['hopsworks']['systemd'] = "false"
   end
   package "dtrx"
   package "libkrb5-dev"
@@ -123,9 +123,9 @@ when "debian"
 when "rhel"
   package "krb5-libs"
   
-  remote_file "#{Chef::Config[:file_cache_path]}/dtrx.tar.gz" do
-    user node["glassfish"]["user"]
-    group node["glassfish"]["group"]
+  remote_file "#{Chef::Config['file_cache_path']}/dtrx.tar.gz" do
+    user node['glassfish']['user']
+    group node['glassfish']['group']
     source "http://brettcsmith.org/2007/dtrx/dtrx-7.1.tar.gz"
     mode 0755
     action :create
@@ -135,7 +135,7 @@ when "rhel"
     user "root"
     code <<-EOF
     set -e
-    cd #{Chef::Config[:file_cache_path]}
+    cd #{Chef::Config['file_cache_path']}
     tar -xzf dtrx.tar.gz
     cd dtrx-7.1
     python setup.py install --prefix=/usr/local
@@ -152,36 +152,36 @@ end
 
 node.override = {
   'java' => {
-    'install_flavor' => node["java"]["install_flavor"],
-    'jdk_version' => node["java"]["jdk_version"],
+    'install_flavor' => node['java']['install_flavor'],
+    'jdk_version' => node['java']['jdk_version'],
     'oracle' => {
       'accept_oracle_download_terms' => true
     }
   },
   'glassfish' => {
-    'version' => node["glassfish"]["version"],
+    'version' => node['glassfish']['version'],
     'domains' => {
       domain_name => {
         'config' => {
           'systemd_enabled' => systemd,
           'systemd_start_timeout' => 240,
-          'min_memory' => node["glassfish"]["min_mem"],
-          'max_memory' => node["glassfish"]["max_mem"],
-          'max_perm_size' => node["glassfish"]["max_perm_size"],
-          'max_stack_size' => node["glassfish"]["max_stack_size"], 
+          'min_memory' => node['glassfish']['min_mem'],
+          'max_memory' => node['glassfish']['max_mem'],
+          'max_perm_size' => node['glassfish']['max_perm_size'],
+          'max_stack_size' => node['glassfish']['max_stack_size'], 
           'port' => web_port,
           'admin_port' => admin_port,
           'username' => username,
           'password' => password,
-          'master_password' => node["hopsworks"]["master"]["password"],
+          'master_password' => node['hopsworks']['master']['password'],
           'remote_access' => false,
           'secure' => false,
-          'jvm_options' => ["-DHADOOP_HOME=#{node["hops"]["dir"]}/hadoop", "-DHADOOP_CONF_DIR=#{node["hops"]["dir"]}/hadoop/etc/hadoop", '-Dcom.sun.enterprise.tools.admingui.NO_NETWORK=true', '-Dlog4j.configuration=file:///${com.sun.aas.instanceRoot}/config/log4j.properties']
+          'jvm_options' => ["-DHADOOP_HOME=#{node['hops']['dir']}/hadoop", "-DHADOOP_CONF_DIR=#{node['hops']['dir']}/hadoop/etc/hadoop", '-Dcom.sun.enterprise.tools.admingui.NO_NETWORK=true', '-Dlog4j.configuration=file:///${com.sun.aas.instanceRoot}/config/log4j.properties']
         },
         'extra_libraries' => {
           'jdbcdriver' => {
             'type' => 'common',
-            'url' => node["hopsworks"]["mysql_connector_url"]
+            'url' => node['hopsworks']['mysql_connector_url']
           }
         },
         'threadpools' => {
@@ -271,7 +271,7 @@ node.override = {
 
 
 
-installed = "#{node["glassfish"]["base_dir"]}/.installed"
+installed = "#{node['glassfish']['base_dir']}/.installed"
 if ::File.exists?( "#{installed}" ) == false
 
   package 'openssl'
@@ -280,15 +280,15 @@ if ::File.exists?( "#{installed}" ) == false
   include_recipe 'glassfish::attribute_driven_domain'
 
   file "#{installed}" do # Mark that glassfish is installed
-    owner node["glassfish"]["user"]
+    owner node['glassfish']['user']
   end
 
-  cauth = File.basename(node["hopsworks"]["cauth_url"])
+  cauth = File.basename(node['hopsworks']['cauth_url'])
 
   remote_file "#{theDomain}/lib/#{cauth}"  do
-    user node["glassfish"]["user"]
-    group node["glassfish"]["group"]
-    source node["hopsworks"]["cauth_url"]
+    user node['glassfish']['user']
+    group node['glassfish']['group']
+    source node['hopsworks']['cauth_url']
     mode 0755
     action :create_if_missing
   end
@@ -297,39 +297,39 @@ end
 
 
 # If the install.rb recipe failed and is re-run, install_dir needs to reset it
-if node["glassfish"]["install_dir"].include?("versions") == false
-  node.override["glassfish"]["install_dir"] = "#{node["glassfish"]["install_dir"]}/glassfish/versions/current"
+if node['glassfish']['install_dir'].include?("versions") == false
+  node.override['glassfish']['install_dir'] = "#{node['glassfish']['install_dir']}/glassfish/versions/current"
 end
 
 
 template "#{theDomain}/docroot/404.html" do
   source "404.html.erb"
-  owner node["glassfish"]["user"]
+  owner node['glassfish']['user']
   mode 0777
   variables({
-              :org_name => node["hopsworks"]["org_name"]
+              :org_name => node['hopsworks']['org_name']
             })
   action :create
 end 
 
 cookbook_file"#{theDomain}/docroot/obama-smoked-us.gif" do
   source 'obama-smoked-us.gif'
-  owner node["glassfish"]["user"]
-  group node["glassfish"]["group"]
+  owner node['glassfish']['user']
+  group node['glassfish']['group']
   mode '0755'
   action :create
 end
 
 
-# if node["glassfish"]["port"] == 80
+# if node['glassfish']['port'] == 80
 #   authbind_port "AuthBind GlassFish Port 80" do
 #     port 80
-#     user node["glassfish"]["user"]
+#     user node['glassfish']['user']
 #   end
 # end
 
 
-case node.platform
+case node['platform']
 when "rhel"
 
   # Needed by sparkmagic
@@ -351,7 +351,7 @@ when "rhel"
       :restart_domain_command => "#{asadmin} restart-domain #{password_file} #{domain_name}",
       :stop_domain_command => "#{asadmin} stop-domain #{password_file} #{domain_name}",
       :authbind => requires_authbind,
-      :listen_ports => [admin_port, node["glassfish"]["port"]])
+      :listen_ports => [admin_port, node['glassfish']['port']])
   end
 
 end
@@ -381,11 +381,11 @@ if systemd == true
 
 end
 
-ca_dir = node["certs"]["dir"]
+ca_dir = node['certs']['dir']
 
 directory ca_dir do
-  owner node["glassfish"]["user"]
-  group node["glassfish"]["group"]
+  owner node['glassfish']['user']
+  group node['glassfish']['group']
   mode "700"
   action :create
 end
@@ -394,8 +394,8 @@ dirs = %w{certs crl newcerts private intermediate}
 
 for d in dirs 
   directory "#{ca_dir}/#{d}" do
-    owner node["glassfish"]["user"]
-    group node["glassfish"]["group"]
+    owner node['glassfish']['user']
+    group node['glassfish']['group']
     mode "700"
     action :create
   end
@@ -405,8 +405,8 @@ int_dirs = %w{certs crl csr newcerts private}
 
 for d in int_dirs 
   directory "#{ca_dir}/intermediate/#{d}" do
-    owner node["glassfish"]["user"]
-    group node["glassfish"]["group"]
+    owner node['glassfish']['user']
+    group node['glassfish']['group']
     mode "700"
     action :create
   end
@@ -414,7 +414,7 @@ end
 
 template "#{ca_dir}/openssl-ca.cnf" do
   source "caopenssl.cnf.erb"
-  owner node["glassfish"]["user"]
+  owner node['glassfish']['user']
   mode "600"
   variables({
               :ca_dir =>  "#{ca_dir}"
@@ -424,7 +424,7 @@ end
 
 template "#{ca_dir}/intermediate/openssl-intermediate.cnf" do
   source "intermediateopenssl.cnf.erb"
-  owner node["glassfish"]["user"]
+  owner node['glassfish']['user']
   mode "600"
   variables({
               :int_ca_dir =>  "#{ca_dir}/intermediate"
@@ -435,7 +435,7 @@ end
 template "#{ca_dir}/intermediate/createusercerts.sh" do
   source "createusercerts.sh.erb"
   owner "root"
-  group node["glassfish"]["group"]
+  group node['glassfish']['group']
   mode "510"
   variables({
               :int_ca_dir =>  "#{ca_dir}/intermediate/"
@@ -446,7 +446,7 @@ end
 template "#{ca_dir}/intermediate/deleteusercerts.sh" do
   source "deleteusercerts.sh.erb"
   owner "root"
-  group node["glassfish"]["group"]
+  group node['glassfish']['group']
   mode "510"
   variables({
               :int_ca_dir =>  "#{ca_dir}/intermediate/"
@@ -457,7 +457,7 @@ end
 template "#{ca_dir}/intermediate/deleteprojectcerts.sh" do
   source "deleteprojectcerts.sh.erb"
   owner "root"
-  group node["glassfish"]["group"]
+  group node['glassfish']['group']
   mode "510"
   variables({
               :int_ca_dir =>  "#{ca_dir}/intermediate/"
@@ -467,64 +467,64 @@ end
 
 template "#{theDomain}/bin/ndb_backup.sh" do
   source "ndb_backup.sh.erb"
-  owner node["glassfish"]["user"]
-  group node["glassfish"]["group"]
+  owner node['glassfish']['user']
+  group node['glassfish']['group']
   mode "754"
   action :create
 end
 
 template "#{theDomain}/bin/jupyter.sh" do
   source "jupyter.sh.erb"
-  owner node["glassfish"]["user"]
-  group node["glassfish"]["group"]
+  owner node['glassfish']['user']
+  group node['glassfish']['group']
   mode "550"
   action :create
 end
 
 template "#{theDomain}/bin/jupyter-project-cleanup.sh" do
   source "jupyter-project-cleanup.sh.erb"
-  owner node["glassfish"]["user"]
-  group node["glassfish"]["group"]
+  owner node['glassfish']['user']
+  group node['glassfish']['group']
   mode "550"
   action :create
 end
 
 template "#{theDomain}/bin/jupyter-kill.sh" do
   source "jupyter-kill.sh.erb"
-  owner node["glassfish"]["user"]
-  group node["jupyter"]["group"]
+  owner node['glassfish']['user']
+  group node['jupyter']['group']
   mode "550"
   action :create
 end
 
 template "#{theDomain}/bin/jupyter-stop.sh" do
   source "jupyter-stop.sh.erb"
-  owner node["glassfish"]["user"]
-  group node["jupyter"]["group"]
+  owner node['glassfish']['user']
+  group node['jupyter']['group']
   mode "550"
   action :create
 end
 
 template "#{theDomain}/bin/jupyter-launch.sh" do
   source "jupyter-launch.sh.erb"
-  owner node["glassfish"]["user"]
-  group node["jupyter"]["group"]
+  owner node['glassfish']['user']
+  group node['jupyter']['group']
   mode "550"
   action :create
 end
 
 template "#{theDomain}/bin/unzip-hdfs-files.sh" do
   source "unzip-hdfs-files.sh.erb"
-  owner node["glassfish"]["user"]
-  group node["glassfish"]["group"]
+  owner node['glassfish']['user']
+  group node['glassfish']['group']
   mode "550"
   action :create
 end
 
 template "#{theDomain}/bin/unzip-background.sh" do
   source "unzip-background.sh.erb"
-  owner node["glassfish"]["user"]
-  group node["glassfish"]["group"]
+  owner node['glassfish']['user']
+  group node['glassfish']['group']
   mode "550"
   action :create
 end
@@ -537,7 +537,7 @@ template "/etc/sudoers.d/glassfish" do
   group "root"
   mode "0440"
   variables({
-              :user => node["glassfish"]["user"],
+              :user => node['glassfish']['user'],
               :int_sh_dir =>  "#{ca_dir}/intermediate/createusercerts.sh",
               :delete_usercert =>  "#{ca_dir}/intermediate/deleteusercerts.sh",
               :delete_projectcert =>  "#{ca_dir}/intermediate/deleteprojectcerts.sh",
@@ -549,7 +549,7 @@ template "/etc/sudoers.d/glassfish" do
 end  
 
 # Replace sysv with our version. It increases the max number of open files limit (ulimit -n)
-case node.platform
+case node['platform']
 when "ubuntu"
   file "/etc/init.d/glassfish-#{domain_name}" do
     owner "root"
@@ -578,9 +578,9 @@ end
 
 
 
-# group node["kagent"]["certs_group"] do
+# group node['kagent']['certs_group'] do
 #   action :modify
-#   members ["#{node["jupyter"]["user"]}"]
+#   members ["#{node['jupyter']['user']}"]
 #   append true
 # end
 
@@ -591,20 +591,20 @@ end
 
 template "#{theDomain}/config/ca.ini" do
   source "ca.ini.erb"
-  owner node["glassfish"]["user"]
+  owner node['glassfish']['user']
   mode 0750
   action :create
 end
 
 # template "#{theDomain}/bin/csr-ca.py" do
 #   source "csr-ca.py"
-#   owner node["glassfish"]["user"]
+#   owner node['glassfish']['user']
 #   mode 0750
 #   action :create
 # end
 # template "#{theDomain}/bin/ca-keystore.sh" do
 #   source "ca-keystore.sh.erb"
-#   owner node["glassfish"]["user"]
+#   owner node['glassfish']['user']
 #   mode 0750
 #   action :create
 # end
--- recipes/master.rb
+++ recipes/master.rb
@@ -1,6 +1,6 @@
 #rows_path = "#{domains_dir}/post.sql"
 
-case node.platform
+case node['platform']
 when "rhel"
   package "openssh-clients"
 end
@@ -8,27 +8,27 @@ end
 package "openssh-server"
 
 
-homedir = "/home/#{node.hopsworks.user}"
+homedir = "/home/#{node['hopsworks']['user']}"
 
 kagent_keys "#{homedir}" do
-  cb_user node.hopsworks.user
-  cb_group node.hopsworks.group
+  cb_user node['hopsworks']['user']
+  cb_group node['hopsworks']['group']
   action :generate  
 end  
 
 kagent_keys "#{homedir}" do
-  cb_user node.hopsworks.user
-  cb_group node.hopsworks.group
+  cb_user node['hopsworks']['user']
+  cb_group node['hopsworks']['group']
   cb_name "hopsworks"
   cb_recipe "master"  
   action :return_publickey
 end  
 
 domain_name="domain1"
-domains_dir = node[:glassfish][:domains_dir]
+domains_dir = node['glassfish']['domains_dir']
 admin_port = 4848
-username=node[:hopsworks][:admin][:user]
-password=node[:hopsworks][:admin][:password]
+username=node['hopsworks']['admin']['user']
+password=node['hopsworks']['admin']['password']
 
 #
 # mod_ajp http://www.devwithimagination.com/2015/08/13/apache-as-a-reverse-proxy-to-glassfish/
@@ -37,7 +37,7 @@ password=node[:hopsworks][:admin][:password]
 # docker
 # https://github.com/jelastic-jps/glassfish/
 
-glassfish_asadmin "create-node-ssh --nodehost node['host'] --installdir #{node[:glassfish][:base_dir]}/versions/current master" do
+glassfish_asadmin "create-node-ssh --nodehost node['host'] --installdir #{node['glassfish']['base_dir']}/versions/current master" do
    domain_name domain_name
    password_file "#{domains_dir}/#{domain_name}_admin_passwd"
    username username
