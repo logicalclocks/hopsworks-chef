5392ae6f83b66b2fabcd0515b8c0c4db571a8693
karamel <jdowling@sics.se>
2017-10-07 10:16:14 +0200

    install jupyter python kernel in backgroun when first starting jupyter notebook

--- recipes/install.rb
+++ recipes/install.rb
@@ -494,6 +494,15 @@ template "#{theDomain}/bin/jupyter.sh" do
   action :create
 end
 
+template "#{theDomain}/bin/jupyter-kernel-install.sh" do
+  source "jupyter-kernel-install.sh.erb"
+  owner node['glassfish']['user']
+  group node['glassfish']['group']
+  mode "550"
+  action :create
+end
+
+
 template "#{theDomain}/bin/jupyter-project-cleanup.sh" do
   source "jupyter-project-cleanup.sh.erb"
   owner node['glassfish']['user']
