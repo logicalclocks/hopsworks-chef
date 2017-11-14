b28ecc1b184575016cef1e8289139ca1f2cce5a5
ErmiasG <ermiasg@kth.se>
2017-11-14 16:42:09 +0100

    add CLUSTER_AGENT to twofactor exclude groups list

--- attributes/default.rb
+++ attributes/default.rb
@@ -60,8 +60,8 @@ default['hopsworks']['pixiedust']['enabled']        = "false"
 default['hopsworks']['admin']['user']               = "adminuser"
 default['hopsworks']['admin']['password']           = "adminpw"
 default['glassfish']['cert']['password']            = "#{node['hopsworks']['admin']['password']}"
-default['hopsworks']['twofactor_auth']           = "false"
-default['hopsworks']['twofactor_exclude_groups'] = "AGENT" #semicolon separated list of roles
+default['hopsworks']['twofactor_auth']              = "false"
+default['hopsworks']['twofactor_exclude_groups']    = "AGENT;CLUSTER_AGENT" #semicolon separated list of roles
 
 ## Suffix can be: (defaults to minutes if omitted)
 ## ms: milliseconds
