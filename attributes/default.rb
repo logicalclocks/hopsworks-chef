cc4dbbe2a94113ff1513719323f22fc249998e41
Alex Ormenisan <ormenisan.adrian@gmail.com>
2017-12-05 13:59:20 +0100

    no implicit conversion from int to string

--- attributes/default.rb
+++ attributes/default.rb
@@ -138,19 +138,19 @@ default['hopsworks']['hopssite']['version']            = "none" # default for {h
 if(node['hopsworks']['hopssite']['version'].eql? "none") 
   default['hopsworks']['dela']['enabled']              = "false"
   default['hopsworks']['hopssite']['domain']           = "hops.site"
-  default['hopsworks']['hopssite']['port']             = 51081
+  default['hopsworks']['hopssite']['port']             = "51081"
   default['hopssite']['url']                           = "https://hops.site:443"
 end
 if(node['hopsworks']['hopssite']['version'].eql? "hops")
   default['hopsworks']['dela']['enabled']              = "true"
   default['hopsworks']['hopssite']['domain']           = "hops.site"
-  default['hopsworks']['hopssite']['port']             = 51081
+  default['hopsworks']['hopssite']['port']             = "51081"
   default['hopssite']['url']                           = "https://hops.site:443"
 end
 if(node['hopsworks']['hopssite']['version'].eql? "bbc5")
   default['hopsworks']['dela']['enabled']              = "true"
   default['hopsworks']['hopssite']['domain']           = "bbc5.sics.se"
-  default['hopsworks']['hopssite']['port']             = 42004
+  default['hopsworks']['hopssite']['port']             = "42004"
   default['hopssite']['url']                           = "http://bbc5.sics.se:8080"
 end
 default['hopsworks']['hopssite']['base_uri']  = "https://" + node['hopsworks']['hopssite']['domain'] + ":" + node['hopsworks']['hopssite']['port']  + "/hops-site/api"
