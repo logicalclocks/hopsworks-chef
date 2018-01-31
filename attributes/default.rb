7e26c1320661a4be208b532fc81deed9415adea5
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2018-01-31 14:36:56 +0100

    Update otp auth version

--- attributes/default.rb
+++ attributes/default.rb
@@ -54,7 +54,7 @@ default['hopsworks']['http_logs']['enabled']     = "true"
 
 
 default['glassfish']['package_url']              = node['download_url'] + "/payara-#{node['glassfish']['version']}.zip"
-default['hopsworks']['cauth_url']                = "#{node['download_url']}/otp-auth-2.0.jar"
+default['hopsworks']['cauth_url']                = "#{node['download_url']}/otp-auth-0.3.0.jar"
 default['hopsworks']['war_url']                  = "#{node['download_url']}/hopsworks/#{node['hopsworks']['version']}/hopsworks-web.war"
 default['hopsworks']['ca_url']                   = "#{node['download_url']}/hopsworks/#{node['hopsworks']['version']}/hopsworks-ca.war"
 default['hopsworks']['ear_url']                  = "#{node['download_url']}/hopsworks/#{node['hopsworks']['version']}/hopsworks-ear.ear"
