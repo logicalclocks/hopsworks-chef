38dbec438d6eed21e8f00105b35833e8feadd144
Fabio Buso <dev.siroibaf@gmail.com>
2018-02-20 17:29:18 +0100

    Prepare for version: 0.5.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,12 +12,12 @@ include_attribute "drelephant"
 include_attribute "dela"
 include_attribute "hive2"
 
-default['hopsworks']['version']                  = "0.4.0-SNAPSHOT"
+default['hopsworks']['version']                  = "0.5.0-SNAPSHOT"
 
 # Flyway needs to know the previous versions of Hopsworks to generate the .sql files.
 # comma-separated string of previous versions hopsworks (not including the current version)
 # E.g., "0.1.1, 0.1.2"
-default['hopsworks']['versions']                 = "0.1.0,0.2.0,0.3.0"
+default['hopsworks']['versions']                 = "0.1.0,0.2.0,0.3.0,0.4.0"
 
 default['glassfish']['variant']                  = "payara"
 default['hopsworks']['user']                     = node['install']['user'].empty? ? "glassfish" : node['install']['user']
@@ -206,7 +206,7 @@ default['hopssite']['cert']['c']                       = node['hopsworks']['cert
 # Dela end
 
 default['hopsworks']['max_gpu_request_size']           = 1
-default['hopsworks']['max_cpu_request_size']           = 1 
+default['hopsworks']['max_cpu_request_size']           = 1
 
 default['hopsworks']['anaconda_enabled']               = node['kagent']['conda_enabled']
 
