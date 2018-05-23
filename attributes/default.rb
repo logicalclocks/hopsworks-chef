3b726d427325b92818bf26d0bc8883855eb520e8
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2018-05-23 11:19:13 +0200

    [HOPSWORKS-198] set hive as the only interpreter (#175)

--- attributes/default.rb
+++ attributes/default.rb
@@ -246,7 +246,7 @@ default['tfserving']['group']                          = node['install']['user']
 default['hopsworks']['livy_zeppelin_session_timeout']  = "3600"
 
 # Zeppelin
-default['hopsworks']['zeppelin_interpreters']  = "org.apache.zeppelin.livy.LivySparkInterpreter,org.apache.zeppelin.livy.LivyPySparkInterpreter,org.apache.zeppelin.livy.LivySparkRInterpreter,org.apache.zeppelin.livy.LivySparkSQLInterpreter,org.apache.zeppelin.spark.SparkInterpreter,org.apache.zeppelin.spark.PySparkInterpreter,org.apache.zeppelin.rinterpreter.RRepl,org.apache.zeppelin.rinterpreter.KnitR,org.apache.zeppelin.spark.SparkRInterpreter,org.apache.zeppelin.spark.SparkSqlInterpreter,org.apache.zeppelin.spark.DepInterpreter,org.apache.zeppelin.markdown.Markdown,org.apache.zeppelin.angular.AngularInterpreter,org.apache.zeppelin.flink.FlinkInterpreter"
+default['hopsworks']['zeppelin_interpreters']  = "org.apache.zeppelin.hopshive.HopsHiveInterpreter"
 
 
 #
