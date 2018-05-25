67dc47623e83aa5add442150b2dd63e645bd6465
Theofilos Kakantousis <t.kakantousis@gmail.com>
2018-05-25 10:52:37 +0200

    Prepare for version: 0.6.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jdowling@kth.se"
 license          "Apache v2.0"
 description      "Installs/Configures HopsWorks, the UI for Hops Hadoop."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.0"
+version          "0.6.0"
 source_url       "https://github.com/hopshadoop/hopsworks-chef"
 
 
