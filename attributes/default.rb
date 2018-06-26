34c4c0dc5dededabf80d632208960dfe948497a1
Jim Dowling <jdowling@sics.se>
2018-06-26 15:31:47 +0200

    [HOPSWORKS-587] Chef recipe bugfixes (#180)

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,12 +13,12 @@ include_attribute "dela"
 include_attribute "hive2"
 include_attribute "hops"
 
-default['hopsworks']['version']                  = "0.6.0-SNAPSHOT"
+default['hopsworks']['version']                  = node['install']['version']
 
 # Flyway needs to know the previous versions of Hopsworks to generate the .sql files.
 # comma-separated string of previous versions hopsworks (not including the current version)
 # E.g., "0.1.1, 0.1.2"
-default['hopsworks']['versions']                 = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0"
+default['hopsworks']['versions']                 = node['install']['versions']
 
 default['glassfish']['variant']                  = "payara"
 default['hopsworks']['user']                     = node['install']['user'].empty? ? "glassfish" : node['install']['user']
@@ -30,7 +30,7 @@ default['hopsworks']['port']                     = "8080"
 default['hopsworks']['secure_port']              = "8181"
 default['glassfish']['admin']['port']            = node['hopsworks']['admin']['port']
 default['glassfish']['port']                     = node['hopsworks']['port'].to_i
-default['glassfish']['version']                  = '4.1.2.174'
+default['glassfish']['version']                  = '4.1.2.174' #'5.182'  
 
 default['hopsworks']['dir']                      = node['install']['dir'].empty? ? "/usr/local" : node['install']['dir']
 default['glassfish']['install_dir']              = node['hopsworks']['dir']
