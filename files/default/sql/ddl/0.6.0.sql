287a75979f28c80ed3d398def5c89ba52ba290dd
robzor92 <robin.eric.andersson@gmail.com>
2018-08-16 17:13:16 +0200

    sql updates

--- files/default/sql/ddl/0.6.0.sql
+++ files/default/sql/ddl/0.6.0.sql
@@ -4,16 +4,16 @@
 CREATE TABLE IF NOT EXISTS `tensorboard` (
   `project_id` INT(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `hdfs_user_id` int(11) NOT NULL,
   `endpoint` VARCHAR(100) NOT NULL,
   `elastic_id` VARCHAR(100) NOT NULL,
   `pid` BIGINT NOT NULL,
   `last_accessed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `hdfs_logdir` VARCHAR(10000) NOT NULL,
-  `hdfs_user_id` int(11) NOT NULL,
   PRIMARY KEY (`project_id`,`user_id`),
-  FOREIGN KEY `fk_project_id` (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  FOREIGN KEY `fk_user_id` (`user_id`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  FOREIGN KEY `fk_hdfs_user_id` (`hdfs_user_id`) REFERENCES `hops`.`hdfs_users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+  FOREIGN KEY `project_id_fk` (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  FOREIGN KEY `user_id_fk` (`user_id`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  FOREIGN KEY `hdfs_user_id_fk` (`hdfs_user_id`) REFERENCES `hops`.`hdfs_users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 ALTER TABLE `hosts` DROP KEY `hostname`;
