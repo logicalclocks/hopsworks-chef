4088bf0032bf21d3f8acd0a829dcc0b236e79d68
Robin Andersson <robin.eric.andersson@gmail.com>
2019-02-01 14:26:37 +0100

    remove key and only use unique key

--- files/default/sql/ddl/updates/0.9.0.sql
+++ files/default/sql/ddl/updates/0.9.0.sql
@@ -1,1 +1,7 @@
 ALTER TABLE jupyter_project ADD CONSTRAINT unique_hdfs_user UNIQUE (hdfs_user_id);
+
+ALTER TABLE jupyter_project DROP FOREIGN KEY `FK_103_525`;
+
+ALTER TABLE jupyter_project ADD CONSTRAINT `fk_hdfs_user_id` FOREIGN KEY (`hdfs_user_id`) REFERENCES `hops`.`hdfs_users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE jupyter_project DROP KEY `hdfs_user_idx`;
