61775fc50c6911706049f81cba8ace3a6bda5ac1
Theofilos Kakantousis <t.kakantousis@gmail.com>
2019-03-18 16:26:57 +0100

    add files for 0.9.1

--- files/default/sql/ddl/updates/0.10.0.sql
+++ files/default/sql/ddl/updates/0.10.0.sql
@@ -1,5 +1,3 @@
-ALTER TABLE jupyter_project CHANGE `last_accessed` `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
 DROP TABLE IF EXISTS  `hopsworks`.`projectgenericuser_certs`;
 DROP TABLE IF EXISTS  `hopsworks`.`zeppelin_interpreter_confs`;
 
