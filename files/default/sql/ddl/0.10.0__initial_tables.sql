4ccaee2ea4935310e2c8d10b7dfc2cb72f6434d6
Jim Dowling <jdowling@sics.se>
2019-04-30 09:04:52 +0200

    updates

--- files/default/sql/ddl/0.10.0__initial_tables.sql
+++ files/default/sql/ddl/0.10.0__initial_tables.sql
@@ -1880,3 +1880,19 @@ CREATE TABLE IF NOT EXISTS `oauth_login_state` (
     ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
+
+--
+--  Maggy Driver Service Lookup Table in Hopsworks
+--
+CREATE TABLE IF NOT EXISTS `maggy_driver` (
+  `app_id` char(30) COLLATE latin1_general_cs NOT NULL,
+  `host_ip` varchar(128) COLLATE latin1_general_cs NOT NULL,
+  `port` int(11) NOT NULL,
+  `secret` varchar(128) COLLATE latin1_general_cs NOT NULL,
+  `created`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`app_id`)
+)
+  ENGINE = ndbcluster
+  DEFAULT CHARSET = latin1
+  COLLATE = latin1_general_cs;
+
