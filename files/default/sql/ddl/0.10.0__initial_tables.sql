695573be63719c32e9c5018e0a8fe6c729acd6bb
Jim Dowling <jdowling@sics.se>
2019-05-27 11:35:08 +0200

    [HOPSWORKS-1089] Integration of sparkmagic with maggy (#285)

--- files/default/sql/ddl/0.10.0__initial_tables.sql
+++ files/default/sql/ddl/0.10.0__initial_tables.sql
@@ -1818,3 +1818,21 @@ CREATE TABLE IF NOT EXISTS `oauth_login_state` (
     ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
+
+--
+--  Maggy Driver Service Lookup Table in Hopsworks
+--
+CREATE TABLE IF NOT EXISTS `maggy_driver` (
+  `id` MEDIUMINT NOT NULL AUTO_INCREMENT,
+  `app_id` char(30) COLLATE latin1_general_cs NOT NULL,
+  `host_ip` varchar(128) COLLATE latin1_general_cs NOT NULL,
+  `port` int(11) NOT NULL,
+  `secret` varchar(128) COLLATE latin1_general_cs NOT NULL,
+  `created`    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  KEY (`app_id`, `port`),
+  PRIMARY KEY (`id`)
+)
+  ENGINE = ndbcluster
+  DEFAULT CHARSET = latin1
+  COLLATE = latin1_general_cs;
+
