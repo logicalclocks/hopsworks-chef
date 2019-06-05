c8bbad2d5b7a002d157556017d302b2fd47b5536
Fabio Buso <buso.fabio@gmail.com>
2019-06-05 10:26:52 +0200

    Remove HOTFIX to restart Hopsworks during deployment

--- metadata.rb
+++ metadata.rb
@@ -44,7 +44,6 @@ recipe  "hopsworks::image", "Prepare for use as a virtualbox image."
 recipe  "hopsworks::rollback", "Rollback an upgrade to Hopsworks."
 
 recipe  "hopsworks::migrate", "Call expat to migrate between Hopsworks versions"
-recipe  "hopsworks::restart", "Hotfix"
 
 recipe  "hopsworks::purge", "Deletes glassfish installation."
 recipe  "hopsworks::hopssite", "Install hopssite on current vm"
--- recipes/restart.rb
+++
@@ -1,8 +0,0 @@
-bash 'restart-glassfish' do
-    user "root"
-    code <<-EOF
-       systemctl restart glassfish-domain1
-    EOF
-end
-
-
