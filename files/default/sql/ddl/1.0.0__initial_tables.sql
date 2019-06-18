8d64d581015e2a564a781d2747e398cd01cc26ac
Fabio Buso <buso.fabio@gmail.com>
2019-06-18 23:50:36 +0200

    [HOPSWORKS-1134] append - flyway creates schema history table itself

--- files/default/sql/ddl/1.0.0__initial_tables.sql
+++ files/default/sql/ddl/1.0.0__initial_tables.sql
@@ -450,28 +450,6 @@ CREATE TABLE `files_to_remove` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `flyway_schema_history`
---
-
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `flyway_schema_history` (
-  `installed_rank` int(11) NOT NULL,
-  `version` varchar(50) DEFAULT NULL,
-  `description` varchar(200) NOT NULL,
-  `type` varchar(20) NOT NULL,
-  `script` varchar(1000) NOT NULL,
-  `checksum` int(11) DEFAULT NULL,
-  `installed_by` varchar(100) NOT NULL,
-  `installed_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `execution_time` int(11) NOT NULL,
-  `success` tinyint(1) NOT NULL,
-  PRIMARY KEY (`installed_rank`),
-  KEY `flyway_schema_history_s_idx` (`success`)
-) ENGINE=ndbcluster DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Temporary table structure for view `hops_users`
 --
 
