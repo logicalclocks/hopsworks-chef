f90442cd04657794fe7ba89048f30b1c09c4fc8f
Ermias <ermiasg@kth.se>
2019-06-19 17:31:29 +0200

    add validation key updated date  and type

--- files/default/sql/ddl/updates/undo/1.0.0__undo.sql
+++ files/default/sql/ddl/updates/undo/1.0.0__undo.sql
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 DROP TABLE IF EXISTS `secrets`;
+
+ALTER TABLE `hopsworks`.`users` DROP COLUMN `validation_key_updated`;
+ALTER TABLE `hopsworks`.`users` DROP COLUMN `validation_key_type`;
+ALTER TABLE `hopsworks`.`users` CHANGE `activated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
