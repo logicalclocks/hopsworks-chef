7e3aee31e3ae8092c1544eff1ad9fb59dc36f777
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2019-07-10 09:01:05 +0200

    [HOPSWORKS-1183] Secrets storage tables (#307)

--- files/default/sql/ddl/1.0.0__initial_tables.sql
+++ files/default/sql/ddl/1.0.0__initial_tables.sql
@@ -1752,3 +1752,16 @@ CREATE TABLE `variables` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2019-06-14  8:07:38
+
+CREATE TABLE IF NOT EXISTS `secrets` (
+       `uid` INT NOT NULL,
+       `secret_name` VARCHAR(125) NOT NULL,
+       `secret` VARBINARY(10000) NOT NULL,
+       `added_on` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       `visibility` TINYINT NOT NULL,
+       `pid_scope` INT DEFAULT NULL,
+       PRIMARY KEY (`uid`, `secret_name`),
+       FOREIGN KEY `secret_uid` (`uid`) REFERENCES `users` (`uid`)
+          ON DELETE CASCADE
+          ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
