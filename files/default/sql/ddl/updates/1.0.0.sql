cafb93a9902b39d37a2cfafb6605acfa157e34ad
Ermias <ermiasg@kth.se>
2019-07-18 17:39:10 +0200

    tables for api key

--- files/default/sql/ddl/updates/1.0.0.sql
+++ files/default/sql/ddl/updates/1.0.0.sql
@@ -16,31 +16,26 @@ ALTER TABLE `hopsworks`.`users` ADD COLUMN `validation_key_type` VARCHAR(20) DEF
 ALTER TABLE `hopsworks`.`users` CHANGE COLUMN `activated` `activated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 CREATE TABLE IF NOT EXISTS `api_key` (
-  `id` INT(11) NOT NULL,
-  `key` VARCHAR(512) NOT NULL,
-  `salt` VARCHAR(256) NOT NULL,
-  `created` TIMESTAMP NOT NULL,
-  `modified` TIMESTAMP NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `user` INT(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `prefix` varchar(45) NOT NULL,
+  `secret` varchar(512) NOT NULL,
+  `salt` varchar(256) NOT NULL,
+  `created` timestamp NOT NULL,
+  `modified` timestamp NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `key_UNIQUE` (`key` ASC),
-  UNIQUE INDEX `user_key_UNIQUE` (`user`, `name`),
-  INDEX `fk_api_key_1_idx` (`user`),
-  CONSTRAINT `fk_api_key_1`
-    FOREIGN KEY (`user`)
-    REFERENCES `users` (`uid`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+  UNIQUE KEY `prefix_UNIQUE` (`prefix`),
+  UNIQUE KEY `index4` (`user_id`,`name`),
+  KEY `fk_api_key_1_idx` (`user_id`),
+  CONSTRAINT `fk_api_key_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE NO ACTION) 
+  ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `api_key_scope` (
-  `id` INT(11) NOT NULL,
-  `api_key` INT(11) NOT NULL,
-  `scope` VARCHAR(45) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `api_key` int(11) NOT NULL,
+  `scope` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `index2` (`api_key`, `scope`),
-  CONSTRAINT `fk_api_key_scope_1`
-    FOREIGN KEY (`api_key`)
-    REFERENCES `api_key` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+  UNIQUE KEY `index2` (`api_key`,`scope`),
+  CONSTRAINT `fk_api_key_scope_1` FOREIGN KEY (`api_key`) REFERENCES `api_key` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION) 
+  ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
