8b2dc75395779a081b101758516e71277ef91a05
ErmiasG <ermiasg@kth.se>
2019-08-01 16:36:03 +0200

    [HOPSWORKS-1135] (#312)

--- files/default/sql/ddl/1.0.0__initial_tables.sql
+++ files/default/sql/ddl/1.0.0__initial_tables.sql
@@ -1767,3 +1767,28 @@ CREATE TABLE IF NOT EXISTS `secrets` (
           ON DELETE CASCADE
           ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `api_key` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `prefix` varchar(45) NOT NULL,
+  `secret` varchar(512) NOT NULL,
+  `salt` varchar(256) NOT NULL,
+  `created` timestamp NOT NULL,
+  `modified` timestamp NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `prefix_UNIQUE` (`prefix`),
+  UNIQUE KEY `index4` (`user_id`,`name`),
+  KEY `fk_api_key_1_idx` (`user_id`),
+  CONSTRAINT `fk_api_key_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE NO ACTION) 
+  ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `api_key_scope` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `api_key` int(11) NOT NULL,
+  `scope` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index2` (`api_key`,`scope`),
+  CONSTRAINT `fk_api_key_scope_1` FOREIGN KEY (`api_key`) REFERENCES `api_key` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION) 
+  ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
--- files/default/sql/ddl/updates/undo/1.0.0__undo.sql
+++ files/default/sql/ddl/updates/undo/1.0.0__undo.sql
@@ -3,3 +3,6 @@ DROP TABLE IF EXISTS `secrets`;
 ALTER TABLE `hopsworks`.`users` DROP COLUMN `validation_key_updated`;
 ALTER TABLE `hopsworks`.`users` DROP COLUMN `validation_key_type`;
 ALTER TABLE `hopsworks`.`users` CHANGE COLUMN `activated` `activated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+DROP TABLE IF EXISTS `hopsworks`.`api_key`;
+DROP TABLE IF EXISTS `hopsworks`.`api_key_scope`;
