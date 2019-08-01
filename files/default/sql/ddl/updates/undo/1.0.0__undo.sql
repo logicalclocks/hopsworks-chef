fe18fb899d980478fe78b5d7e52620b4a3b83da6
Ermias <ermiasg@kth.se>
2019-08-01 16:34:55 +0200

    fix prefix

--- files/default/sql/ddl/updates/undo/1.0.0__undo.sql
+++ files/default/sql/ddl/updates/undo/1.0.0__undo.sql
@@ -4,5 +4,5 @@ ALTER TABLE `hopsworks`.`users` DROP COLUMN `validation_key_updated`;
 ALTER TABLE `hopsworks`.`users` DROP COLUMN `validation_key_type`;
 ALTER TABLE `hopsworks`.`users` CHANGE COLUMN `activated` `activated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
-DROP TABLE IF EXISTS `api_key`;
-DROP TABLE IF EXISTS `api_key_scope`;
+DROP TABLE IF EXISTS `hopsworks`.`api_key`;
+DROP TABLE IF EXISTS `hopsworks`.`api_key_scope`;
