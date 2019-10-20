c85b64c2a596a7be3299ba46109c24aadf65b4a6
Ermias <ermiasg@kth.se>
2019-10-20 16:50:38 +0200

    fix table

--- files/default/sql/ddl/updates/undo/1.1.0__undo.sql
+++ files/default/sql/ddl/updates/undo/1.1.0__undo.sql
@@ -1,3 +1,14 @@
 ALTER TABLE `hopsworks`.`jobs` CHANGE COLUMN `json_config` `json_config` TEXT COLLATE latin1_general_cs NOT NULL;
 ALTER TABLE `hopsworks`.`jupyter_settings` CHANGE COLUMN `json_config` `json_config` TEXT COLLATE latin1_general_cs NOT NULL;
 ALTER TABLE `hopsworks`.`message` CHANGE COLUMN `content` `content` TEXT COLLATE latin1_general_cs NOT NULL;
+
+DROP TABLE IF EXISTS `hopsworks`.`dataset_shared_with`;
+
+ALTER TABLE `hopsworks`.`dataset`
+ADD `shared` tinyint(1) NOT NULL DEFAULT '0',
+ADD `status` tinyint(1) NOT NULL DEFAULT '1',
+ADD `editable` tinyint(1) NOT NULL DEFAULT '1',
+ADD `inode_id` bigint(20) NOT NULL,
+DROP INDEX `uq_dataset` ,
+ADD UNIQUE INDEX `uq_dataset` (`inode_pid`,`projectId`,`inode_name`),
+ADD INDEX `inode_id`;
