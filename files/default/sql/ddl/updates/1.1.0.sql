cae78c1ebe00e17d53e96a3b1f14f372b0d8d094
Theofilos Kakantousis <t.kakantousis@gmail.com>
2019-11-01 16:02:41 +0100

    fix for multiple shared datasets

--- files/default/sql/ddl/updates/1.1.0.sql
+++ files/default/sql/ddl/updates/1.1.0.sql
@@ -1,4 +1,3 @@
--- Create table without FK. FK will be added after we delete the shared datasets, otherwise the delete query will fail with FK constrain violation
 CREATE TABLE IF NOT EXISTS `hopsworks`.`dataset_shared_with` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `dataset` int(11) NOT NULL,
@@ -12,11 +11,11 @@ CREATE TABLE IF NOT EXISTS `hopsworks`.`dataset_shared_with` (
   CONSTRAINT `fk_dataset_shared_with_2` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1;
 
--- Populate the new table
+-- Insert existing shared datasets into new table
 INSERT INTO hopsworks.dataset_shared_with (dataset, project, accepted)
 SELECT hopsworks.dataset.id, datasetinfo.projectid, datasetinfo.shared
-from hopsworks.dataset
-     join
+FROM hopsworks.dataset
+         JOIN
      (
          SELECT hopsworks.dataset.id,
                 hopsworks.dataset.inode_name as dataset_name,
@@ -30,11 +29,11 @@ from hopsworks.dataset
                            AND hopsworks.project.inode_name = hops.hdfs_inodes.name
                            AND hopsworks.project.partition_id = hops.hdfs_inodes.partition_id
                            AND hopsworks.dataset.inode_pid != hops.hdfs_inodes.id
-     ) as datasetinfo
-        on hopsworks.dataset.inode_name = datasetinfo.dataset_name
-             and hopsworks.dataset.inode_pid = datasetinfo.inode_pid
+     ) AS datasetinfo
+     ON hopsworks.dataset.inode_name = datasetinfo.dataset_name
+         AND hopsworks.dataset.inode_pid = datasetinfo.inode_pid
 WHERE hopsworks.dataset.id NOT IN
-(
+      (
           SELECT hopsworks.dataset.id
           FROM hopsworks.dataset
                    JOIN hopsworks.project ON hopsworks.dataset.projectId = hopsworks.project.id
@@ -43,9 +42,21 @@ WHERE hopsworks.dataset.id NOT IN
                             AND hopsworks.project.inode_name = hops.hdfs_inodes.name
                             AND hopsworks.project.partition_id = hops.hdfs_inodes.partition_id
                             AND hopsworks.dataset.inode_pid != hops.hdfs_inodes.id
-);
+      );
 
-
+-- Delete shared datasets from dataset table
+DELETE hopsworks.dataset
+FROM hopsworks.dataset
+         JOIN (
+    SELECT hops.hdfs_inodes.id as inode_id, hopsworks.project.projectname, hopsworks.project.id as project_id
+    FROM hopsworks.project
+             JOIN hops.hdfs_inodes
+                  ON hopsworks.project.inode_pid = hops.hdfs_inodes.parent_id
+                      AND hopsworks.project.inode_name = hops.hdfs_inodes.name
+                      AND hopsworks.project.partition_id = hops.hdfs_inodes.partition_id
+) AS projectinfo
+              ON hopsworks.dataset.inode_pid = projectinfo.inode_id
+                  AND hopsworks.dataset.projectId != projectinfo.project_id;
 
 ALTER TABLE `hopsworks`.`dataset`
     DROP COLUMN `shared`,
