a39de6b2d9dd28c50e89ba871a84b6f171afb19b
Theofilos Kakantousis <t.kakantousis@gmail.com>
2019-11-01 15:57:45 +0100

    fix for multiple shared datasets

--- files/default/sql/ddl/updates/1.1.0.sql
+++ files/default/sql/ddl/updates/1.1.0.sql
@@ -6,48 +6,46 @@ CREATE TABLE IF NOT EXISTS `hopsworks`.`dataset_shared_with` (
   `accepted` tinyint(1) NOT NULL DEFAULT '0',
   `shared_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `index2` (`dataset`,`project`)
+  UNIQUE KEY `index2` (`dataset`,`project`),
+  KEY `fk_dataset_shared_with_2_idx` (`project`),
+  CONSTRAINT `fk_dataset_shared_with_1` FOREIGN KEY (`dataset`) REFERENCES `dataset` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_dataset_shared_with_2` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1;
 
 -- Populate the new table
 INSERT INTO hopsworks.dataset_shared_with (dataset, project, accepted)
-SELECT id as dataset, project_id as project, shared as accepted
-FROM hopsworks.dataset JOIN (
-    SELECT hops.hdfs_inodes.id as inode_id, hopsworks.project.projectname, hopsworks.project.id as project_id
-    FROM hopsworks.project
-             JOIN hops.hdfs_inodes
-                  ON hopsworks.project.inode_pid = hops.hdfs_inodes.parent_id
-                      AND hopsworks.project.inode_name = hops.hdfs_inodes.name
-                      AND hopsworks.project.partition_id = hops.hdfs_inodes.partition_id
-) AS projectinfo
-    ON hopsworks.dataset.inode_pid = projectinfo.inode_id
-        AND hopsworks.dataset.projectId != projectinfo.project_id;
+SELECT hopsworks.dataset.id, datasetinfo.projectid, datasetinfo.shared
+from hopsworks.dataset
+     join
+     (
+         SELECT hopsworks.dataset.id,
+                hopsworks.dataset.inode_name as dataset_name,
+                hopsworks.dataset.shared     as shared,
+                hopsworks.project.id         as projectid,
+                hopsworks.dataset.inode_pid
+         FROM hopsworks.dataset
+                  JOIN hopsworks.project ON hopsworks.dataset.projectId = hopsworks.project.id
+                  JOIN hops.hdfs_inodes
+                       ON hopsworks.project.inode_pid = hops.hdfs_inodes.parent_id
+                           AND hopsworks.project.inode_name = hops.hdfs_inodes.name
+                           AND hopsworks.project.partition_id = hops.hdfs_inodes.partition_id
+                           AND hopsworks.dataset.inode_pid != hops.hdfs_inodes.id
+     ) as datasetinfo
+        on hopsworks.dataset.inode_name = datasetinfo.dataset_name
+             and hopsworks.dataset.inode_pid = datasetinfo.inode_pid
+WHERE hopsworks.dataset.id NOT IN
+(
+          SELECT hopsworks.dataset.id
+          FROM hopsworks.dataset
+                   JOIN hopsworks.project ON hopsworks.dataset.projectId = hopsworks.project.id
+                   JOIN hops.hdfs_inodes
+                        ON hopsworks.project.inode_pid = hops.hdfs_inodes.parent_id
+                            AND hopsworks.project.inode_name = hops.hdfs_inodes.name
+                            AND hopsworks.project.partition_id = hops.hdfs_inodes.partition_id
+                            AND hopsworks.dataset.inode_pid != hops.hdfs_inodes.id
+);
 
--- Delete shared datasets from dataset table
-DELETE hopsworks.dataset FROM hopsworks.dataset join (
-    SELECT hops.hdfs_inodes.id as inode_id, hopsworks.project.projectname, hopsworks.project.id as project_id
-    FROM hopsworks.project
-             JOIN hops.hdfs_inodes
-                  ON hopsworks.project.inode_pid = hops.hdfs_inodes.parent_id
-                      AND hopsworks.project.inode_name = hops.hdfs_inodes.name
-                      AND hopsworks.project.partition_id = hops.hdfs_inodes.partition_id
-) AS projectinfo
-    ON hopsworks.dataset.inode_pid = projectinfo.inode_id
-        AND hopsworks.dataset.projectId != projectinfo.project_id;
 
-
--- Add foreign keys
-ALTER TABLE `hopsworks`.`dataset_shared_with` ADD INDEX `fk_dataset_shared_with_1_idx` (`dataset` ASC), ADD INDEX `fk_dataset_shared_with_2_idx` (`project` ASC);
-ALTER TABLE `hopsworks`.`dataset_shared_with` ADD CONSTRAINT `fk_dataset_shared_with_1`
-    FOREIGN KEY (`dataset`)
-        REFERENCES `hopsworks`.`dataset` (`id`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    ADD CONSTRAINT `fk_dataset_shared_with_2`
-      FOREIGN KEY (`project`)
-          REFERENCES `hopsworks`.`project` (`id`)
-          ON DELETE NO ACTION
-          ON UPDATE NO ACTION;
 
 ALTER TABLE `hopsworks`.`dataset`
     DROP COLUMN `shared`,
