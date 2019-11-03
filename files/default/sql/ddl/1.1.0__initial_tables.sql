68d6a48c1f29a3a1da2b95fac1a7491c08335f04
Jim Dowling <jdowling@sics.se>
2019-11-03 15:16:23 +0100

    updates

--- files/default/sql/ddl/1.1.0__initial_tables.sql
+++ files/default/sql/ddl/1.1.0__initial_tables.sql
@@ -1515,15 +1515,15 @@ CREATE TABLE `training_dataset_feature` (
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `feature_group_hudi_commits` (
+CREATE TABLE `feature_group_hudi_commit` (
   `feature_group_id` int(11) NOT NULL, -- from hudidataset name -> lookup feature_group
   `commit_id` int(11) NOT NULL,
-  `hdfs_user` varchar(255) COLLATE latin1_general_cs DEFAULT NULL,  -- server-side
---  `app_id` char(30) COLLATE latin1_general_cs DEFAULT NULL,  
+  `hdfs_user` varchar(255) COLLATE latin1_general_cs DEFAULT NULL, 
   `commit_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `num_rows` int(11) DEFAULT '0', -- count on the DF
   PRIMARY KEY (`feature_group_id`, `commit_id`),
   KEY `commit_id_idx` (`commit_id`),
+  KEY `hdfs_user_idx` (`hdfs_user`),  
   KEY `commit_date_idx` (`commit_date`),  
   CONSTRAINT `feature_group_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
