5b4fb94321793fbef305cfca6b6ce378a67032da
Moritz Meister <meister.mo@gmail.com>
2019-11-05 10:42:54 +0100

    Add Statistics Settings column and table

--- files/default/sql/ddl/updates/undo/1.1.0__undo.sql
+++ files/default/sql/ddl/updates/undo/1.1.0__undo.sql
@@ -5,3 +5,12 @@ ALTER TABLE `hopsworks`.`message` CHANGE COLUMN `content` `content` TEXT COLLATE
 ALTER TABLE hopsworks.`jupyter_settings` DROP COLUMN `python_kernel`;
 ALTER TABLE hopsworks.`jupyter_settings` DROP COLUMN `docker_config`;
 
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `desc_stats_enabled`;
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `feat_corr_enabled`;
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `feat_hist_enabled`;
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `cluster_analysis_enabled`;
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `num_clusters`;
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `num_bins`;
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `corr_method`;
+
+DROP TABLE IF EXISTS `hopsworks`.`statistic_columns`;
