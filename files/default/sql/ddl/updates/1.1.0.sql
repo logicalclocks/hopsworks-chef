32f29956e117950aecb23604d4e25fec99cc8555
Ermias <ermiasg@kth.se>
2019-11-06 18:31:33 +0100

    fix sql

--- files/default/sql/ddl/updates/1.1.0.sql
+++ files/default/sql/ddl/updates/1.1.0.sql
@@ -3,5 +3,5 @@ ALTER TABLE `hopsworks`.`jupyter_settings` CHANGE COLUMN `json_config` `json_con
 ALTER TABLE `hopsworks`.`message` CHANGE COLUMN `content` `content` VARCHAR(11000) COLLATE latin1_general_cs NOT NULL;
 
 SET SQL_SAFE_UPDATES = 0;
-UPDATE `hopsworks`.`users` SET `tours_state`>0 WHERE `tours_state`=0;
+UPDATE `hopsworks`.`users` SET `tours_state`=0;
 SET SQL_SAFE_UPDATES = 1;
