f6583c6803626e5bf29555550b0aeb1e09d452d8
kai-chi <kajetan.maliszewski@gmail.com>
2020-01-09 11:23:01 +0100

    [HOPSWORKS-1503] REST API - Admin - Services

--- files/default/sql/ddl/updates/undo/1.3.0__undo.sql
+++ files/default/sql/ddl/updates/undo/1.3.0__undo.sql
@@ -9,3 +9,6 @@ ALTER TABLE `hopsworks`.`training_dataset` ADD CONSTRAINT `fk_hdfs_user_id` FORE
 ALTER TABLE `hopsworks`.`feature_store_feature` MODIFY COLUMN `description` VARCHAR(10000) COLLATE latin1_general_cs NOT NULL;
 
 DROP TABLE IF EXISTS `feature_store_tag`;
+
+ALTER TABLE `hopsworks`.`host_services` DROP KEY `service_UNIQUE`;
+ALTER TABLE `hopsworks`.`host_services` CHANGE COLUMN `name` `service` varchar(48) COLLATE latin1_general_cs NOT NULL;
