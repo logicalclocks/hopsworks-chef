88e3cda63639e882412df1e239144dccf2238c5e
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-01-27 22:02:38 +0100

    truncate conda commands

--- files/default/sql/ddl/updates/1.1.0.sql
+++ files/default/sql/ddl/updates/1.1.0.sql
@@ -280,5 +280,9 @@ ALTER TABLE `hopsworks`.`project_topics`
 ALTER TABLE `hopsworks`.`executions` ADD COLUMN `args` VARCHAR(10000) NOT NULL DEFAULT '' AFTER `hdfs_user`;
 
 ALTER TABLE `hopsworks`.`tensorboard` CHANGE `elastic_id` `ml_id` varchar(100) COLLATE latin1_general_cs NOT NULL;
+
+-- Truncate conda commands due to NON_NULL user_id which breaks FK
+TRUNCATE TABLE `hopsworks`.`conda_commands`;
+
 ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN  `user_id` int(11) NOT NULL;
 ALTER TABLE `hopsworks`.`conda_commands` ADD FOREIGN KEY `user_fk` (`user_id`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE NO ACTION;
