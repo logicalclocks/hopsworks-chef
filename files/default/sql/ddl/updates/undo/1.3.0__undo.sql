db93caba55c8278e86bdaaa0bfcfb341a9a65bb0
Ermias <ermiasg@kth.se>
2020-03-13 12:53:39 +0100

    create tags table

--- files/default/sql/ddl/updates/undo/1.3.0__undo.sql
+++ files/default/sql/ddl/updates/undo/1.3.0__undo.sql
@@ -7,3 +7,5 @@ ALTER TABLE `hopsworks`.`training_dataset` ADD COLUMN `hdfs_user_id` int(11) NOT
 ALTER TABLE `hopsworks`.`training_dataset` ADD CONSTRAINT `fk_hdfs_user_id` FOREIGN KEY (`hdfs_user_id`) REFERENCES `hops`.`hdfs_users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 ALTER TABLE `hopsworks`.`feature_store_feature` MODIFY COLUMN `description` VARCHAR(10000) COLLATE latin1_general_cs NOT NULL;
+
+DROP TABLE IF EXISTS `feature_store_tag`;
