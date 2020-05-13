c43429e242c1b321c59f178f8b1d7c300890dfca
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2020-05-13 20:00:07 +0200

    [HOPSWORKS-1779] add kube master (#436)

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,6 +13,7 @@ include_attribute "dela"
 include_attribute "hive2"
 include_attribute "hops"
 include_attribute "hops_airflow"
+include_attribute "kube-hops"
 
 default['hopsworks']['version']                  = node['install']['version']
 default['hopsworks']['current_version']          = node['install']['current_version']
