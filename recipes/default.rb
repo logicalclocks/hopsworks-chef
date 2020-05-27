c45c6be0d2ea8a0d67359faca81edc633d10a7bf
Fabio Buso <buso.fabio@gmail.com>
2020-05-27 13:12:15 +0200

    Fix typo

--- recipes/default.rb
+++ recipes/default.rb
@@ -1057,7 +1057,7 @@ hopsworks_grants "restart_glassfish" do
 end
 
 hopsworks_certs "generate-int-certs" do
-  subject     "/CN=#{consul_helpers.get_service_fqdn("hopsworks.glassfish")}/OU=0"
+  subject     "/CN=#{consul_helper.get_service_fqdn("hopsworks.glassfish")}/OU=0"
   action      :generate_int_certs
   not_if      (::File.exist?("#{node['hopsworks']['config_dir']}/internal_bundle.crt"))
 end
