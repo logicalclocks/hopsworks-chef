4e41b1d2793f77e888b738654a9e5e3f81e0f261
Antonios Kouzoupis <antonios@logicalclocks.com>
2020-07-23 18:27:07 +0200

    [HOPSWORKS-1923] Remove user from certs group

--- recipes/install.rb
+++ recipes/install.rb
@@ -58,6 +58,14 @@ user node['hopsworks']['user'] do
   not_if { node['install']['external_users'].casecmp("true") == 0 }
 end
 
+group node["kagent"]["certs_group"] do
+  action :manage
+  append true
+  excluded_members node['hopsworks']['user']
+  not_if { node['install']['external_users'].casecmp("true") == 0 }
+  only_if { conda_helpers.is_upgrade }
+end
+
 group node['conda']['group'] do
   action :modify
   members ["#{node['hopsworks']['user']}"]
