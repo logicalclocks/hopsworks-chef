3b75084c58da1d57d74f6a10282a932829f93dc1
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2020-09-06 20:23:27 +0200

    [HOPSWORKS-1995] move docker image regex (#482)

--- attributes/default.rb
+++ attributes/default.rb
@@ -435,6 +435,3 @@ default['hopsworks']['hdfs']['storage_policy']['base']        = "DB"
 default['hopsworks']['hdfs']['storage_policy']['log']         = "HOT"
 
 default['hopsworks']['enable_metadata_designer']              = "false"
-
-# Regular expression to sanitize projects Docker images
-default['hopsworks']['docker']['image-validation-regex']      = "^([a-z0-9]+(-[a-z0-9]+)*\.)*[a-z0-9]+(:[0-9]*)?(\/([a-zA-Z0-9\-]*))?\/([-:._a-zA-Z0-9]{0,62}[-:.a-zA-Z0-9]$)"
