59a748656310333e2000dd1a480e4dbcf8597191
Fabio Buso <buso.fabio@gmail.com>
2020-09-16 11:40:32 +0200

    [HOPSWORKS-2002] Fix update SQL file

--- files/default/sql/ddl/updates/undo/1.4.0__undo.sql
+++ files/default/sql/ddl/updates/undo/1.4.0__undo.sql
@@ -1,28 +1,31 @@
-ALTER TABLE `hopsworks`.`python_dep` DROP COLUMN `base_env`;
 ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN `host_id` int(11) NOT NULL;
 ALTER TABLE `hopsworks`.`conda_commands` ADD KEY (`host_id`);
 ALTER TABLE `hopsworks`.`conda_commands` ADD CONSTRAINT `FK_481_519` FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION ;
-ALTER TABLE `hopsworks`.`conda_commands` CHANGE `docker_image` `proj` varchar(255) COLLATE latin1_general_cs NOT NULL;
+ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN `proj` varchar(255) COLLATE latin1_general_cs NOT NULL;
+ALTER TABLE `hopsworks`.`conda_commands` DROP COLUMN `error_message`;
+ALTER TABLE `hopsworks`.`conda_commands` CHANGE `environment_yml` `environment_yml` VARCHAR(10000) COLLATE latin1_general_cs DEFAULT NULL;
+ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN `machine_type` varchar(52) COLLATE latin1_general_cs DEFAULT NULL;
+
 ALTER TABLE `hopsworks`.`jupyter_project` CHANGE `cid` `pid` bigint(20) NOT NULL;
 ALTER TABLE `hopsworks`.`tensorboard` CHANGE `cid` `pid` bigint(20) NOT NULL;
 ALTER TABLE `hopsworks`.`serving` CHANGE `cid` `local_pid` bigint(20) NOT NULL;
-ALTER TABLE `hopsworks`.`conda_commands` DROP COLUMN `error_message`;
-ALTER TABLE `hopsworks`.`conda_commands` CHANGE `environment_yml` `environment_yml` VARCHAR(10000) COLLATE latin1_general_cs DEFAULT NULL;
 
 ALTER TABLE `hopsworks`.`jupyter_settings` CHANGE COLUMN `base_dir` `base_dir` varchar(255) COLLATE latin1_general_cs DEFAULT '/Jupyter/';
 
+SET SQL_SAFE_UPDATES = 0;
 UPDATE `hopsworks`.`jupyter_settings` `j`
 JOIN `hopsworks`.`project` `p`
 ON `j`.`project_id` = `p`.`id`
 SET `j`.`base_dir` = '/Jupyter/';
+SET SQL_SAFE_UPDATES = 1;
 
 ALTER TABLE `hopsworks`.`jupyter_git_config` DROP COLUMN `git_backend`;
 
+ALTER TABLE `hopsworks`.`python_dep` DROP COLUMN `base_env`;
 ALTER TABLE `hopsworks`.`python_dep` DROP INDEX `dependency`;
 ALTER TABLE `hopsworks`.`python_dep` ADD COLUMN `machine_type` int(11) NOT NULL;
 ALTER TABLE `hopsworks`.`python_dep` ADD CONSTRAINT `dependency` UNIQUE (`dependency`,`version`,`install_type`,`repo_id`,`machine_type`);
 
-ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN `machine_type` varchar(52) COLLATE latin1_general_cs DEFAULT NULL;
 ALTER TABLE `hopsworks`.`hosts` ADD COLUMN `conda_enabled` tinyint(1) NOT NULL DEFAULT '1';
 
 CREATE TABLE IF NOT EXISTS `online_feature_group` (
@@ -34,7 +37,6 @@ CREATE TABLE IF NOT EXISTS `online_feature_group` (
   ENGINE = ndbcluster
   DEFAULT CHARSET = latin1
   COLLATE = latin1_general_cs;
-
 
 ALTER TABLE `hopsworks`.`cached_feature_group` DROP COLUMN `online_enabled`;
 ALTER TABLE `hopsworks`.`cached_feature_group` DROP COLUMN `default_storage`;
@@ -54,16 +56,19 @@ CREATE TABLE `hopsworks`.`tf_lib_mapping` (
 ALTER TABLE `hopsworks`.`project` DROP COLUMN `docker_image`;
 ALTER TABLE `hopsworks`.`project` ADD COLUMN `conda_env` tinyint(1) DEFAULT '0';
 
-ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN `docker_image` varchar(255) COLLATE latin1_general_cs NOT NULL;
 
 ALTER TABLE `hopsworks`.`feature_store_s3_connector` DROP COLUMN `server_encryption_algorithm`;
 ALTER TABLE `hopsworks`.`feature_store_s3_connector` DROP COLUMN `server_encryption_key`;
 
 ALTER TABLE `hopsworks`.`training_dataset_split` DROP INDEX `dataset_id_split_name`;
+
 ALTER TABLE `hopsworks`.`remote_user` DROP COLUMN `status`;
+
 ALTER TABLE `hopsworks`.`dataset` DROP COLUMN `permission`;
 ALTER TABLE `hopsworks`.`dataset_shared_with` DROP COLUMN `permission`;
 
 ALTER TABLE `hopsworks`.`activity` CHANGE COLUMN `activity` `activity` VARCHAR(128) COLLATE latin1_general_cs NOT NULL;
 
 UPDATE `hopsworks`.`anaconda_repo` SET `url`="PyPi" WHERE `url`="pypi";
+
+DROP TABLE IF EXISTS `remote_group_project_mapping`; 
