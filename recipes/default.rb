60994fd75947d8f9ed43982aba7138087fea9429
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-09-24 14:48:40 +0200

    Do not deploy Cloud version if it's unmanaged version (#500)
    * [unmanaged_localhost] Do not deploy Cloud version if it's unmanaged version
    
    * [unmanaged_localhost] Put back deleted line

--- recipes/default.rb
+++ recipes/default.rb
@@ -784,8 +784,14 @@ end
 node.override['glassfish']['asadmin']['timeout'] = 400
   
 if node['install']['enterprise']['install'].casecmp? "true" and exists_local("cloud", "default")
-  ear_name = (node['install']['kubernetes'].casecmp?("true") and node['install']['managed_kubernetes'].casecmp?("true")) ? "hopsworks-ear-cloud-kube.ear" : "hopsworks-ear-cloud.ear"
-  node.override['hopsworks']['ear_url'] = "#{node['hopsworks']['download_url']}/#{ear_name}"
+  unmanaged = false
+  if node.attribute? 'cloud' and node['cloud'].attribute? 'init' and node['cloud']['init'].attribute? 'config' and node['cloud']['init']['config'].attribute? 'unmanaged'
+    unmanaged = node['cloud']['init']['config']['unmanaged'].casecmp? 'true'
+  end
+  unless unmanaged
+    ear_name = (node['install']['kubernetes'].casecmp?("true") and node['install']['managed_kubernetes'].casecmp?("true")) ? "hopsworks-ear-cloud-kube.ear" : "hopsworks-ear-cloud.ear"
+    node.override['hopsworks']['ear_url'] = "#{node['hopsworks']['download_url']}/#{ear_name}"
+  end
 end
 
 glassfish_deployable "hopsworks-ear" do
