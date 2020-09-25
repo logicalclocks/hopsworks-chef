e1cad36f6b8f99d54fc0b13f633b2c93d0393148
Moritz Meister <8422705+moritzmeister@users.noreply.github.com>
2020-09-25 14:03:55 +0200

    [HOPSWORKS-1982] Deequ statistics for Feature Groups/Training Datasets (#480)

--- files/default/sql/ddl/2.0.0__initial_tables.sql
+++ files/default/sql/ddl/2.0.0__initial_tables.sql
@@ -388,23 +388,25 @@ CREATE TABLE `feature_store` (
 
 
 --
--- Table structure for table `featurestore_statistic`
+-- Table structure for table `feature_store_statistic`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `featurestore_statistic` (
+CREATE TABLE `feature_store_statistic` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `feature_group_id` int(11) DEFAULT NULL,
-  `training_dataset_id` int(11) DEFAULT NULL,
-  `name` varchar(500) COLLATE latin1_general_cs DEFAULT NULL,
-  `statistic_type` int(11) NOT NULL DEFAULT '0',
-  `value` varchar(13300) COLLATE latin1_general_cs NOT NULL,
+  `commit_time` VARCHAR(20) COLLATE latin1_general_cs NOT NULL,
+  `inode_pid` BIGINT(20) NOT NULL,
+  `inode_name` VARCHAR(255) COLLATE latin1_general_cs NOT NULL,
+  `partition_id` BIGINT(20) NOT NULL,
+  `feature_group_id` INT(11),
+  `training_dataset_id`INT(11),
   PRIMARY KEY (`id`),
   KEY `feature_group_id` (`feature_group_id`),
   KEY `training_dataset_id` (`training_dataset_id`),
-  CONSTRAINT `FK_693_956` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `FK_812_957` FOREIGN KEY (`training_dataset_id`) REFERENCES `training_dataset` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+  CONSTRAINT `fg_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `td_fk` FOREIGN KEY (`training_dataset_id`) REFERENCES `training_dataset` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `inode_fk` FOREIGN KEY (`inode_pid`,`inode_name`,`partition_id`) REFERENCES `hops`.`hdfs_inodes` (`parent_id`,`name`,`partition_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
