775093d97dede00d494e2b19958ae85b72bd7385
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-10-08 15:35:05 +0200

    Create rules and fg_rules tables

--- files/default/sql/ddl/2.2.0__initial_tables.sql
+++ files/default/sql/ddl/2.2.0__initial_tables.sql
@@ -291,6 +291,7 @@ CREATE TABLE `feature_group` (
   `feature_group_type` INT(11) NOT NULL DEFAULT '0',
   `on_demand_feature_group_id` INT(11) NULL,
   `cached_feature_group_id` INT(11) NULL,
+  `validation_type` INT(11) NOT NULL DEFAULT '4',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_version` (`feature_store_id`, `name`, `version`),
   KEY `feature_store_id` (`feature_store_id`),
@@ -1965,6 +1966,20 @@ CREATE TABLE `remote_group_project_mapping` (
   CONSTRAINT `fk_remote_group_project_mapping_1` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
+CREATE TABLE  IF NOT EXISTS `feature_group_validation` (
+                                                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                           `validation_time` TIMESTAMP(3),
+                                                           `inode_pid` BIGINT(20) NOT NULL,
+                                                           `inode_name` VARCHAR(255) COLLATE latin1_general_cs NOT NULL,
+                                                           `partition_id` BIGINT(20) NOT NULL,
+                                                           `feature_group_id` INT(11),
+                                                           `status` VARCHAR(20) COLLATE latin1_general_cs NOT NULL,
+                                                           PRIMARY KEY (`id`),
+                                                           KEY `feature_group_id` (`feature_group_id`),
+                                                           CONSTRAINT `fg_fk_fgv` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                                                           CONSTRAINT `featuregroupvalidation_inode_fk` FOREIGN KEY (`inode_pid`,`inode_name`,`partition_id`) REFERENCES `hops`.`hdfs_inodes` (`parent_id`,`name`,`partition_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
 CREATE TABLE `feature_group_commit` (
   `feature_group_id` int(11) NOT NULL, -- from hudi dataset name -> lookup feature_group
   `commit_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
@@ -1975,11 +1990,13 @@ CREATE TABLE `feature_group_commit` (
   `inode_pid`                         BIGINT(20)      NOT NULL,
   `inode_name`                        VARCHAR(255)    NOT NULL,
   `partition_id`                      BIGINT(20)      NOT NULL,
+  `validation_id` int(11),
   PRIMARY KEY (`feature_group_id`, `commit_id`),
   KEY `commit_id_idx` (`commit_id`),
   KEY `commit_date_idx` (`committed_on`),
   CONSTRAINT `feature_group_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `hopsfs_parquet_inode_fk` FOREIGN KEY (`inode_pid`, `inode_name`, `partition_id`) REFERENCES `hops`.`hdfs_inodes` (`parent_id`, `name`, `partition_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+  CONSTRAINT `hopsfs_parquet_inode_fk` FOREIGN KEY (`inode_pid`, `inode_name`, `partition_id`) REFERENCES `hops`.`hdfs_inodes` (`parent_id`, `name`, `partition_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fgc_fk_fgv` FOREIGN KEY (`validation_id`) REFERENCES `feature_group_validation` (`id`) ON DELETE SET NULL  ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 CREATE TABLE `cloud_role_mapping` (
@@ -2029,4 +2046,43 @@ CREATE TABLE `cached_feature_extra_constraints` (
   PRIMARY KEY (`id`),
   KEY `cached_feature_group_fk` (`cached_feature_group_id`),
   CONSTRAINT `cached_feature_group_fk1` FOREIGN KEY (`cached_feature_group_id`) REFERENCES `cached_feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `validation_rule` (
+                                                 `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                 `name` varchar(45) COLLATE latin1_general_cs DEFAULT NULL,
+                                                 `predicate` varchar(45) COLLATE latin1_general_cs DEFAULT NULL,
+                                                 `value_type` varchar(45) COLLATE latin1_general_cs DEFAULT NULL,
+                                                 `description` varchar(100) COLLATE latin1_general_cs DEFAULT NULL,
+                                                 PRIMARY KEY (`id`),
+                                                 UNIQUE KEY `unique_validation_rule` (`name`,`predicate`,`value_type`)
+) ENGINE=ndbcluster AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `feature_store_expectation` (
+                                                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                           `name` varchar(100) COLLATE latin1_general_cs DEFAULT NULL,
+                                                           `description` varchar(1000) COLLATE latin1_general_cs DEFAULT NULL,
+                                                           `feature_store_id` int(11) NOT NULL,
+                                                           `assertions` varchar(12000) COLLATE latin1_general_cs DEFAULT NULL,
+                                                           PRIMARY KEY (`id`),
+                                                           KEY `unique_fs_rules` (`feature_store_id`,`name`),
+                                                           CONSTRAINT `fk_fs_expectation_to_fs` FOREIGN KEY (`feature_store_id`) REFERENCES `feature_store` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `feature_group_expectation` (
+                                                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                           `feature_group_id` int(11) NOT NULL,
+                                                           `feature_store_expectation_id` int(11) NOT NULL,
+                                                           `description` varchar(1000) COLLATE latin1_general_cs DEFAULT NULL,
+                                                           PRIMARY KEY (`id`),
+                                                           CONSTRAINT `fk_fg_expectation_to_fg` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                                                           CONSTRAINT `fk_fg_expectation_to_fs_expectation` FOREIGN KEY (`feature_store_expectation_id`) REFERENCES `feature_store_expectation` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `feature_store_expectation_rule` (
+                                                                `feature_store_expectation_id` int(11) NOT NULL,
+                                                                `validation_rule_id` int(11) NOT NULL,
+                                                                PRIMARY KEY (`feature_store_expectation_id`, `validation_rule_id`),
+                                                                CONSTRAINT `fk_fs_expectation_rule_id` FOREIGN KEY (`feature_store_expectation_id`) REFERENCES `feature_store_expectation` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                                                                CONSTRAINT `fk_validation_rule_id` FOREIGN KEY (`validation_rule_id`) REFERENCES `validation_rule` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
--- files/default/sql/ddl/updates/2.2.0.sql
+++ files/default/sql/ddl/updates/2.2.0.sql
@@ -9,3 +9,58 @@ ALTER TABLE `users` DROP COLUMN `security_question`, DROP COLUMN `security_answe
 
 ALTER TABLE `hopsworks`.`feature_store_tag` DROP COLUMN `type`;
 ALTER TABLE `hopsworks`.`feature_store_tag` ADD COLUMN `tag_schema` VARCHAR(13000) NOT NULL DEFAULT '{"type":"string"}';
+CREATE TABLE IF NOT EXISTS `validation_rule` (
+                                                 `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                 `name` varchar(45) COLLATE latin1_general_cs DEFAULT NULL,
+                                                 `predicate` varchar(45) COLLATE latin1_general_cs DEFAULT NULL,
+                                                 `value_type` varchar(45) COLLATE latin1_general_cs DEFAULT NULL,
+                                                 `description` varchar(100) COLLATE latin1_general_cs DEFAULT NULL,
+                                                 PRIMARY KEY (`id`),
+                                                 UNIQUE KEY `unique_validation_rule` (`name`,`predicate`,`value_type`)
+) ENGINE=ndbcluster AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `feature_store_expectation` (
+                                                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                           `name` varchar(100) COLLATE latin1_general_cs DEFAULT NULL,
+                                                           `description` varchar(1000) COLLATE latin1_general_cs DEFAULT NULL,
+                                                           `feature_store_id` int(11) NOT NULL,
+                                                           `assertions` varchar(12000) COLLATE latin1_general_cs DEFAULT NULL,
+                                                           PRIMARY KEY (`id`),
+                                                           KEY `unique_fs_rules` (`feature_store_id`,`name`),
+                                                           CONSTRAINT `fk_fs_expectation_to_fs` FOREIGN KEY (`feature_store_id`) REFERENCES `feature_store` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `feature_group_expectation` (
+                                                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                           `feature_group_id` int(11) NOT NULL,
+                                                           `feature_store_expectation_id` int(11) NOT NULL,
+                                                           `description` varchar(1000) COLLATE latin1_general_cs DEFAULT NULL,
+                                                           PRIMARY KEY (`id`),
+                                                           CONSTRAINT `fk_fg_expectation_to_fg` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                                                           CONSTRAINT `fk_fg_expectation_to_fs_expectation` FOREIGN KEY (`feature_store_expectation_id`) REFERENCES `feature_store_expectation` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `feature_store_expectation_rule` (
+                                                                `feature_store_expectation_id` int(11) NOT NULL,
+                                                                `validation_rule_id` int(11) NOT NULL,
+                                                                PRIMARY KEY (`feature_store_expectation_id`, `validation_rule_id`),
+                                                                CONSTRAINT `fk_fs_expectation_rule_id` FOREIGN KEY (`feature_store_expectation_id`) REFERENCES `feature_store_expectation` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                                                                CONSTRAINT `fk_validation_rule_id` FOREIGN KEY (`validation_rule_id`) REFERENCES `validation_rule` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE  IF NOT EXISTS `feature_group_validation` (
+                                                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                                                           `validation_time` TIMESTAMP(3),
+                                                           `inode_pid` BIGINT(20) NOT NULL,
+                                                           `inode_name` VARCHAR(255) COLLATE latin1_general_cs NOT NULL,
+                                                           `partition_id` BIGINT(20) NOT NULL,
+                                                           `feature_group_id` INT(11),
+                                                           `status` VARCHAR(20) COLLATE latin1_general_cs NOT NULL,
+                                                           PRIMARY KEY (`id`),
+                                                           KEY `feature_group_id` (`feature_group_id`),
+                                                           CONSTRAINT `fg_fk_fgv` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                                                           CONSTRAINT `featuregroupvalidation_inode_fk` FOREIGN KEY (`inode_pid`,`inode_name`,`partition_id`) REFERENCES `hops`.`hdfs_inodes` (`parent_id`,`name`,`partition_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+ALTER TABLE `hopsworks`.`feature_group` ADD COLUMN `validation_type` INT(11) DEFAULT '4' AFTER `corr_method`;
+ALTER TABLE `hopsworks`.`feature_group_commit` ADD COLUMN `validation_id` int(11), ADD CONSTRAINT `fgc_fk_fgv` FOREIGN KEY (`validation_id`) REFERENCES `feature_group_validation` (`id`) ON DELETE SET NULL  ON UPDATE NO ACTION;
