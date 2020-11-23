92083216f8178bde320451c9b7ceeaedbe1b95f4
davitbzh <davit@logicalclocks.com>
2020-11-23 18:49:33 +0100

    cashed_feature

--- files/default/sql/ddl/2.1.0__initial_tables.sql
+++ files/default/sql/ddl/2.1.0__initial_tables.sql
@@ -2049,3 +2049,20 @@ CREATE TABLE `databricks_instance` (
   FOREIGN KEY `db_secret_fk` (`uid`, `secret_name`) REFERENCES `secrets` (`uid`, `secret_name`) ON DELETE CASCADE ON UPDATE NO ACTION,
   FOREIGN KEY `db_user_fk` (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cashed_feature` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cached_feature_group_id` int(11) NULL,
+  `name` varchar(1000) COLLATE latin1_general_cs NOT NULL,
+  `primary_column` tinyint(1) NOT NULL DEFAULT '0',
+  `partition_column` tinyint(1) NOT NULL DEFAULT '0',
+  `precombine_column` tinyint(1) NOT NULL DEFAULT '0',
+  `description` varchar(10000) COLLATE latin1_general_cs,
+  `type` varchar(1000) COLLATE latin1_general_cs NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cached_feature_group_fk` (`cached_feature_group_id`),
+  CONSTRAINT `cached_feature_group_fk1` FOREIGN KEY (`cached_feature_group_id`) REFERENCES `cached_feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+/*!40101 SET character_set_client = @saved_cs_client */;
--- files/default/sql/ddl/updates/undo/2.1.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.1.0__undo.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `hopsworks`.`conda_commands` ADD CONSTRAINT `FK_284_520` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+DROP TABLE IF EXISTS `hopsworks`.`cashed_feature`;
 
 ALTER TABLE `hopsworks`.`conda_commands` CHANGE `environment_file` `environment_yml` VARCHAR(1000) COLLATE latin1_general_cs DEFAULT NULL;
