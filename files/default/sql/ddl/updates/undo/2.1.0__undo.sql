face81fe1d648e3026fef6153af17c745554de01
davitbzh <davit@logicalclocks.com>
2020-12-03 09:19:03 +0100

    cached_feature_extra_constraints

--- files/default/sql/ddl/updates/undo/2.1.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.1.0__undo.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS `hopsworks`.`cached_feature_extra_constraints`;
 ALTER TABLE `hopsworks`.`feature_group_commit` MODIFY `committed_on` DATETIME;
-ALTER TABLE `hopsworks`.`conda_commands` ADD CONSTRAINT `FK_284_520` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-DROP TABLE IF EXISTS `hopsworks`.`cached_feature_extra_constraints`;
 
+ALTER TABLE `hopsworks`.`conda_commands` ADD CONSTRAINT `FK_284_520` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 ALTER TABLE `hopsworks`.`conda_commands` CHANGE `environment_file` `environment_yml` VARCHAR(1000) COLLATE latin1_general_cs DEFAULT NULL;
