ac4990ba0b1dbabcb7c32287aef5963ca5c85ff4
Fabio Buso <buso.fabio@gmail.com>
2020-12-06 16:25:47 +0100

    add query over everything parameters

--- files/default/sql/ddl/2.1.0__initial_tables.sql
+++ files/default/sql/ddl/2.1.0__initial_tables.sql
@@ -1879,12 +1879,14 @@ CREATE TABLE IF NOT EXISTS `feature_store_hopsfs_connector` (
 
 CREATE TABLE IF NOT EXISTS `on_demand_feature_group` (
   `id`                      INT(11)         NOT NULL AUTO_INCREMENT,
-  `query`                   VARCHAR(11000)  NOT NULL,
+  `query`                   VARCHAR(11000),
   `connector_id`            INT(11)         NOT NULL,
   `description`             VARCHAR(1000)   NULL,
   `inode_pid`               BIGINT(20)      NOT NULL,
   `inode_name`              VARCHAR(255)    NOT NULL,
   `partition_id`            BIGINT(20)      NOT NULL,
+  `data_format`             VARCHAR(10),
+  `path`                    VARCHAR(1000),
   PRIMARY KEY (`id`),
   ADD CONSTRAINT `on_demand_conn_fk` FOREIGN KEY (`connector_id`) 
   REFERENCES `hopsworks`.`feature_store_connector` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
--- files/default/sql/ddl/updates/2.1.0.sql
+++ files/default/sql/ddl/updates/2.1.0.sql
@@ -88,6 +88,9 @@ ALTER TABLE `feature_store_hopsfs_connector`
     DROP COLUMN `description`;
 
 ALTER TABLE `on_demand_feature_group` ADD COLUMN `connector_id` int(11), 
+    ADD COLUMN `data_format` VARCHAR(10),
+    ADD COLUMN `path` VARCHAR(1000),
+    MODIFY `query` VARCHAR(11000),
     ADD CONSTRAINT `on_demand_conn_fk` FOREIGN KEY (`connector_id`) 
     REFERENCES `hopsworks`.`feature_store_connector` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
