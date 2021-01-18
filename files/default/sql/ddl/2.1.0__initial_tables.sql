36bd8409afbae3a2efa56d40156dd76741bf52fb
davitbzh <davit@logicalclocks.com>
2021-01-18 17:58:47 +0100

    HudiCommitStatistics

--- files/default/sql/ddl/2.1.0__initial_tables.sql
+++ files/default/sql/ddl/2.1.0__initial_tables.sql
@@ -407,16 +407,19 @@ CREATE TABLE `feature_store` (
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `feature_store_statistic` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `commit_time` VARCHAR(20) COLLATE latin1_general_cs NOT NULL,
+  `commit_time` timestamp NOT NULL,
   `inode_pid` BIGINT(20) NOT NULL,
   `inode_name` VARCHAR(255) COLLATE latin1_general_cs NOT NULL,
   `partition_id` BIGINT(20) NOT NULL,
   `feature_group_id` INT(11),
+  `feature_group_commit_id` BIGINT(20),
   `training_dataset_id`INT(11),
   PRIMARY KEY (`id`),
   KEY `feature_group_id` (`feature_group_id`),
   KEY `training_dataset_id` (`training_dataset_id`),
+  KEY `feature_group_commit_id_fk` (`feature_group_id`, `feature_group_commit_id`),
   CONSTRAINT `fg_fk_fss` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fg_ci_fk_fss` FOREIGN KEY (`feature_group_id`, `feature_group_commit_id`) REFERENCES `feature_group_commit` (`feature_group_id`, `commit_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `td_fk_fss` FOREIGN KEY (`training_dataset_id`) REFERENCES `training_dataset` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `inode_fk` FOREIGN KEY (`inode_pid`,`inode_name`,`partition_id`) REFERENCES `hops`.`hdfs_inodes` (`parent_id`,`name`,`partition_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
