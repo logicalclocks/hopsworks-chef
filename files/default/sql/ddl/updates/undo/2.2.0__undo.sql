cf4fee001fe7ac4033c310498ae7e0561a7e50a0
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2021-02-09 18:59:33 +0100

    [HOPSWORKS-2071] Data validation (#512)

--- files/default/sql/ddl/updates/undo/2.2.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.2.0__undo.sql
@@ -52,3 +52,8 @@ ALTER TABLE `users`
 
 ALTER TABLE `hopsworks`.`feature_store_tag` ADD COLUMN `type` varchar(45) NOT NULL DEFAULT 'STRING';
 ALTER TABLE `hopsworks`.`feature_store_tag` DROP COLUMN `tag_schema`;
+DROP TABLE IF EXISTS `validation_rule`;
+DROP TABLE IF EXISTS `feature_group_rule`;
+DROP TABLE IF EXISTS `feature_group_validation`;
+DROP TABLE IF EXISTS `feature_store_expectation_rule`;
+ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `validation_type`;
