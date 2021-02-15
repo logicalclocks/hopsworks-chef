9c29af496c7e6bab98df2a9aeb1bc0db625e6bef
Fabio Buso <buso.fabio@gmail.com>
2021-02-15 13:23:12 +0100

    [HOPSWORKS-2366] Replace service with systemctl in recipes

--- providers/grants.rb
+++ providers/grants.rb
@@ -1,28 +1,3 @@
-action :reload_systemd do
-
-if node['services']['enabled'] == "true"
-  bash 'enable_systemd' do
-    user "root"
-    ignore_failure true
-    code <<-EOF
-          systemctl daemon-reload
-          systemctl reset-failed
-          systemctl enable glassfish-domain1
-    EOF
-  end
-end
-
-  bash 'reload_systemd' do
-    user "root"
-    retries 1
-    code <<-EOF
-          systemctl daemon-reload
-          service glassfish-domain1 restart
-    EOF
-  end
-
-end
-
 action :create_timers do
   exec = "#{node['ndb']['scripts_dir']}/mysql-client.sh"
 
@@ -37,12 +12,4 @@ action :create_timers do
     not_if "#{exec} -e 'show databases' | grep glassfish_timers"
   end
 
-
 end
-
-action :sshkeys do
-  # Set attribute for dashboard's public_key to the ssh public key
-  key=IO.readlines("#{node['glassfish']['base_dir']}/.ssh/id_rsa.pub").first
-  node.normal['hopsworks']['public_key']=key.gsub("\n","")
-end
-
