449696176c67a9c243c1d6943a578cc5b40533ef
Theofilos Kakantousis <t.kakantousis@gmail.com>
2021-04-27 10:51:28 +0200

    [HOPSWORKS-2506] Feature validation rule fixes

--- files/default/sql/ddl/updates/undo/2.3.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.3.0__undo.sql
@@ -1,1 +1,4 @@
 DROP TABLE IF EXISTS `default_job_configuration`;
+ALTER TABLE `hopsworks`.`serving` DROP COLUMN `deployed`;
+ALTER TABLE `hopsworks`.`serving` DROP COLUMN `revision`;
+ALTER TABLE `hopsworks`.`validation_rule` DROP COLUMN `feature_type`;
