70a724e0cc26f199fec94b0e83211702db0013e3
robzor <robin.eric.andersson@gmail.com>
2021-04-29 11:25:11 +0200

    [HOPSWORKS-2381] Project level configuration for Jupyter notebooks and Jobs (#602)

--- files/default/sql/ddl/2.3.0__initial_tables.sql
+++ files/default/sql/ddl/2.3.0__initial_tables.sql
@@ -818,6 +818,21 @@ CREATE TABLE `python_environment` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `default_job_configuration`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `default_job_configuration` (
+  `project_id` int(11) NOT NULL,
+  `type` varchar(128) COLLATE latin1_general_cs NOT NULL,
+  `config` VARCHAR(12500) COLLATE latin1_general_cs DEFAULT NULL,
+  PRIMARY KEY (`project_id`, `type`),
+  CONSTRAINT `FK_JOBCONFIG_PROJECT` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `project_devices`
 --
 
