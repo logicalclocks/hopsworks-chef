b69ba1878dc6926cb1c122110f3be0c307867c52
Theofilos Kakantousis <t.kakantousis@gmail.com>
2021-04-30 14:59:03 +0200

    [HOPSWORKS-2506] Feature validation type

--- files/default/sql/ddl/updates/2.3.0.sql
+++ files/default/sql/ddl/updates/2.3.0.sql
@@ -5,7 +5,5 @@ CREATE TABLE IF NOT EXISTS `default_job_configuration` (
   PRIMARY KEY (`project_id`, `type`),
   CONSTRAINT `FK_JOBCONFIG_PROJECT` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-ALTER TABLE `hopsworks`.`serving` ADD COLUMN `deployed` timestamp DEFAULT NULL;
-ALTER TABLE `hopsworks`.`serving` ADD COLUMN `revision` VARCHAR(8) DEFAULT NULL;
 
 ALTER TABLE `hopsworks`.`validation_rule` ADD COLUMN `feature_type` VARCHAR(45) COLLATE latin1_general_cs DEFAULT NULL AFTER `accepted_type`;
--- files/default/sql/ddl/updates/undo/2.3.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.3.0__undo.sql
@@ -1,4 +1,2 @@
 DROP TABLE IF EXISTS `default_job_configuration`;
-ALTER TABLE `hopsworks`.`serving` DROP COLUMN `deployed`;
-ALTER TABLE `hopsworks`.`serving` DROP COLUMN `revision`;
 ALTER TABLE `hopsworks`.`validation_rule` DROP COLUMN `feature_type`;
