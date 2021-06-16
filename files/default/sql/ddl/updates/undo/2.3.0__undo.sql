20671bf4bf849c40cf7454936b419d0e06cff432
moritzmeister <meister.mo@gmail.com>
2021-06-16 11:32:02 +0200

    increase schema column size

--- files/default/sql/ddl/updates/undo/2.3.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.3.0__undo.sql
@@ -13,3 +13,4 @@ ALTER TABLE `hopsworks`.`training_dataset_join` DROP COLUMN `prefix`;
 
 ALTER TABLE `hopsworks`.`serving` DROP COLUMN `docker_resource_config`;
 
+ALTER TABLE `schemas` MODIFY COLUMN `schema` VARCHAR(10000) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL;
