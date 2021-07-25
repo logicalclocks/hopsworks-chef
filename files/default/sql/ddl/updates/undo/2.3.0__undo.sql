3e6d24fbae5ea6ac67f3b7262dca31d2ff55e60e
Fabio Buso <buso.fabio@gmail.com>
2021-07-26 01:20:08 +0200

    Revert changes to the undo file

--- files/default/sql/ddl/updates/undo/2.3.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.3.0__undo.sql
@@ -14,3 +14,10 @@ ALTER TABLE `hopsworks`.`training_dataset_join` DROP COLUMN `prefix`;
 ALTER TABLE `hopsworks`.`serving` DROP COLUMN `docker_resource_config`;
 
 ALTER TABLE `schemas` MODIFY COLUMN `schema` VARCHAR(10000) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL;
+
+ALTER TABLE `hopsworks`.`serving` RENAME COLUMN `model_path` TO `artifact_path`;
+ALTER TABLE `hopsworks`.`serving` RENAME COLUMN `model_version` TO `version`;
+ALTER TABLE `hopsworks`.`serving` DROP COLUMN `artifact_version`;
+ALTER TABLE `hopsworks`.`serving` DROP COLUMN `transformer`;
+ALTER TABLE `hopsworks`.`serving` DROP COLUMN `transformer_instances`;
+ALTER TABLE `hopsworks`.`serving` DROP COLUMN `inference_logging`;
