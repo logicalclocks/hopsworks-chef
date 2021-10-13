3b6f811fc233a6df5e9747dfbb64afd74b037abe
Davit Bzhalava <davitbzhalava@MacBook-Pro-som-tillhor-Davit.local>
2021-10-13 15:20:53 +0400

    rebase with master

--- files/default/sql/ddl/updates/undo/2.4.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.4.0__undo.sql
@@ -74,5 +74,3 @@ ALTER TABLE `hopsworks`.`jupyter_settings` DROP COLUMN `no_limit`;
 ALTER TABLE `hopsworks`.`oauth_login_state` MODIFY COLUMN `state` VARCHAR(128);
 
 ALTER TABLE `hopsworks`.`feature_group` DROP COLUMN `event_time`;
-
-ALTER TABLE `hopsworks`.`feature_store_statistic` DROP COLUMN `for_transformation`;
