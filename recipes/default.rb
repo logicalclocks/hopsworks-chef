3dc9880ce090f0a4f5415760ad55908b021ce9b0
Jim Dowling <jim@logicalclocks.com>
2021-10-27 23:40:21 +0200

    add glassfish to airflow group

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,17 @@ include_recipe "java"
 Chef::Recipe.send(:include, Hops::Helpers)
 Chef::Resource.send(:include, Hops::Helpers)
 
+#
+# Hopsworks needs to be able to read DAGs using the airflow group role
+#
+group node['airflow']['group'] do
+  action :modify
+  members [hopsworksGroup]    
+  append true
+  not_if { node['install']['external_users'].casecmp("true") == 0 }  
+end
+
+
 domain_name= node['hopsworks']['domain_name']
 domains_dir = node['hopsworks']['domains_dir']
 # This is set correctly in hopsworks::install by the chef-glassfish recipe. As each recipe has it's own
