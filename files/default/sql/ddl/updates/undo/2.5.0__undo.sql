6099d5e8d54ee22c24ce500a08d7090a404e0a6d
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2021-11-10 14:54:32 +0100

    [HOPSWORKS-2721] Compliance rules improvements (#678)

---
+++ files/default/sql/ddl/updates/undo/2.5.0__undo.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `hopsworks`.`validation_rule` MODIFY COLUMN description VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL;
+ALTER TABLE `hopsworks`.`validation_rule` DROP INDEX `unique_validation_rule`;
+ALTER TABLE `hopsworks`.`validation_rule` ADD CONSTRAINT `unique_validation_rule` UNIQUE KEY (`name`,`predicate`,`accepted_type`);
