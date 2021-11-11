017be6f2d86dd616cbdc9b0ecb9509d8ff293a74
Robin Andersson <robin.eric.andersson@gmail.com>
2021-11-11 10:54:26 +0100

    [HOPSWORKS-2797] Add model name and serving state filters to ServingService (#682)

--- files/default/sql/ddl/updates/undo/2.5.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.5.0__undo.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `hopsworks`.`validation_rule` MODIFY COLUMN description VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL;
 ALTER TABLE `hopsworks`.`validation_rule` DROP INDEX `unique_validation_rule`;
 ALTER TABLE `hopsworks`.`validation_rule` ADD CONSTRAINT `unique_validation_rule` UNIQUE KEY (`name`,`predicate`,`accepted_type`);
+
+ALTER TABLE `hopsworks`.`serving` DROP COLUMN `model_name`;
