635cf816540db1c575ee88f240e8e642139753fb
davitbzh <davit.bzhalava@gmail.com>
2021-12-20 23:04:25 +0100

    rebase with master

--- files/default/sql/ddl/updates/2.5.0.sql
+++ files/default/sql/ddl/updates/2.5.0.sql
@@ -157,5 +157,4 @@ ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
 ALTER TABLE `hopsworks`.`feature_group` ADD COLUMN `stream_feature_group_id` INT(11) NULL;
 ALTER TABLE `hopsworks`.`feature_group` ADD KEY `stream_feature_group_fk` (`stream_feature_group_id`);
 ALTER TABLE `hopsworks`.`feature_group` ADD CONSTRAINT `stream_feature_group_fk` FOREIGN KEY (`stream_feature_group_id`) REFERENCES `stream_feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
 ALTER TABLE `hopsworks`.`cached_feature_extra_constraints` ADD COLUMN `stream_feature_group_id` INT(11) NULL;
