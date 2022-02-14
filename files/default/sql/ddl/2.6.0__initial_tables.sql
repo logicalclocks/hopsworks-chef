2eb46f956b1aea009cb13304d318816efb0b0071
kenneth <kenneth@logicalclocks.com>
2022-02-14 21:27:28 +0100

    create model feature before training dataset

--- files/default/sql/ddl/2.6.0__initial_tables.sql
+++ files/default/sql/ddl/2.6.0__initial_tables.sql
@@ -1299,6 +1299,25 @@ CREATE TABLE `topic_acls` (
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
+CREATE TABLE `model_feature` (
+                                 `id` int(11) NOT NULL AUTO_INCREMENT,
+                                 `name` varchar(63) NOT NULL,
+                                 `feature_store_id` int(11) NOT NULL,
+                                 `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                                 `creator` int(11) NOT NULL,
+                                 `version` int(11) NOT NULL,
+                                 `description` varchar(10000) COLLATE latin1_general_cs DEFAULT NULL,
+                                 `label` VARCHAR(255) NULL,
+                                 PRIMARY KEY (`id`),
+                                 UNIQUE KEY `name_version` (`feature_store_id`, `name`, `version`),
+                                 KEY `feature_store_id` (`feature_store_id`),
+                                 KEY `creator` (`creator`),
+                                 CONSTRAINT `mf_creator_fk` FOREIGN KEY (`creator`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+                                 CONSTRAINT `mf_feature_store_id_fk` FOREIGN KEY (`feature_store_id`) REFERENCES `feature_store` (`id`)
+                                     ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster AUTO_INCREMENT=9 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
 --
 -- Table structure for table `training_dataset`
 --
@@ -2186,21 +2205,3 @@ CREATE TABLE IF NOT EXISTS `git_repository_remotes` (
                                           KEY `git_repository_fk` (`repository`),
                                           CONSTRAINT `git_repository_fk` FOREIGN KEY (`repository`) REFERENCES `git_repositories` (`id`) ON DELETE CASCADE
 ) ENGINE=ndbcluster AUTO_INCREMENT=6164 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
-CREATE TABLE `model_feature` (
-                                 `id` int(11) NOT NULL AUTO_INCREMENT,
-                                 `name` varchar(63) NOT NULL,
-                                 `feature_store_id` int(11) NOT NULL,
-                                 `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-                                 `creator` int(11) NOT NULL,
-                                 `version` int(11) NOT NULL,
-                                 `description` varchar(10000) COLLATE latin1_general_cs DEFAULT NULL,
-                                 `label` VARCHAR(255) NULL,
-                                 PRIMARY KEY (`id`),
-                                 UNIQUE KEY `name_version` (`feature_store_id`, `name`, `version`),
-                                 KEY `feature_store_id` (`feature_store_id`),
-                                 KEY `creator` (`creator`),
-                                 CONSTRAINT `mf_creator_fk` FOREIGN KEY (`creator`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-                                 CONSTRAINT `mf_feature_store_id_fk` FOREIGN KEY (`feature_store_id`) REFERENCES `feature_store` (`id`)
-                                     ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=ndbcluster AUTO_INCREMENT=9 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
