4cedfbc225c8d8707a15aa2c13c93495cdbb6b0b
kenneth <kenneth@logicalclocks.com>
2022-02-15 11:58:05 +0100

    rename to feature view

--- files/default/sql/ddl/updates/2.6.0.sql
+++ files/default/sql/ddl/updates/2.6.0.sql
@@ -1,5 +1,5 @@
 -- Add ModelFeature entity
-CREATE TABLE `model_feature` (
+CREATE TABLE `feature_view` (
                                  `id` int(11) NOT NULL AUTO_INCREMENT,
                                  `name` varchar(63) NOT NULL,
                                  `feature_store_id` int(11) NOT NULL,
@@ -12,23 +12,24 @@ CREATE TABLE `model_feature` (
                                  UNIQUE KEY `name_version` (`feature_store_id`, `name`, `version`),
                                  KEY `feature_store_id` (`feature_store_id`),
                                  KEY `creator` (`creator`),
-                                 CONSTRAINT `mf_creator_fk` FOREIGN KEY (`creator`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-                                 CONSTRAINT `mf_feature_store_id_fk` FOREIGN KEY (`feature_store_id`) REFERENCES `feature_store` (`id`)
-                                     ON DELETE NO ACTION ON UPDATE NO ACTION
+                                 CONSTRAINT `fv_creator_fk` FOREIGN KEY (`creator`) REFERENCES `users` (`uid`) ON
+                                     DELETE NO ACTION ON UPDATE NO ACTION,
+                                 CONSTRAINT `fv_feature_store_id_fk` FOREIGN KEY (`feature_store_id`) REFERENCES
+                                     `feature_store` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=ndbcluster AUTO_INCREMENT=9 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-ALTER TABLE `hopsworks`.`training_dataset` ADD COLUMN `model_feature_id` INT(11) DEFAULT NULL;
-ALTER TABLE `hopsworks`.`training_dataset` ADD CONSTRAINT `td_model_feature_fk` FOREIGN KEY
-    (`model_feature_id`) REFERENCES `model_feature` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-ALTER TABLE `hopsworks`.`training_dataset_join` ADD COLUMN `model_feature_id` INT(11) DEFAULT NULL;
-ALTER TABLE `hopsworks`.`training_dataset_join` ADD CONSTRAINT `tdj_model_feature_fk` FOREIGN KEY (`model_feature_id`) REFERENCES
-    `model_feature` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-ALTER TABLE `hopsworks`.`training_dataset_feature` ADD COLUMN `model_feature_id` INT(11) DEFAULT NULL;
-ALTER TABLE `hopsworks`.`training_dataset_feature` ADD CONSTRAINT `tdf_model_feature_fk` FOREIGN KEY (`model_feature_id`)
-    REFERENCES `model_feature` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-ALTER TABLE `hopsworks`.`feature_store_activity` ADD COLUMN `model_feature_id` INT(11) DEFAULT NULL;
-ALTER TABLE `hopsworks`.`feature_store_activity` ADD CONSTRAINT `fsa_model_feature_fk` FOREIGN KEY (`model_feature_id`) REFERENCES
-    `model_feature` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `hopsworks`.`training_dataset` ADD COLUMN `feature_view_id` INT(11) DEFAULT NULL;
+ALTER TABLE `hopsworks`.`training_dataset` ADD CONSTRAINT `td_feature_view_fk` FOREIGN KEY
+    (`feature_view_id`) REFERENCES `feature_view` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `hopsworks`.`training_dataset_join` ADD COLUMN `feature_view_id` INT(11) DEFAULT NULL;
+ALTER TABLE `hopsworks`.`training_dataset_join` ADD CONSTRAINT `tdj_feature_view_fk` FOREIGN KEY (`feature_view_id`) REFERENCES
+    `feature_view` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `hopsworks`.`training_dataset_feature` ADD COLUMN `feature_view_id` INT(11) DEFAULT NULL;
+ALTER TABLE `hopsworks`.`training_dataset_feature` ADD CONSTRAINT `tdf_feature_view_fk` FOREIGN KEY (`feature_view_id`)
+    REFERENCES `feature_view` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `hopsworks`.`feature_store_activity` ADD COLUMN `feature_view_id` INT(11) DEFAULT NULL;
+ALTER TABLE `hopsworks`.`feature_store_activity` ADD CONSTRAINT `fsa_feature_view_fk` FOREIGN KEY (`feature_view_id`) REFERENCES
+    `feature_view` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 ALTER TABLE `hopsworks`.`training_dataset` ADD COLUMN `start_time` TIMESTAMP NULL;
 ALTER TABLE `hopsworks`.`training_dataset` ADD COLUMN `end_time` TIMESTAMP NULL;
