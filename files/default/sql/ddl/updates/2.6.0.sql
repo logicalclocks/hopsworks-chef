d06b3dedf6ed71370da78d13493828743a5341b2
davitbzh <44586065+davitbzh@users.noreply.github.com>
2022-03-14 11:59:32 +0100

    [HOPSWORKS-2543] add support to insert_stream() to ingest data into offline fs (#667)

--- files/default/sql/ddl/updates/2.6.0.sql
+++ files/default/sql/ddl/updates/2.6.0.sql
@@ -73,3 +73,24 @@ ALTER TABLE `hopsworks`.`external_training_dataset`
     ADD CONSTRAINT `ext_td_inode_fk` FOREIGN KEY (`inode_pid`, `inode_name`, `partition_id`) REFERENCES `hops`.`hdfs_inodes` (`parent_id`, `name`, `partition_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 ALTER TABLE `hopsworks`.`serving` ADD COLUMN `description` varchar(1000) COLLATE latin1_general_cs DEFAULT NULL;
+
+-- Add StreamFeatureGroup
+CREATE TABLE IF NOT EXISTS `stream_feature_group` (
+                                                      `id`                             INT(11) NOT NULL AUTO_INCREMENT,
+                                                      `offline_feature_group`          BIGINT(20) NOT NULL,
+                                                      PRIMARY KEY (`id`),
+                                                      CONSTRAINT `stream_fg_hive_fk` FOREIGN KEY (`offline_feature_group`) REFERENCES `metastore`.`TBLS` (`TBL_ID`) ON DELETE CASCADE ON UPDATE NO ACTION
+)
+ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
+
+ALTER TABLE `hopsworks`.`feature_group` ADD COLUMN `stream_feature_group_id` INT(11) NULL;
+ALTER TABLE `hopsworks`.`feature_group` ADD KEY `stream_feature_group_fk` (`stream_feature_group_id`);
+ALTER TABLE `hopsworks`.`feature_group` ADD CONSTRAINT `stream_feature_group_fk` FOREIGN KEY (`stream_feature_group_id`) REFERENCES `stream_feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE `hopsworks`.`cached_feature` ADD COLUMN `stream_feature_group_id` INT(11) NULL;
+ALTER TABLE `hopsworks`.`cached_feature` ADD KEY `stream_feature_group_fk2` (`stream_feature_group_id`);
+ALTER TABLE `hopsworks`.`cached_feature` ADD CONSTRAINT `stream_feature_group_fk2` FOREIGN KEY (`stream_feature_group_id`) REFERENCES `stream_feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE `hopsworks`.`cached_feature_extra_constraints` ADD COLUMN `stream_feature_group_id` INT(11) NULL;
+
+ALTER TABLE `hopsworks`.`feature_group_commit` MODIFY COLUMN `committed_on` TIMESTAMP(6) NOT NULL;
