018df2d8bfdb83eaf5e3acec2bac679364ba7650
Fabio Buso <buso.fabio@gmail.com>
2022-05-30 10:28:33 +0200

    [HOPSWORKS-3173] Kafka store number of partitions and replicas in the database (#765)

--- files/default/sql/ddl/updates/undo/2.6.0__undo.sql
+++ files/default/sql/ddl/updates/undo/2.6.0__undo.sql
@@ -84,3 +84,5 @@ DROP TABLE IF EXISTS `hopsworks`.`feature_store_bigquery_connector`;
 SET SQL_SAFE_UPDATES = 0;
 UPDATE `hopsworks`.`api_key_scope` SET `scope` = 'PYTHON' WHERE `scope` = 'PYTHON_LIBRARIES';
 SET SQL_SAFE_UPDATES = 1;
+
+ALTER TABLE `hopsworks`.`project_topics` DROP COLUMN `num_partitions`, DROP COLUMN `num_replicas`;
