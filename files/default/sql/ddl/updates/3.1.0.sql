8d1ea3c3877ce2e99c1b18390fd39993285a1e2e
Victor Jouffrey <vicatjou@gmail.com>
2022-09-15 09:33:21 +0200

    Add validation_time column in validation result table

--- files/default/sql/ddl/updates/3.1.0.sql
+++ files/default/sql/ddl/updates/3.1.0.sql
@@ -31,3 +31,6 @@ CREATE TABLE IF NOT EXISTS `tutorial` (
     `style` VARCHAR(200) NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
+
+-- Validation Result history
+ALTER TABLE `hopsworks`.`validation_result` ADD COLUMN `validation_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP;
