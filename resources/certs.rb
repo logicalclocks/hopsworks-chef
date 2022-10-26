62743532bbe7a11805258cd1d76f58143d3b92e6
Fabio Buso <buso.fabio@gmail.com>
2022-10-26 18:05:14 +0200

    [HWORKS-190] Hopsworks-chef should allow installation of custom certi… (#826)
    * [HWORKS-190] Hopsworks-chef should allow installation of custom certificate for Hopsworks UI
    
    * re-enable secure-admin after installing the certificate

--- resources/certs.rb
+++ resources/certs.rb
@@ -1,3 +1,3 @@
-actions :generate, :sign_hopssite, :generate_int_certs, :download_azure_ca_cert
+actions :generate, :sign_hopssite, :generate_int_certs, :download_azure_ca_cert, :import_certs
 
 attribute :subject, :kind_of => String 
