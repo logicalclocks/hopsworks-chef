8f131d0a56f587803b4f7f3ba013eaf578d326c4
Fabio Buso <buso.fabio@gmail.com>
2022-10-26 18:05:14 +0200

    [HWORKS-190] Hopsworks-chef should allow installation of custom certi… (#826)
    * [HWORKS-190] Hopsworks-chef should allow installation of custom certificate for Hopsworks UI
    
    * re-enable secure-admin after installing the certificate

--- providers/certs.rb
+++ providers/certs.rb
@@ -33,13 +33,82 @@ action :generate_int_certs do
         keytool -import -noprompt -alias internal -file root_ca.pem -keystore cacerts.jks -storepass #{node['hopsworks']['master']['password']}
     EOH
   end
-
 end
+
+action :import_certs do
+
+  remote_file "#{node['hopsworks']['config_dir']}/https_key.key" do
+    source node['hopsworks']['https']['key_url']
+    user node['hopsworks']['user']
+    group node['hopsworks']['group']
+    mode '0700'
+    action :create
+  end
+
+  remote_file "#{node['hopsworks']['config_dir']}/https_cert.pem" do
+    source node['hopsworks']['https']['cert_url']
+    user node['hopsworks']['user']
+    group node['hopsworks']['group']
+    mode '0700'
+    action :create
+  end
+
+  remote_file "#{node['hopsworks']['config_dir']}/https_ca.pem" do
+    source node['hopsworks']['https']['ca_url']
+    user node['hopsworks']['user']
+    group node['hopsworks']['group']
+    mode '0700'
+    action :create
+  end
+
+  # Add the certificate to the keystore.jks
+  bash "add_to_keystore" do
+    user node['hopsworks']['user']
+    group node['hopsworks']['group']
+    cwd node['hopsworks']['config_dir']
+    code <<-EOH
+        set -e
+        openssl pkcs12 -export -in https_cert.pem -inkey https_key.key -out cert_and_key.p12 -name s1as -CAfile https_ca.pem -caname s1as -password pass:#{node['hopsworks']['master']['password']}
+
+        # Remove existing certificate
+        keytool -delete -alias s1as -keystore keystore.jks -storepass #{node['hopsworks']['master']['password']}
+
+        # Import into the keystore
+        keytool -importkeystore -destkeystore keystore.jks -srckeystore cert_and_key.p12 -srcstoretype PKCS12 -alias s1as -srcstorepass #{node['hopsworks']['master']['password']} -deststorepass #{node['hopsworks']['master']['password']} -destkeypass #{node['hopsworks']['master']['password']}
+
+        # Remove existing certificate
+        keytool -delete -alias s1as -keystore cacerts.jks -storepass #{node['hopsworks']['master']['password']}
+
+        # Add Root CA into cacerts.jks
+        keytool -import -noprompt -alias s1as -file https_ca.pem -keystore cacerts.jks -storepass #{node['hopsworks']['master']['password']}
+    EOH
+  end
+
+  # Enable and disable secure admin to get it to pick up the new certificate
+  # If we don't do this, users won't be able to log in through the admin UI.
+  # We don't need to restart glassfish here as it's one immediately after by the recipes
+  glassfish_asadmin "disable-secure-admin" do
+    domain_name node['hopsworks']['domain_name']
+    password_file "#{node['hopsworks']['domains_dir']}/#{node['hopsworks']['domain_name']}_admin_passwd"
+    username node['hopsworks']['admin']['user']
+    admin_port node['hopsworks']['admin']['port']
+    secure false
+  end
+
+  glassfish_asadmin "enable-secure-admin" do
+    domain_name node['hopsworks']['domain_name']
+    password_file "#{node['hopsworks']['domains_dir']}/#{node['hopsworks']['domain_name']}_admin_passwd"
+    username node['hopsworks']['admin']['user']
+    admin_port node['hopsworks']['admin']['port']
+    secure false
+  end
+end
+
 
 action :download_azure_ca_cert do
   # Download azure root CA cert
   remote_file '/tmp/DigiCertGlobalRootG2.crt' do
-    source "#{node['hopsworks']['azure-ca-cert']['download-url']}"
+    source node['hopsworks']['azure-ca-cert']['download-url']
     mode '0755'
     action :create
   end
@@ -50,10 +119,10 @@ action :download_azure_ca_cert do
     cwd "/tmp"
     code <<-EOH
       set -e
-      GFDOMAIN=#{node['glassfish']['domains_dir']}/domain1
+      GFDOMAIN=#{node['hopsworks']['config_dir']}
       # Import into the keystore
-      keytool -import -noprompt -alias digicertglobalrootg2 -file DigiCertGlobalRootG2.crt -keystore $GFDOMAIN/config/cacerts.jks -storepass #{node['hopsworks']['master']['password']}
+      keytool -import -noprompt -alias digicertglobalrootg2 -file DigiCertGlobalRootG2.crt -keystore $GFDOMAIN/cacerts.jks -storepass #{node['hopsworks']['master']['password']}
     EOH
-    only_if "keytool -list -keystore $GFDOMAIN/config/cacerts.jks -alias digicertglobalrootg2 -storepass #{node['hopsworks']['master']['password']}"
+    not_if "keytool -list -keystore #{node['hopsworks']['config_dir']}/cacerts.jks -alias digicertglobalrootg2 -storepass #{node['hopsworks']['master']['password']}"
   end
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1197,9 +1197,13 @@ hopsworks_certs "generate-int-certs" do
   not_if      { ::File.exist?("#{node['hopsworks']['config_dir']}/internal_bundle.crt") }
 end
 
+hopsworks_certs "import-user-certs" do
+  action :import_certs
+  not_if { node['hopsworks']['https']['key_url'].eql?("") }
+end
+
 hopsworks_certs "download_azure_ca_cert" do
   action      :download_azure_ca_cert
-  not_if      { ::File.exist?("/tmp/DigiCertGlobalRootG2.crt") }
 end
 
 # Force reload of the certificate
