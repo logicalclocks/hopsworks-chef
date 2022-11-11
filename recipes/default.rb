82787c90f2955a42ba79423e2755b9a2bf5a6f2d
Gautier Berthou <gautier@hopsworks.ai>
2022-11-11 10:15:34 +0100

    [CLOUD-165] do not set ear url for cloud in chef

--- recipes/default.rb
+++ recipes/default.rb
@@ -915,18 +915,6 @@ end
 
 node.override['glassfish']['asadmin']['timeout'] = 600
 
-if node['install']['enterprise']['install'].casecmp? "true" and exists_local("cloud", "default")
-  unmanaged = false
-  if node.attribute? 'cloud' and node['cloud'].attribute? 'init' and node['cloud']['init'].attribute? 'config' and node['cloud']['init']['config'].attribute? 'unmanaged'
-    unmanaged = node['cloud']['init']['config']['unmanaged'].casecmp? 'true'
-  end
-  unless unmanaged
-    ear_name = (node['install']['kubernetes'].casecmp?("true") and node['install']['managed_kubernetes'].casecmp?("true")) ? "hopsworks-ear-cloud-kube.ear" : "hopsworks-ear-cloud.ear"
-    node.override['hopsworks']['ear_url'] = "#{node['hopsworks']['download_url']}/#{ear_name}"
-  end
-end
-
-
 if current_version.eql?("") == false
 #
 # undeploy previous version
