09e1df54a2e51adbb3a25e39d85ff76dc8f283f5
Robin Andersson <robin.eric.andersson@gmail.com>
2023-02-15 14:09:51 +0100

    [HWORKS-390] app_id field in execution table is too short (#888)

--- files/default/sql/ddl/updates/undo/3.2.0__undo.sql
+++ files/default/sql/ddl/updates/undo/3.2.0__undo.sql
@@ -4,3 +4,7 @@ ALTER TABLE `hopsworks`.`project` ADD COLUMN `retention_period` date DEFAULT NUL
   ADD COLUMN `deleted` tinyint(1) DEFAULT '0';
 
 DROP TABLE IF EXISTS `hopsworks`.`hdfs_command_execution`;
+
+ALTER TABLE `hopsworks`.`executions` MODIFY COLUMN `app_id` char(30) COLLATE latin1_general_cs DEFAULT NULL;
+
+ALTER TABLE `hopsworks`.`maggy_driver` MODIFY COLUMN `app_id` char(30) COLLATE latin1_general_cs NOT NULL;
