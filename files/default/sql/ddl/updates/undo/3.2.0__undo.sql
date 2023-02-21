38557796fd513c42450e2e0e21dcb8d2cbcd2728
Ralf <bubriks@gmail.com>
2023-02-21 10:36:53 +0100

    Update 3.2.0__undo.sql

--- files/default/sql/ddl/updates/undo/3.2.0__undo.sql
+++ files/default/sql/ddl/updates/undo/3.2.0__undo.sql
@@ -51,6 +51,8 @@ CREATE TABLE `topic_acls` (
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+SET SQL_SAFE_UPDATES = 0;
 UPDATE `project_team`
 SET team_role = 'Data scientist'
 WHERE team_member = 'serving@hopsworks.se';
+SET SQL_SAFE_UPDATES = 1;
