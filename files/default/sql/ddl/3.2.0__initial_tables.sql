344e13bee5e472bc94b75a77b81a85baeac6d000
Victor Jouffrey <37411285+vatj@users.noreply.github.com>
2023-02-27 13:39:38 +0100

    [FSTORE-626] Draft tables for persistence (#878)
    * Second draft
    
    * Add table for persistence of the feature monitoring config and result
    
    * Edit based on Review
    
    * Fix name change
    
    * Code review edit
    
    * Allow schedule config to be null, for manual trigger only use case
    
    * Change one to one mapping

--- files/default/sql/ddl/3.2.0__initial_tables.sql
+++ files/default/sql/ddl/3.2.0__initial_tables.sql
@@ -2277,3 +2277,57 @@ CREATE TABLE IF NOT EXISTS `hdfs_command_execution` (
   CONSTRAINT `fk_hdfs_file_command_1` FOREIGN KEY (`execution_id`) REFERENCES `executions` (`id`) ON DELETE CASCADE,
   CONSTRAINT `fk_hdfs_file_command_2` FOREIGN KEY (`src_inode_partition_id`,`src_inode_pid`,`src_inode_name`) REFERENCES `hops`.`hdfs_inodes` (`partition_id`, `parent_id`, `name`) ON DELETE CASCADE
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `feature_monitoring_config` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `feature_group_id` INT(11),
+    `feature_view_id` INT(11),
+    `job_id` INT(11) NOT NULL, -- dummy this should become ref to another table
+    `feature_name` VARCHAR(63) COLLATE latin1_general_cs NOT NULL,
+    `description` VARCHAR(2000) COLLATE latin1_general_cs DEFAULT NULL,
+    `name` VARCHAR(255) COLLATE latin1_general_cs NOT NULL,
+    `enabled` BOOLEAN DEFAULT TRUE,
+    `feature_monitoring_type` tinyint(4) NOT NULL,
+    `alert_config` VARCHAR(63) COLLATE latin1_general_cs, -- dummy this should become ref to another table
+    `scheduler_config` VARCHAR(63) COLLATE latin1_general_cs, -- dummy this should become ref to another table
+    `detection_window_config_id` INT(11),
+    `reference_window_config_id` INT(11),
+    `statistics_comparison_config_id` INT(11),
+    PRIMARY KEY (`id`),
+    KEY (`feature_name`),
+    CONSTRAINT `fg_monitoring_config_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+    CONSTRAINT `fv_monitoring_config_fk` FOREIGN KEY (`feature_view_id`) REFERENCES `feature_view` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `monitoring_window_config` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `window_config_type` INT(11) NOT NULL,
+    `specific_id` INT(11),
+    `time_offset` VARCHAR(63),
+    `window_length` VARCHAR(63),
+    `row_percentage` INT(11),
+    `specific_value` FLOAT,
+    PRIMARY KEY (`id`)
+) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `statistics_comparison_config` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `strict` BOOLEAN DEFAULT FALSE,
+    `relative` BOOLEAN DEFAULT FALSE,
+    `threshold` FLOAT,
+    `compare_on` INT(11) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `feature_monitoring_result` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `feature_monitoring_config_id` INT(11) NOT NULL,
+    `execution_id` INT(11) NOT NULL,  -- dummy this should become ref to another table
+    `monitoring_time` timestamp DEFAULT CURRENT_TIMESTAMP,
+    `shift_detected` BOOLEAN DEFAULT FALSE,
+    `detection_stats_id` INT(11) NOT NULL, -- dummy this should become ref to another table
+    `reference_stats_id` INT(11), -- dummy this should become ref to another table
+    `difference` FLOAT,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `config_monitoring_result_fk` FOREIGN KEY (`feature_monitoring_config_id`) REFERENCES `feature_monitoring_config` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
