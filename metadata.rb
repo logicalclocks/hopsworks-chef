39b3bae7c0b06b70b28ed7427c9f6d7e5f3bd656
Victor Jouffrey <37411285+vatj@users.noreply.github.com>
2023-03-09 11:15:42 +0100

    Feature flag feature monitoring (#893)

--- metadata.rb
+++ metadata.rb
@@ -1016,6 +1016,10 @@ attribute "hopsworks/max_allowed_long_running_http_requests",
 
 attribute "hopsworks/enable_flyingduck",
           :description => "Whether to enable flyingduck or not. Default: false",
+          
+# Feature monitoring feature flag
+attribute "hopsworks/enable_feature_monitoring",
+          :description => "Whether to enable feature monitoring with scheduling and alerting",
           :type => 'string'
 
 attribute "hopsworks/loadbalancer_external_domain",
