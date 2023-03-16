249a35f7e9d3ce6f134e3904d6beb7507db39a46
Javier de la Rúa Martínez <javier@logicalclocks.com>
2023-03-16 10:04:30 +0100

    [FSTORE-742] Statistics persistence in the DB (#898)

--- files/default/sql/ddl/updates/undo/3.2.0__undo.sql
+++ files/default/sql/ddl/updates/undo/3.2.0__undo.sql
@@ -64,6 +64,6 @@ DROP TABLE IF EXISTS `hopsworks`.`feature_monitoring_result`;
 DROP TABLE IF EXISTS `hopsworks`.`statistics_comparison_config`;
 DROP TABLE IF EXISTS `hopsworks`.`monitoring_window_config`;
 DROP TABLE IF EXISTS `hopsworks`.`feature_monitoring_config`;
-
+DROP TABLE IF EXISTS `hopsworks`.`feature_descriptive_statistics`;
 
 DROP TABLE IF EXISTS `hopsworks`.`job_schedule`;
