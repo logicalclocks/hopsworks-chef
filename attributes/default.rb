b9001b1eeb194df39521106e425164969ef2f08c
Victor Jouffrey <vicatjou@gmail.com>
2023-03-16 11:10:49 +0100

    Enable feature monitoring for testing

--- attributes/default.rb
+++ attributes/default.rb
@@ -490,7 +490,7 @@ default['hopsworks']['enable_bigquery_storage_connectors'] = "true"
 ##
 ## Feature flag Feature Monitoring
 ##
-default['hopsworks']['enable_feature_monitoring'] = "false"
+default['hopsworks']['enable_feature_monitoring'] = "true"
 
 # The maximum number of http threads in the thread pool are 200 by default
 default['hopsworks']['max_allowed_long_running_http_requests']                  = 50
