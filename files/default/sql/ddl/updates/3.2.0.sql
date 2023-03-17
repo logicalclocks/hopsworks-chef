4ebd3d94cf86f9349bf68241df3b92620b3ea252
Javier de la Rúa Martínez <javier@logicalclocks.com>
2023-03-17 11:43:04 +0100

    [FSTORE-742] Add row_percentage and feature type columns (#903)

--- files/default/sql/ddl/updates/3.2.0.sql
+++ files/default/sql/ddl/updates/3.2.0.sql
@@ -247,6 +247,7 @@ CREATE TABLE IF NOT EXISTS `feature_monitoring_result` (
 
 CREATE TABLE IF NOT EXISTS `feature_descriptive_statistics` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
+    `feature_type` varchar(20) NOT NULL,
     `min` FLOAT NULL, 
     `max` FLOAT NULL,   
     `sum` FLOAT NULL,
@@ -266,6 +267,7 @@ CREATE TABLE IF NOT EXISTS `feature_descriptive_statistics` (
     -- for filtering
     `start_time` TIMESTAMP NULL,
     `end_time` TIMESTAMP NULL,
+    `row_percentage` INT(11) NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
 
