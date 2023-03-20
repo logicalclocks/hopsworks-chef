5a9b83f9ea63fcac5655a545a18175e5e5a9e477
Fabio Buso <buso.fabio@gmail.com>
2023-03-20 23:37:52 +0100

    [HWORKS-457] Create users_groups view outside flyway (#900)

--- recipes/default.rb
+++ recipes/default.rb
@@ -360,6 +360,14 @@ for version in versions do
   end
 end
 
+# Create the users_groups view if it doesn't exists (see HWORKS-457 for explanation)
+bash "create users_groups view" do
+  user "root"
+  code <<-EOH
+    #{node['ndb']['scripts_dir']}/mysql-client.sh hopsworks -e "CREATE OR REPLACE VIEW users_groups AS select u.username AS username, u.password AS password, u.secret AS secret, u.email AS email,g.group_name AS group_name from ((user_group ug join users u on((u.uid = ug.uid))) join bbc_group g on((g.gid = ug.gid)));"
+  EOH
+end
+
 # Check if Kafka is to be installed and create user with grants
 begin
   valid_recipe("kkafka", "default")
