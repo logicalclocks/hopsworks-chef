eb498ddded77d608e931bf0d94fb60ed9071c756
Victor Jouffrey <vicatjou@gmail.com>
2023-04-19 10:30:09 +0200

    Fix rebase issues

--- files/default/sql/ddl/updates/undo/3.3.0__undo.sql
+++
@@ -1,8 +0,0 @@
--- revert feature monitoring changes
-DROP TABLE IF EXISTS `hopsworks`.`feature_monitoring_result`;
-DROP TABLE IF EXISTS `hopsworks`.`feature_descriptive_statistics`;
-DROP TABLE IF EXISTS `hopsworks`.`feature_monitoring_config`;
-DROP TABLE IF EXISTS `hopsworks`.`monitoring_window_config`;
-DROP TABLE IF EXISTS `hopsworks`.`statistics_comparison_config`;
-DROP TABLE IF EXISTS `hopsworks`.`job_schedule`;
-DROP TABLE IF EXISTS `hopsworks`.`feature_view_alert`;
