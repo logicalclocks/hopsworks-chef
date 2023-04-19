0f66a19269233e1e29232da25feff91f46400b35
Victor Jouffrey <vicatjou@gmail.com>
2023-04-19 10:11:51 +0200

    Move from 3.2 to 3.3

--- metadata.rb
+++ metadata.rb
@@ -1004,6 +1004,7 @@ attribute "hopsworks/max_allowed_long_running_http_requests",
 
 attribute "hopsworks/enable_flyingduck",
           :description => "Whether to enable flyingduck or not. Default: false",
+          :type => 'string'
           
 # Feature monitoring feature flag
 attribute "hopsworks/enable_feature_monitoring",
