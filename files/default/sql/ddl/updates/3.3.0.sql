04b16584e363052aed2b8c1854927b539bc0bfef
Gibson Chikafa <gibbschikafa@gmail.com>
2023-04-20 08:34:25 +0200

    Remove inode foreign key from feature_store_code

--- files/default/sql/ddl/updates/3.3.0.sql
+++ files/default/sql/ddl/updates/3.3.0.sql
@@ -25,3 +25,20 @@ DROP COLUMN `inode_pid`,
     DROP COLUMN `partition_id`;
 
 ALTER TABLE `hopsworks`.`git_repositories` ADD UNIQUE KEY `repository_path_constraint_unique` (`path`);
+
+-- HWORKS-515: Remove inode foreign key from feature_store_code
+ALTER TABLE `hopsworks`.`feature_store_code` ADD COLUMN `path` VARCHAR(1000) COLLATE latin1_general_cs NOT NULL;
+
+-- Migration
+SET SQL_SAFE_UPDATES = 0;
+UPDATE
+    `hopsworks`.`feature_store_code`
+SET
+    path = path_resolver_fn(`inode_pid`, `inode_name`);
+SET SQL_SAFE_UPDATES = 1;
+
+ALTER TABLE `hopsworks`.`feature_store_code` DROP FOREIGN KEY `inode_fk_fsc`;
+ALTER TABLE `hopsworks`.`feature_store_code`
+DROP COLUMN `inode_pid`,
+    DROP COLUMN `inode_name`,
+    DROP COLUMN `partition_id`;
