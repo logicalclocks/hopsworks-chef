7e62d3207edfb593934ed7084ff579ba29c2870e
Mahmoud Ismail <mahmoud@hopsworks.ai>
2023-04-24 15:27:27 +0200

    [CLOUD-568] Add path_resolver to 3.3 migrations

--- files/default/sql/ddl/updates/3.3.0.sql
+++ files/default/sql/ddl/updates/3.3.0.sql
@@ -1,3 +1,50 @@
+DROP PROCEDURE IF EXISTS `path_resolver`;
+DROP FUNCTION IF EXISTS `path_resolver_fn`;
+
+DELIMITER //
+CREATE PROCEDURE path_resolver(IN `parent_id` BIGINT, 
+                               OUT `inode_path` VARCHAR(255))
+BEGIN
+  DECLARE next_parent_id BIGINT;
+  DECLARE inode_name VARCHAR(255);
+  DECLARE parent_path VARCHAR(255);
+
+  IF `parent_id` = 1 THEN
+    -- We are at the root of the file system
+    SET `inode_path` =  "/";
+  ELSE
+
+    -- Not the root, we need to traverse more.
+    -- Get the information about the current inode
+    SELECT `h`.`parent_id`, `h`.`name`
+    INTO next_parent_id, inode_name
+    FROM `hops`.`hdfs_inodes` `h`
+    WHERE `h`.`id` = `parent_id`;
+
+    -- Recursively traverse upstream
+    CALL path_resolver(next_parent_id, parent_path);
+
+    -- Assemble the path
+    SET `inode_path` = CONCAT(parent_path, inode_name, "/");
+  END IF;
+END //
+
+
+-- MySQL does not support recursive functions. 
+-- Wrap the procedure above into a function to make the rest of the migrations easier
+CREATE FUNCTION path_resolver_fn(`parent_id` BIGINT, `name` VARCHAR(255))
+RETURNS VARCHAR(255)
+DETERMINISTIC
+BEGIN
+  DECLARE parent_path VARCHAR(255);
+
+  CALL path_resolver(`parent_id`, parent_path);
+
+  RETURN CONCAT(parent_path, `name`);
+END //
+
+DELIMITER ;
+
 -- HWORKS-480: Remove inode foreign key from git repositories
 ALTER TABLE `hopsworks`.`git_repositories` ADD COLUMN `name` VARCHAR(255) COLLATE latin1_general_cs NOT NULL;
 ALTER TABLE `hopsworks`.`git_repositories` ADD COLUMN `path` VARCHAR(1000) COLLATE latin1_general_cs NOT NULL;
