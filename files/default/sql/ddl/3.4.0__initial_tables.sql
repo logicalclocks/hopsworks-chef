059b11ec8134928e621b8f2ed516c46cd7b0dc64
Victor Jouffrey <37411285+vatj@users.noreply.github.com>
2023-05-03 01:32:36 +0200

    [FSTORE-771] ux python proposal (#929)
    * Feature flag feature monitoring (#893)
    
    * Move from 3.2 to 3.4
    
    * [FSTORE-626] Draft tables for persistence (#878)
    
    * Second draft
    
    * Add table for persistence of the feature monitoring config and result
    
    * Edit based on Review
    
    * Fix name change
    
    * Code review edit
    
    * Allow schedule config to be null, for manual trigger only use case
    
    * Change one to one mapping
    
    * Feature flag feature monitoring (#893)
    
    * [FSTORE-674][FSTORE-675] feature monitoring alert changes to feature_group_alert (#912)
    
    * feature monitoring alert changes to feature_group_alert
    
    * add default for entity type
    
    * fix-comments-refactoring
    
    * refactor feature_group_alert to feature_store_alert
    
    * changes to create new feature_view_alert table and revert previous changes to feature_group_alert table
    
    * changes to create new feature_view_alert table and revert previous changes to feature_group_alert table
    
    * Move from 3.2 to 3.4
    
    * Fix rebase issues
    
    * remove alert_config field
    
    ---------
    
    Co-authored-by: Dhananjay Mukhedkar <55157590+DhananjayMukhedkar@users.noreply.github.com>

--- files/default/sql/ddl/3.4.0__initial_tables.sql
+++ files/default/sql/ddl/3.4.0__initial_tables.sql
@@ -2238,10 +2238,10 @@ CREATE TABLE `serving_key` (
 CREATE TABLE IF NOT EXISTS `monitoring_window_config` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `window_config_type` INT(11) NOT NULL,
-    `specific_id` INT(11),
+    `training_dataset_id` INT(11),
     `time_offset` VARCHAR(63),
     `window_length` VARCHAR(63),
-    `row_percentage` INT(11),
+    `row_percentage` FLOAT,
     `specific_value` FLOAT,
     PRIMARY KEY (`id`)
 ) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
@@ -2251,21 +2251,31 @@ CREATE TABLE IF NOT EXISTS `statistics_comparison_config` (
     `strict` BOOLEAN DEFAULT FALSE,
     `relative` BOOLEAN DEFAULT FALSE,
     `threshold` FLOAT,
-    `compare_on` INT(11) NOT NULL,
+    `metric` INT(11) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `job_schedule` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `job_id` int NOT NULL,
+    `start_datetime` timestamp,
+    `enabled` BOOLEAN NOT NULL,
+    `job_frequency` varchar(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `job_id` (`job_id`),
+    CONSTRAINT `fk_schedule_job` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`) ON DELETE CASCADE
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `feature_monitoring_config` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `feature_group_id` INT(11),
     `feature_view_id` INT(11),
-    `feature_name` VARCHAR(63) COLLATE latin1_general_cs NOT NULL,
+    `feature_name` VARCHAR(63) COLLATE latin1_general_cs DEFAULT NULL,
     `description` VARCHAR(2000) COLLATE latin1_general_cs DEFAULT NULL,
     `name` VARCHAR(255) COLLATE latin1_general_cs NOT NULL,
     `enabled` BOOLEAN DEFAULT TRUE,
     `feature_monitoring_type` tinyint(4) NOT NULL,
-    `alert_config` VARCHAR(63) COLLATE latin1_general_cs, -- dummy this should become ref to another table
-    `scheduler_config` VARCHAR(63) COLLATE latin1_general_cs, -- dummy this should become ref to another table
+    `scheduler_config_id` INT(11),
     `job_id` INT(11) NOT NULL,
     `detection_window_config_id` INT(11),
     `reference_window_config_id` INT(11),
@@ -2279,12 +2289,14 @@ CREATE TABLE IF NOT EXISTS `feature_monitoring_config` (
     CONSTRAINT `job_monitoring_config_fk` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
     CONSTRAINT `detection_window_config_monitoring_config_fk` FOREIGN KEY (`detection_window_config_id`) REFERENCES `monitoring_window_config` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
     CONSTRAINT `reference_window_config_monitoring_config_fk` FOREIGN KEY (`reference_window_config_id`) REFERENCES `monitoring_window_config` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-    CONSTRAINT `statistics_comparison_config_monitoring_config_fk` FOREIGN KEY (`statistics_comparison_config_id`) REFERENCES `statistics_comparison_config` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    CONSTRAINT `statistics_comparison_config_monitoring_config_fk` FOREIGN KEY (`statistics_comparison_config_id`) REFERENCES `statistics_comparison_config` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+    CONSTRAINT `scheduler_config_fk` FOREIGN KEY (`scheduler_config_id`) REFERENCES `job_schedule` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `feature_descriptive_statistics` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `feature_type` varchar(20) NOT NULL,
+    `feature_name` VARCHAR(63) COLLATE latin1_general_cs NOT NULL,
     -- for any feature type
     `count` BIGINT NOT NULL,
     `completeness` FLOAT NULL,
@@ -2306,18 +2318,19 @@ CREATE TABLE IF NOT EXISTS `feature_descriptive_statistics` (
     -- for filtering
     `start_time` TIMESTAMP NULL,    -- commit time
     `end_time` TIMESTAMP NOT NULL,  -- commit time
-    `row_percentage` INT(11) NOT NULL,
+    `row_percentage` FLOAT NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `feature_monitoring_result` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `feature_monitoring_config_id` INT(11) NOT NULL,
-    `execution_id` INT(11) NOT NULL,  -- dummy this should become ref to another table
+    `feature_name` VARCHAR(63) COLLATE latin1_general_cs NOT NULL,
+    `execution_id` INT(11) NOT NULL,
     `monitoring_time` timestamp DEFAULT CURRENT_TIMESTAMP,
     `shift_detected` BOOLEAN DEFAULT FALSE,
-    `detection_stats_id` INT(11) NOT NULL, -- dummy this should become ref to another table
-    `reference_stats_id` INT(11), -- dummy this should become ref to another table
+    `detection_stats_id` INT(11) NOT NULL,
+    `reference_stats_id` INT(11),
     `difference` FLOAT,
     PRIMARY KEY (`id`),
     CONSTRAINT `config_monitoring_result_fk` FOREIGN KEY (`feature_monitoring_config_id`) REFERENCES `feature_monitoring_config` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
@@ -2325,34 +2338,17 @@ CREATE TABLE IF NOT EXISTS `feature_monitoring_result` (
     CONSTRAINT `reference_stats_monitoring_result_fk` FOREIGN KEY (`reference_stats_id`) REFERENCES `feature_descriptive_statistics` (`id`) ON DELETE NO ACTION
 ) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
 
-CREATE TABLE IF NOT EXISTS `job_schedule` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `job_id` int NOT NULL,
-    `start_datetime` timestamp,
-    `enable` BOOLEAN NOT NULL,
-    `job_frequency` varchar(20) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `job_id` (`job_id`),
-    CONSTRAINT `fk_schedule_job` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`) ON DELETE CASCADE
-) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
 -- alert changes for feature monitoring
 CREATE TABLE IF NOT EXISTS `hopsworks`.`feature_view_alert` (
-                                                `id` int AUTO_INCREMENT
-                                                    PRIMARY KEY,
-                                                `status` varchar(45) NOT NULL,
-                                                `type` varchar(45) NOT NULL,
-                                                `severity` varchar(45) NOT NULL,
-                                                `receiver` int NOT NULL,
-                                                `created` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                                                `feature_view_id` int NOT NULL,
-                                                CONSTRAINT `unique_feature_view_status`
-                                                    UNIQUE (`feature_view_id`, `status`),
-                                                CONSTRAINT `fk_fv_alert_1`
-                                                    FOREIGN KEY (`receiver`) REFERENCES `hopsworks`.`alert_receiver` (`id`)
-                                                        ON DELETE CASCADE,
-                                                CONSTRAINT `fk_fv_alert_2`
-                                                    FOREIGN KEY (`feature_view_id`) REFERENCES `hopsworks`.`feature_view` (`id`)
-                                                        ON DELETE CASCADE
+    `id` int AUTO_INCREMENT PRIMARY KEY,
+    `status` varchar(45) NOT NULL,
+    `type` varchar(45) NOT NULL,
+    `severity` varchar(45) NOT NULL,
+    `receiver` int NOT NULL,
+    `created` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `feature_view_id` int NOT NULL,
+    CONSTRAINT `unique_feature_view_status` UNIQUE (`feature_view_id`, `status`),
+    CONSTRAINT `fk_fv_alert_1` FOREIGN KEY (`receiver`) REFERENCES `hopsworks`.`alert_receiver` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_fv_alert_2` FOREIGN KEY (`feature_view_id`) REFERENCES `hopsworks`.`feature_view` (`id`) ON DELETE CASCADE
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 -- end alert changes for feature monitoring
--- metadata.rb
+++ metadata.rb
@@ -1016,6 +1016,15 @@ attribute "hopsworks/max_allowed_long_running_http_requests",
 
 attribute "hopsworks/enable_flyingduck",
           :description => "Whether to enable flyingduck or not. Default: false",
+          
+# Feature monitoring feature flag
+attribute "hopsworks/enable_feature_monitoring",
+          :description => "Whether to enable feature monitoring with scheduling and alerting",
+          :type => 'string'
+          
+# Feature monitoring feature flag
+attribute "hopsworks/enable_feature_monitoring",
+          :description => "Whether to enable feature monitoring with scheduling and alerting",
           :type => 'string'
           
 # Feature monitoring feature flag
