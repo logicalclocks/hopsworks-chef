7aadb2aa266c8c964102eecc2285a508b30f8c5e
Alex Ormenisan <aaor@kth.se>
2023-05-29 11:41:25 +0200

    [HWORKS-544] Opensearch docs creation moved to Hopsworks

--- files/default/sql/ddl/3.4.0__initial_tables.sql
+++ files/default/sql/ddl/3.4.0__initial_tables.sql
@@ -2233,3 +2233,20 @@ CREATE TABLE `serving_key` (
                                CONSTRAINT `feature_view_serving_key_fk` FOREIGN KEY (`feature_view_id`) REFERENCES `feature_view` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
                                CONSTRAINT `feature_group_serving_key_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE `command_search` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `inode_id` bigint NOT NULL,
+  `project_id` int,
+  `op` VARCHAR(20) NOT NULL,
+  `status` VARCHAR(20) NOT NULL,
+  `feature_group_id` int(11),
+  `feature_view_id` int(11),
+  `training_dataset_id` int(11),
+  `error_message` varchar(10000),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_command_search_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `fk_command_search_feature_group` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `fk_command_search_feature_view` FOREIGN KEY (`feature_view_id`) REFERENCES `feature_view` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `fk_command_search_training_dataset` FOREIGN KEY (`training_dataset_id`) REFERENCES `training_dataset` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
--- files/default/sql/ddl/updates/3.4.0.sql
+++ files/default/sql/ddl/updates/3.4.0.sql
@@ -21,3 +21,20 @@ CREATE TABLE `serving_key` (
 -- HWORKS-351: Add support to run generic docker commands
 ALTER TABLE `hopsworks`.`conda_commands` MODIFY COLUMN `arg` VARCHAR(11000) DEFAULT NULL;
 ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN `custom_commands_file` VARCHAR(255) DEFAULT NULL;
+
+CREATE TABLE `command_search` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `inode_id` bigint NOT NULL,
+  `project_id` int,
+  `op` VARCHAR(20) NOT NULL,
+  `status` VARCHAR(20) NOT NULL,
+  `feature_group_id` int(11),
+  `feature_view_id` int(11),
+  `training_dataset_id` int(11),
+  `error_message` varchar(10000),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_command_search_project` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `fk_command_search_feature_group` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `fk_command_search_feature_view` FOREIGN KEY (`feature_view_id`) REFERENCES `feature_view` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `fk_command_search_training_dataset` FOREIGN KEY (`training_dataset_id`) REFERENCES `training_dataset` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
