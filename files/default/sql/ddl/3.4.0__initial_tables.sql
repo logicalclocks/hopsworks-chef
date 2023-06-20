067e2d994981d1dd63eedcb6f5987417aa5f3d55
kenneth <kenneth@logicalclocks.com>
2023-06-20 16:50:40 +0200

    add serving key

--- files/default/sql/ddl/3.4.0__initial_tables.sql
+++ files/default/sql/ddl/3.4.0__initial_tables.sql
@@ -2215,3 +2215,20 @@ CREATE TABLE IF NOT EXISTS `hdfs_command_execution` (
   KEY `fk_hdfs_file_command_1_idx` (`execution_id`),
   CONSTRAINT `fk_hdfs_file_command_1` FOREIGN KEY (`execution_id`) REFERENCES `executions` (`id`) ON DELETE CASCADE
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+-- FSTORE-921
+CREATE TABLE `serving_key` (
+                               `id` int(11) NOT NULL AUTO_INCREMENT,
+                               `prefix` VARCHAR(63) NULL DEFAULT '',
+                               `feature_name` VARCHAR(1000) NOT NULL,
+                               `join_on` VARCHAR(1000) NULL,
+                               `join_index` int(11) NOT NULL,
+                               `feature_group_id` INT(11) NOT NULL,
+                               `required` tinyint(1) NOT NULL DEFAULT '0',
+                               `feature_view_id` INT(11) NULL,
+                               PRIMARY KEY (`id`),
+                               KEY `feature_view_id` (`feature_view_id`),
+                               KEY `feature_group_id` (`feature_group_id`),
+                               CONSTRAINT `feature_view_serving_key_fk` FOREIGN KEY (`feature_view_id`) REFERENCES `feature_view` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                               CONSTRAINT `feature_group_serving_key_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
--- files/default/sql/ddl/updates/3.4.0.sql
+++ files/default/sql/ddl/updates/3.4.0.sql
@@ -1,2 +1,19 @@
 -- FSTORE-928: When hitting limit of number of projects that one user can create, deleting a project doesn't work as expected
 ALTER TABLE `hopsworks`.`users` DROP COLUMN `num_created_projects`;
+
+-- FSTORE-921
+CREATE TABLE `serving_key` (
+                               `id` int(11) NOT NULL AUTO_INCREMENT,
+                               `prefix` VARCHAR(63) NULL DEFAULT '',
+                               `feature_name` VARCHAR(1000) NOT NULL,
+                               `join_on` VARCHAR(1000) NULL,
+                               `join_index` int(11) NOT NULL,
+                               `feature_group_id` INT(11) NOT NULL,
+                               `required` tinyint(1) NOT NULL DEFAULT '0',
+                               `feature_view_id` INT(11) NULL,
+                               PRIMARY KEY (`id`),
+                               KEY `feature_view_id` (`feature_view_id`),
+                               KEY `feature_group_id` (`feature_group_id`),
+                               CONSTRAINT `feature_view_serving_key_fk` FOREIGN KEY (`feature_view_id`) REFERENCES `feature_view` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+                               CONSTRAINT `feature_group_serving_key_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
