6be99b2e465aeea777b96621d814b9d1dd1f2e40
Ralf <bubriks@gmail.com>
2023-07-12 11:41:55 +0200

    FSTORE-928

---
+++ files/default/sql/ddl/updates/3.4.0.sql
@@ -0,0 +1,2 @@
+-- FSTORE-928: When hitting limit of number of projects that one user can create, deleting a project doesn't work as expected
+ALTER TABLE `hopsworks`.`users` DROP COLUMN `num_created_projects`;
