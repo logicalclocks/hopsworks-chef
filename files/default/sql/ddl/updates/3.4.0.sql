584741ebe4f34ef030e3dd1afea617ccafe7f4a3
Gibson Chikafa <gibbschikafa@gmail.com>
2023-07-21 09:55:20 +0200

    Remove if not exist

--- files/default/sql/ddl/updates/3.4.0.sql
+++ files/default/sql/ddl/updates/3.4.0.sql
@@ -1,21 +1,3 @@
--- HWORKS-626: conda environment history
-CREATE TABLE IF NOT EXISTS `environment_history` (
-                                       `id` int NOT NULL AUTO_INCREMENT,
-                                       `project` int NOT NULL,
-                                       `docker_image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-                                       `downgraded` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-                                       `installed` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-                                       `uninstalled` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-                                       `upgraded` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-                                       `previous_docker_image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-                                       `user` int NOT NULL,
-                                       `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                       PRIMARY KEY (`id`),
-                                       KEY `project` (`project`),
-                                       KEY `docker_image` (`docker_image`),
-                                           CONSTRAINT `env_project_fk` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE CASCADE
-) ENGINE=ndbcluster AUTO_INCREMENT=69 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs
-
 -- FSTORE-928: When hitting limit of number of projects that one user can create, deleting a project doesn't work as expected
 ALTER TABLE `hopsworks`.`users` DROP COLUMN `num_created_projects`;
 
@@ -39,3 +21,21 @@ CREATE TABLE `serving_key` (
 -- HWORKS-351: Add support to run generic docker commands
 ALTER TABLE `hopsworks`.`conda_commands` MODIFY COLUMN `arg` VARCHAR(11000) DEFAULT NULL;
 ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN `custom_commands_file` VARCHAR(255) DEFAULT NULL;
+
+-- HWORKS-626: conda environment history
+CREATE TABLE `environment_history` (
+                                       `id` int NOT NULL AUTO_INCREMENT,
+                                       `project` int NOT NULL,
+                                       `docker_image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+                                       `downgraded` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                                       `installed` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                                       `uninstalled` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                                       `upgraded` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                                       `previous_docker_image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+                                       `user` int NOT NULL,
+                                       `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                       PRIMARY KEY (`id`),
+                                       KEY `project` (`project`),
+                                       KEY `docker_image` (`docker_image`),
+                                       CONSTRAINT `env_project_fk` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE CASCADE
+) ENGINE=ndbcluster AUTO_INCREMENT=69 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs
