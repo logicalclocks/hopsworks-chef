ecd88a48f90b3371ceb99760d5331d7880f8a1c7
Gibson Chikafa <gibbschikafa@gmail.com>
2023-07-22 15:41:14 +0200

    docker_image should be unique

--- files/default/sql/ddl/3.4.0__initial_tables.sql
+++ files/default/sql/ddl/3.4.0__initial_tables.sql
@@ -149,7 +149,7 @@ CREATE TABLE `environment_history` (
                            `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
                            PRIMARY KEY (`id`),
                            KEY `env_project_fk` (`project`),
-                           KEY `docker_image` (`docker_image`),
+                           UNIQUE KEY `docker_image_constraint_unique` (`docker_image`),
                            CONSTRAINT `env_project_fk` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE CASCADE
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs
 /*!40101 SET character_set_client = @saved_cs_client */;
