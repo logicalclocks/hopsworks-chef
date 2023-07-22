dc33b58d7b2e12fd2bf5a7fe84be3b9742129ea9
Gibson Chikafa <gibbschikafa@gmail.com>
2023-07-22 11:51:25 +0200

    Test

--- files/default/sql/ddl/updates/3.4.0.sql
+++ files/default/sql/ddl/updates/3.4.0.sql
@@ -24,18 +24,18 @@ ALTER TABLE `hopsworks`.`conda_commands` ADD COLUMN `custom_commands_file` VARCH
 
 -- HWORKS-626: conda environment history
 CREATE TABLE `environment_history` (
-                                       `id` int NOT NULL AUTO_INCREMENT,
-                                       `project` int NOT NULL,
-                                       `docker_image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-                                       `downgraded` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-                                       `installed` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-                                       `uninstalled` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-                                       `upgraded` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-                                       `previous_docker_image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-                                       `user` int NOT NULL,
-                                       `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                       PRIMARY KEY (`id`),
-                                       KEY `project` (`project`),
-                                       KEY `docker_image` (`docker_image`),
-                                       CONSTRAINT `env_project_fk` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE CASCADE
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `project` int NOT NULL,
+                           `docker_image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+                           `downgraded` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                           `installed` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                           `uninstalled` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                           `upgraded` varchar(7000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                           `previous_docker_image` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+                           `user` int NOT NULL,
+                           `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           PRIMARY KEY (`id`),
+                           KEY `project` (`project`),
+                           KEY `docker_image` (`docker_image`),
+                           CONSTRAINT `env_project_fk` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE CASCADE
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs
