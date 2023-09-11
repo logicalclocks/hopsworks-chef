1a77eac199c21d3e6f9b65005da829274c03a2e0
bubriks <bubriks@gmail.com>
2023-09-11 17:47:26 +0200

    Update 3.4.0.sql

--- files/default/sql/ddl/updates/3.4.0.sql
+++ files/default/sql/ddl/updates/3.4.0.sql
@@ -158,6 +158,11 @@ ALTER TABLE `hopsworks`.`feature_group` ADD COLUMN `topic_name` VARCHAR(255) DEF
 ALTER TABLE `hopsworks`.`project` ADD COLUMN `topic_name` VARCHAR(255) DEFAULT NULL;
 
 SET SQL_SAFE_UPDATES = 0;
+-- deletes unused subjects (fg was deleted)
+DELETE
+FROM `hopsworks`.`subjects`
+WHERE REGEXP_REPLACE(`subject`, "^([0-9]+)_([0-9]+)_(.+)_([0-9]+)(_onlinefs|$)",'$2') NOT IN (SELECT `feature_group`.`id` FROM `feature_group`);
+
 UPDATE `hopsworks`.`subjects`
 SET `subject` = REGEXP_REPLACE(`subject`, "^([0-9]+)_([0-9]+)_(.+)_([0-9]+)(_onlinefs|$)",'$3_$4')
 WHERE REGEXP_SUBSTR(`subject`, "^([0-9]+)_([0-9]+)_(.+)_([0-9]+)(_onlinefs|$)");
