71c68a68dc675b79b16fdc9ca56cc28db404276b
Gibson Chikafa <gibbschikafa@gmail.com>
2023-11-16 11:20:30 +0200

    Updates

--- files/default/sql/ddl/updates/3.4.0.sql
+++ files/default/sql/ddl/updates/3.4.0.sql
@@ -185,4 +185,49 @@ SET `subject_id` = NULL
 
 SET SQL_SAFE_UPDATES = 1;
 
+
+-- Rstudio updates
+ALTER TABLE `hopsworks`.`rstudio_project` DROP COLUMN `host_ip`,
+    DROP COLUMN `token`;
+
+ALTER TABLE `hopsworks`.`rstudio_project` ADD COLUMN `login_password` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL;
+ALTER TABLE `hopsworks`.`rstudio_project` ADD COLUMN `login_username` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL;
+
+ALTER TABLE `hopsworks`.`rstudio_project` MODIFY COLUMN `pid` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL;
+
+ALTER TABLE `hopsworks`.`rstudio_settings`
+    DROP COLUMN num_tf_ps, DROP COLUMN num_tf_gpus,
+    DROP COLUMN num_mpi_np, DROP COLUMN appmaster_cores,
+    DROP COLUMN appmaster_memory, DROP COLUMN num_executors,
+    DROP COLUMN num_executor_cores, DROP COLUMN executor_memory,
+    DROP COLUMN dynamic_initial_executors, DROP COLUMN dynamic_min_executors,
+    DROP COLUMN dynamic_max_executors, DROP COLUMN log_level,
+    DROP COLUMN mode, DROP COLUMN umask,
+    DROP COLUMN archives, DROP COLUMN jars,
+    DROP COLUMN files, DROP COLUMN py_files,
+    DROP COLUMN spark_params;
+
+ALTER TABLE `hopsworks`.`rstudio_settings` ADD COLUMN  `base_dir` varchar(255) COLLATE latin1_general_cs DEFAULT NULL;
+ALTER TABLE `hopsworks`.`rstudio_settings` ADD COLUMN  `job_config` varchar(11000) COLLATE latin1_general_cs DEFAULT NULL;
+ALTER TABLE `hopsworks`.`rstudio_settings` ADD COLUMN  `docker_config` varchar(1000) COLLATE latin1_general_cs DEFAULT NULL;
+
 ALTER TABLE `hopsworks`.`project` ADD COLUMN `rstudio_docker_image` VARCHAR(255) COLLATE latin1_general_cs DEFAULT NULL;
+
+CREATE TABLE `rstudio_environment_build` (
+                                             `id` int NOT NULL AUTO_INCREMENT,
+                                             `build_script` varchar(1000) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+                                             `user` int NOT NULL,
+                                             `project` int NOT NULL,
+                                             `build_start` bigint DEFAULT NULL,
+                                             `build_finish` bigint DEFAULT NULL,
+                                             `build_result` varchar(128) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+                                             `secret` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+                                             `logFile` varchar(1000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                                             `build_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                                             `description` varchar(1000) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
+                                             PRIMARY KEY (`id`),
+                                             KEY `user_fk` (`user`),
+                                             KEY `rstudio_env_build_project_fk` (`project`),
+                                             CONSTRAINT `rstudio_env_build_project_fk` FOREIGN KEY (`project`) REFERENCES `project` (`id`) ON DELETE CASCADE,
+                                             CONSTRAINT `rstudio_env_build_usr_fkc` FOREIGN KEY (`user`) REFERENCES `users` (`uid`) ON DELETE CASCADE
+) ENGINE=ndbcluster AUTO_INCREMENT=5154 DEFAULT CHARSET=latin1
