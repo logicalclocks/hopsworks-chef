ca904685bcee26ea35a09045a173820fa1015671
kennethmhc <kennethmhc@users.noreply.github.com>
2023-12-04 18:16:56 +0100

    [APPEND][FSTORE-1047] Fix hopsworks opensearch setting (#1082)
    * check opensearch setting
    
    * change empty to nil

--- attributes/default.rb
+++ attributes/default.rb
@@ -526,5 +526,5 @@ default['judge']['etc']       = "#{node['judge']['home']}/etc"
 default['judge']['logs']      = "#{node['judge']['home']}/logs"
 
 # Opensearch embedding db
-node['hopsworks']['opensearch']['default_embedding_index']     = ""
-node['hopsworks']['opensearch']['num_default_embedding_index'] = 1
+default['hopsworks']['opensearch']['default_embedding_index']     = node['hopsworks']['opensearch'].nil || node['hopsworks']['opensearch']['default_embedding_index'].nil ? "" : node['hopsworks']['opensearch']['default_embedding_index']
+default['hopsworks']['opensearch']['num_default_embedding_index'] = node['hopsworks']['opensearch'].nil || node['hopsworks']['opensearch']['num_default_embedding_index'].nil ? 1 : node['hopsworks']['opensearch']['num_default_embedding_index']
