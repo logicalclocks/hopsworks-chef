47a6604fc15105aebe4c875941d25bcd30c96b03
Robin Andersson <robin@hopsworks.ai>
2023-12-04 15:27:14 +0100

    [HWORKS-135] Models backend should store metadata in tables instead of opensearch

--- files/default/sql/ddl/updates/3.7.0.sql
+++ files/default/sql/ddl/updates/3.7.0.sql
@@ -19,3 +19,29 @@ CREATE TABLE IF NOT EXISTS `embedding_feature` (
     KEY `embedding_id` (`embedding_id`),
     CONSTRAINT `embedding_feature_fk` FOREIGN KEY (`embedding_id`) REFERENCES `embedding` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
     ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `model` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  UNIQUE KEY `project_unique_name` (`name`, `project_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `model_project_fk` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `model_version` (
+  `model_id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `created` timestamp DEFAULT NULL,
+  `description` VARCHAR(1000) DEFAULT NULL,
+  `metrics` VARCHAR(2000) DEFAULT NULL,
+  `program` VARCHAR(1000) DEFAULT NULL,
+  `framework` VARCHAR(128) DEFAULT NULL,
+  `environment` VARCHAR(1000) DEFAULT NULL,
+  `experiment_id` VARCHAR(128) DEFAULT NULL,
+  `experiment_project_name` VARCHAR(100) DEFAULT NULL,
+  PRIMARY KEY (`model_id`, `version`),
+  CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `model_fk` FOREIGN KEY (`model_id`) REFERENCES `model` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
