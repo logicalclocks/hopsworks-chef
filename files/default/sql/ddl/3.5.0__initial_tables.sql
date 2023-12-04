7707f489b86f6aea203f22f02bee24bb85561865
kennethmhc <kennethmhc@users.noreply.github.com>
2023-12-04 10:29:18 +0100

    [FSTORE-1047] Support Similarity Search in the Feature Store (#1068)
    * add table
    
    * add table
    
    * add setting
    
    * metadata

--- files/default/sql/ddl/3.5.0__initial_tables.sql
+++ files/default/sql/ddl/3.5.0__initial_tables.sql
@@ -2326,3 +2326,25 @@ CREATE TABLE `feature_store_keyword` (
   CONSTRAINT `fk_fs_keyword_fv` FOREIGN KEY (`feature_view_id`) REFERENCES `feature_view` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `fk_fs_keyword_td` FOREIGN KEY (`training_dataset_id`) REFERENCES `training_dataset` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+-- FSTORE-1047
+CREATE TABLE IF NOT EXISTS `embedding` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `feature_group_id` int(11) NOT NULL,
+    `col_prefix` varchar(255) NULL,
+    `vector_db_index_name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `feature_group_id` (`feature_group_id`),
+    CONSTRAINT `feature_group_embedding_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `embedding_feature` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `embedding_id` int(11) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `dimension` int NOT NULL,
+    `similarity_function_type` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `embedding_id` (`embedding_id`),
+    CONSTRAINT `embedding_feature_fk` FOREIGN KEY (`embedding_id`) REFERENCES `embedding` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+    ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
