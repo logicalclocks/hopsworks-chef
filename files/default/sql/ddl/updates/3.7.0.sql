33f16a5aeefbff71ddbccf447de305e760fd183f
kenneth <kenneth@logicalclocks.com>
2023-12-14 16:24:55 +0100

    add consumer group to kafka offset

--- files/default/sql/ddl/updates/3.7.0.sql
+++ files/default/sql/ddl/updates/3.7.0.sql
@@ -19,3 +19,50 @@ CREATE TABLE IF NOT EXISTS `embedding_feature` (
     KEY `embedding_id` (`embedding_id`),
     CONSTRAINT `embedding_feature_fk` FOREIGN KEY (`embedding_id`) REFERENCES `embedding` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
     ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+-- FSTORE-1119
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS add_group_column_to_offset_tables//
+
+CREATE PROCEDURE add_group_column_to_offset_tables()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE db_name VARCHAR(255);
+
+    DECLARE db_cursor CURSOR FOR
+SELECT name FROM `hopsworks`.`feature_store`;
+
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+OPEN db_cursor;
+
+read_loop: LOOP
+        FETCH db_cursor INTO db_name;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        SET @alter_query := CONCAT(
+            'ALTER TABLE `', db_name, '`.`kafka_offsets` ADD COLUMN `consumer_group` VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL DEFAULT ''RONDB'''
+        );
+
+        SET @exists := (
+            SELECT COUNT(*)
+            FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_SCHEMA = db_name AND TABLE_NAME = 'kafka_offsets'
+        );
+
+        IF @exists > 0 THEN
+            PREPARE stmt FROM @alter_query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+        END IF;
+END LOOP;
+
+CLOSE db_cursor;
+END //
+
+DELIMITER ;
+
+CALL add_group_column_to_offset_tables();
