9325e264530fb6f20a47365371194b83b0f0c3e7
Robin Andersson <robin.eric.andersson@gmail.com>
2023-12-18 13:53:16 +0100

    [HWORKS-135] Models backend should store metadata in tables instead of opensearch (#1023)

--- files/default/sql/ddl/updates/3.7.0.sql
+++ files/default/sql/ddl/updates/3.7.0.sql
@@ -1,5 +1,5 @@
 -- FSTORE-1047
-CREATE TABLE IF NOT EXISTS `embedding` (
+CREATE TABLE IF NOT EXISTS `hopsworks`.`embedding` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `feature_group_id` int(11) NOT NULL,
     `col_prefix` varchar(255) NULL,
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `embedding` (
     CONSTRAINT `feature_group_embedding_fk` FOREIGN KEY (`feature_group_id`) REFERENCES `feature_group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
     ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-CREATE TABLE IF NOT EXISTS `embedding_feature` (
+CREATE TABLE IF NOT EXISTS `hopsworks`.`embedding_feature` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `embedding_id` int(11) NOT NULL,
     `name` varchar(255) NOT NULL,
@@ -19,3 +19,29 @@ CREATE TABLE IF NOT EXISTS `embedding_feature` (
     KEY `embedding_id` (`embedding_id`),
     CONSTRAINT `embedding_feature_fk` FOREIGN KEY (`embedding_id`) REFERENCES `embedding` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
     ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `hopsworks`.`model` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  UNIQUE KEY `project_unique_name` (`name`, `project_id`),
+  PRIMARY KEY (`id`),
+  CONSTRAINT `model_project_fk` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+CREATE TABLE IF NOT EXISTS `hopsworks`.`model_version` (
+  `model_id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `description` VARCHAR(1000) DEFAULT NULL,
+  `metrics` VARCHAR(3000) DEFAULT NULL,
+  `program` VARCHAR(1000) DEFAULT NULL,
+  `framework` VARCHAR(128) DEFAULT NULL,
+  `environment` VARCHAR(1000) DEFAULT NULL,
+  `experiment_id` VARCHAR(128) DEFAULT NULL,
+  `experiment_project_name` VARCHAR(128) DEFAULT NULL,
+  PRIMARY KEY (`model_id`, `version`),
+  CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `model_fk` FOREIGN KEY (`model_id`) REFERENCES `model` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
