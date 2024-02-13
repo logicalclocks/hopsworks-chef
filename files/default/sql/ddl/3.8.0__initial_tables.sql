8f5ce1dc0f13c209881eead82f428c9a91d85eaf
Alex Ormenisan <ormenisan.adrian@gmail.com>
2024-02-13 09:44:49 +0100

    [HWORKS-987] model version id should be monotonically increasing (#1103)

--- files/default/sql/ddl/3.8.0__initial_tables.sql
+++ files/default/sql/ddl/3.8.0__initial_tables.sql
@@ -2467,6 +2467,7 @@ CREATE TABLE IF NOT EXISTS `model` (
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `model_version` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `model_id` int(11) NOT NULL,
   `version` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -2478,7 +2479,8 @@ CREATE TABLE IF NOT EXISTS `model_version` (
   `environment` VARCHAR(1000) DEFAULT NULL,
   `experiment_id` VARCHAR(128) DEFAULT NULL,
   `experiment_project_name` VARCHAR(128) DEFAULT NULL,
-  PRIMARY KEY (`model_id`, `version`),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `model_version_key` (`model_id`, `version`),
   CONSTRAINT `user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `model_fk` FOREIGN KEY (`model_id`) REFERENCES `model` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
@@ -2500,12 +2502,11 @@ CREATE TABLE IF NOT EXISTS `embedding_feature` (
     `name` varchar(255) NOT NULL,
     `dimension` int NOT NULL,
     `similarity_function_type` varchar(255) NOT NULL,
-    `hsml_model_id` INT(11) NULL,
-    `hsml_model_version` INT(11) NULL,
+    `model_version_id` INT(11) NULL,
     PRIMARY KEY (`id`),
     KEY `embedding_id` (`embedding_id`),
     CONSTRAINT `embedding_feature_fk` FOREIGN KEY (`embedding_id`) REFERENCES `embedding` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-    CONSTRAINT `embedding_feature_model_version_fk` FOREIGN KEY (`hsml_model_id`, `hsml_model_version`) REFERENCES `model_version` (`model_id`, `version`) ON DELETE SET NULL ON UPDATE NO ACTION
+    CONSTRAINT `embedding_feature_model_version_fk` FOREIGN KEY (`model_version_id`) REFERENCES `model_version` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
     ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 -- FSTORE-612: Feature monitoring
--- files/default/sql/ddl/updates/3.8.0.sql
+++ files/default/sql/ddl/updates/3.8.0.sql
@@ -1,7 +1,11 @@
+-- HWORKS-987
+ALTER TABLE `hopsworks`.`model_version` ADD CONSTRAINT `model_version_key` UNIQUE (`model_id`,`version`);
+ALTER TABLE `hopsworks`.`model_version` DROP PRIMARY KEY;
+ALTER TABLE `hopsworks`.`model_version` ADD COLUMN id int(11) AUTO_INCREMENT PRIMARY KEY;
+
 --FSTORE-1190
 ALTER TABLE `hopsworks`.`embedding_feature`
-    ADD COLUMN `hsml_model_id` INT(11) NULL,
-    ADD COLUMN `hsml_model_version` INT(11) NULL;
+    ADD COLUMN `model_version_id` INT(11) NULL;
 
 ALTER TABLE `hopsworks`.`embedding_feature`
-    ADD CONSTRAINT `embedding_feature_model_version_fk` FOREIGN KEY (`hsml_model_id`, `hsml_model_version`) REFERENCES `model_version` (`model_id`, `version`) ON DELETE SET NULL ON UPDATE NO ACTION;
+    ADD CONSTRAINT `embedding_feature_model_version_fk` FOREIGN KEY (`model_version_id`) REFERENCES `model_version` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
