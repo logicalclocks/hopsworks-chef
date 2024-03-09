b1abe33455c4cea88b2a3f5e7f4709f0b5a0e110
Robin Andersson <robin@hopsworks.ai>
2024-03-09 15:23:43 +0100

    [FSTORE-1096][APPEND] Fix incorrect sql comment

--- files/default/sql/ddl/updates/3.8.0.sql
+++ files/default/sql/ddl/updates/3.8.0.sql
@@ -3,7 +3,7 @@ ALTER TABLE `hopsworks`.`model_version` ADD CONSTRAINT `model_version_key` UNIQU
 ALTER TABLE `hopsworks`.`model_version` DROP PRIMARY KEY;
 ALTER TABLE `hopsworks`.`model_version` ADD COLUMN id int(11) AUTO_INCREMENT PRIMARY KEY;
 
---FSTORE-1190
+-- FSTORE-1190
 ALTER TABLE `hopsworks`.`embedding_feature`
     ADD COLUMN `model_version_id` INT(11) NULL;
 
@@ -12,7 +12,7 @@ ALTER TABLE `hopsworks`.`embedding_feature`
 
 ALTER TABLE `hopsworks`.`serving` ADD COLUMN `api_protocol` TINYINT(1) NOT NULL DEFAULT '0';
 
---FSTORE-1096
+-- FSTORE-1096
 ALTER TABLE `hopsworks`.`feature_store_jdbc_connector`
     ADD COLUMN `secret_uid` INT DEFAULT NULL,
     ADD COLUMN `secret_name` VARCHAR(200) DEFAULT NULL;
