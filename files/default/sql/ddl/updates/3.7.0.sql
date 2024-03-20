dcc1b4cc462a7e47cce744c31fdb78ede1887716
Mahmoud Ismail <mahmoud@hopsworks.ai>
2024-03-20 11:50:01 +0100

    [HWORKS-1084] Handle migration statements in batches for large tables (#1125)
    * [HWORKS-1084] Handle migration statements in batches for large tables
    
    * fixes for review
    
    * fixes for review

--- files/default/sql/ddl/updates/3.7.0.sql
+++ files/default/sql/ddl/updates/3.7.0.sql
@@ -101,25 +101,199 @@ CREATE TABLE IF NOT EXISTS `hopsworks`.`feature_group_descriptive_statistics` ( 
     CONSTRAINT `fgds_fds_fk` FOREIGN KEY (`feature_descriptive_statistics_id`) REFERENCES `feature_descriptive_statistics` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = ndbcluster DEFAULT CHARSET = latin1 COLLATE = latin1_general_cs;
 
-SET SQL_SAFE_UPDATES = 0;
--- -- insert new feature_group_statistics. For the same feature group commit statistics, only insert the last computed statistics. For non-time-travel feature groups, insert all computed statistics.
-INSERT INTO `feature_group_statistics` (id, computation_time, feature_group_id, window_end_commit_time)
+-- Create helper procedures to do batch operations on feature store statistics
+DROP PROCEDURE IF EXISTS BatchProc;
+DROP PROCEDURE IF EXISTS PopulateFeatureGroupStatistics;
+DROP PROCEDURE IF EXISTS BatchPopulateFeatureGroupStatistics;
+DROP PROCEDURE IF EXISTS PopulateFeatureDescriptiveStatistics;
+DROP PROCEDURE IF EXISTS BatchPopulateFeatureDescriptiveStatistics;
+DROP PROCEDURE IF EXISTS PopulateTrainingDatasetStatistics;
+DROP PROCEDURE IF EXISTS BatchPopulateTrainingDatasetStatistics;
+DROP PROCEDURE IF EXISTS PopulateFeatureDescriptiveStatisticsForTrainingDataset;
+DROP PROCEDURE IF EXISTS BatchPopulateFeatureDescriptiveStatisticsForTrainingDataset;
+DROP PROCEDURE IF EXISTS UpdateFeatureStoreActivity;
+DROP PROCEDURE IF EXISTS BatchUpdateFeatureStoreActivity;
+DROP PROCEDURE IF EXISTS UpdateFeatureStoreActivityForTrainingDatset;
+DROP PROCEDURE IF EXISTS BatchUpdateFeatureStoreActivityForTrainingDatset;
+
+DELIMITER //
+
+-- Generic batch procedure to run batch jobs calls on the input procedure name 
+CREATE PROCEDURE BatchProc(proc_name VARCHAR(255), batch_size INT, start_id INT, end_id INT)
+BEGIN
+    DECLARE current_id INT;
+    DECLARE done BOOLEAN DEFAULT FALSE;
+
+    SET current_id = start_id;
+
+    WHILE NOT done DO
+        IF current_id + batch_size > end_id THEN
+            SET batch_size = end_id - current_id + 1;
+            SET done = TRUE;
+        END IF;
+
+        SELECT CONCAT('Processing batch from ', current_id, ' to ', current_id + batch_size - 1) AS log_message;
+
+        SET @sql_stmt := CONCAT('CALL ', proc_name, '(', current_id, ',', current_id + batch_size - 1, ')');
+
+        PREPARE stmt FROM @sql_stmt;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+
+        SET current_id = current_id + batch_size;
+    END WHILE;
+
+    SELECT 'All batches processed' AS log_message;
+
+END //
+
+-- Procedure to populate feature_group_statistics
+CREATE PROCEDURE PopulateFeatureGroupStatistics(start_id INT, end_id INT)
+BEGIN
+  -- -- insert new feature_group_statistics. For the same feature group commit statistics, only insert the last computed statistics. For non-time-travel feature groups, insert all computed statistics.
+  INSERT INTO `hopsworks`.`feature_group_statistics` (id, computation_time, feature_group_id, window_end_commit_time)
   WITH sorted_feature_store_statistic AS (
-    SELECT `feature_store_statistic`.*, ROW_NUMBER() OVER (PARTITION BY feature_group_id, IFNULL(feature_group_commit_id, UUID()) ORDER BY commit_time DESC) as rn -- group by fg_id and commit_id, and sort by commit_time --
-    FROM `feature_store_statistic` WHERE feature_group_id IS NOT NULL
+    SELECT `hopsworks`.`feature_store_statistic`.*, ROW_NUMBER() OVER (PARTITION BY feature_group_id, IFNULL(feature_group_commit_id, UUID()) ORDER BY commit_time DESC) as rn
+    FROM `hopsworks`.`feature_store_statistic` WHERE feature_group_id IS NOT NULL and feature_group_id BETWEEN start_id AND end_id
   )
   SELECT id, commit_time as computation_time, feature_group_id, IFNULL(feature_group_commit_id, UNIX_TIMESTAMP(commit_time)*1000) as window_end_commit_time
-  FROM sorted_feature_store_statistic WHERE rn = 1; -- last computed stats --
+  FROM sorted_feature_store_statistic WHERE rn = 1; -- select last computed stats in the current fg stats batch --
 
--- -- insert one feature descriptive statistic as a reference per feature_group_statistics. These will be used in the expat to parse and create the corresponding feature descriptive statistics rows
-INSERT INTO `feature_descriptive_statistics` (id, feature_name, feature_type, count, num_non_null_values, num_null_values, extended_statistics_path)
+END //
+
+CREATE PROCEDURE BatchPopulateFeatureGroupStatistics()
+BEGIN
+    DECLARE start_id INT;
+    DECLARE end_id INT;
+
+    SELECT MIN(feature_group_id), MAX(feature_group_id) INTO start_id, end_id FROM `hopsworks`.`feature_store_statistic`;
+
+    CALL BatchProc('PopulateFeatureGroupStatistics', 1000, start_id, end_id);
+    
+END //
+
+-- Procedure to populate feature_descriptive_statistics
+CREATE PROCEDURE PopulateFeatureDescriptiveStatistics(start_id INT, end_id INT)
+BEGIN
+  -- -- insert one feature descriptive statistic as a reference per feature_group_statistics. These will be used in the expat to parse and create the corresponding feature descriptive statistics rows
+  INSERT INTO `hopsworks`.`feature_descriptive_statistics` (id, feature_name, feature_type, count, num_non_null_values, num_null_values, extended_statistics_path)
   SELECT id, 'for-migration', 'FEATURE_GROUP' as feature_type, feature_group_id, UNIX_TIMESTAMP(commit_time)*1000, feature_group_commit_id, file_path
-  FROM `feature_store_statistic` WHERE id IN (SELECT id FROM `feature_group_statistics`);
--- -- insert one feature descriptive statistic per orphan feature group statistics file, to be deleted during the expat
-INSERT INTO `feature_descriptive_statistics` (id, feature_name, feature_type, count, extended_statistics_path)
+  FROM `hopsworks`.`feature_store_statistic` WHERE id BETWEEN start_id AND end_id AND id IN (SELECT id FROM `hopsworks`.`feature_group_statistics`);
+
+  -- -- insert one feature descriptive statistic per orphan feature group statistics file, to be deleted during the expat
+  INSERT INTO `hopsworks`.`feature_descriptive_statistics` (id, feature_name, feature_type, count, extended_statistics_path)
   SELECT id, 'to-be-deleted', 'FEATURE_GROUP' as feature_type, feature_group_id, file_path
-  FROM `feature_store_statistic` WHERE feature_group_id IS NOT NULL AND id NOT IN (SELECT id FROM `feature_group_statistics`);
--- To be done in the expat: insert feature_group_descriptive_statistics (many-to-many relationship), parse feature_descriptive_statistics, delete orphan fg statistics files
+  FROM `hopsworks`.`feature_store_statistic` WHERE feature_group_id IS NOT NULL AND id BETWEEN start_id AND end_id AND id NOT IN (SELECT id FROM `hopsworks`.`feature_group_statistics`);
+  -- To be done in the expat: insert feature_group_descriptive_statistics (many-to-many relationship), parse feature_descriptive_statistics, delete orphan fg statistics files
+END //
+
+CREATE PROCEDURE BatchPopulateFeatureDescriptiveStatistics()
+BEGIN
+    DECLARE start_id INT;
+    DECLARE end_id INT;
+
+    SELECT MIN(id), MAX(id) INTO start_id, end_id FROM `hopsworks`.`feature_store_statistic`;
+
+    CALL BatchProc('PopulateFeatureDescriptiveStatistics', 1000, start_id, end_id);
+    
+END //
+
+-- Procedure to populate training_dataset_statistics
+CREATE PROCEDURE PopulateTrainingDatasetStatistics(start_id INT, end_id INT)
+BEGIN
+  -- -- insert new training_dataset_statistics. For the same training dataset, only insert the last computed statistics.
+  INSERT INTO `hopsworks`.`training_dataset_statistics` (id, computation_time, training_dataset_id, before_transformation)
+  WITH sorted_feature_store_statistic AS (
+    SELECT `hopsworks`.`feature_store_statistic`.*, ROW_NUMBER() OVER (PARTITION BY training_dataset_id, for_transformation ORDER BY commit_time DESC) as rn -- group by td_id and before_transformation, and sort by commit_time --
+    FROM `hopsworks`.`feature_store_statistic` WHERE training_dataset_id IS NOT NULL AND training_dataset_id BETWEEN start_id AND end_id
+  )
+  SELECT id, commit_time as computation_time, training_dataset_id, for_transformation
+  FROM sorted_feature_store_statistic WHERE rn = 1; -- last computed stats --
+END //
+
+CREATE PROCEDURE BatchPopulateTrainingDatasetStatistics()
+BEGIN
+    DECLARE start_id INT;
+    DECLARE end_id INT;
+
+    SELECT MIN(training_dataset_id), MAX(training_dataset_id) INTO start_id, end_id FROM `hopsworks`.`feature_store_statistic`;
+
+    CALL BatchProc('PopulateTrainingDatasetStatistics', 1000, start_id, end_id);
+    
+END //
+
+-- Procedure to populate feature_descriptive_statistics
+CREATE PROCEDURE PopulateFeatureDescriptiveStatisticsForTrainingDataset(start_id INT, end_id INT)
+BEGIN
+    -- -- insert one feature descriptive statistic as a reference per training dataset statistics. These will be used in the expat to parse and create the corresponding feature descriptive statistics rows
+    INSERT INTO `hopsworks`.`feature_descriptive_statistics` (id, feature_name, feature_type, count, num_non_null_values, num_null_values, extended_statistics_path)
+    SELECT id, 'for-migration', 'TRAINING_DATASET' as feature_type, training_dataset_id, UNIX_TIMESTAMP(commit_time)*1000, feature_group_commit_id, file_path
+    FROM `hopsworks`.`feature_store_statistic` WHERE id BETWEEN start_id AND end_id AND id IN (SELECT id FROM training_dataset_statistics);
+    -- -- insert one feature descriptive statistic per orphan training dataset statistics file, to be deleted during the expat
+    INSERT INTO `hopsworks`.`feature_descriptive_statistics` (id, feature_name, feature_type, count, extended_statistics_path)
+    SELECT id, 'to-be-deleted', 'TRAINING_DATASET' as feature_type, training_dataset_id, file_path
+    FROM `hopsworks`.`feature_store_statistic` WHERE training_dataset_id IS NOT NULL AND id BETWEEN start_id AND end_id AND id NOT IN (SELECT id FROM `hopsworks`.`training_dataset_statistics`);
+    -- To be done in the expat: insert training_dataset_descriptive_statistics, test_dataset_descriptive_statistics, val_dataset_descriptive_statistics (many-to-many relationship), parse feature_descriptive_statistics
+    -- and delete orphan statistics files. Split statistics are handled in the expat migration as well
+END //
+
+CREATE PROCEDURE BatchPopulateFeatureDescriptiveStatisticsForTrainingDataset()
+BEGIN
+    DECLARE start_id INT;
+    DECLARE end_id INT;
+
+    SELECT MIN(id), MAX(id) INTO start_id, end_id FROM `hopsworks`.`feature_store_statistic`;
+
+    CALL BatchProc('PopulateFeatureDescriptiveStatisticsForTrainingDataset', 1000, start_id, end_id);
+    
+END //
+
+-- Procedure to update feature_store_activity
+CREATE PROCEDURE UpdateFeatureStoreActivity(start_id INT, end_id INT)
+BEGIN
+    UPDATE `hopsworks`.`feature_store_activity` SET feature_group_statistics_id = statistics_id
+    WHERE statistics_id IS NOT NULL AND feature_group_id IS NOT NULL AND feature_group_id BETWEEN start_id AND end_id AND EXISTS(SELECT 1 FROM `hopsworks`.`feature_group_statistics` WHERE id = statistics_id LIMIT 1);
+    
+    DELETE FROM `hopsworks`.`feature_store_activity` WHERE statistics_id IS NOT NULL AND feature_group_id IS NOT NULL AND feature_group_id BETWEEN start_id AND end_id AND feature_group_statistics_id IS NULL;
+END //
+
+CREATE PROCEDURE BatchUpdateFeatureStoreActivity()
+BEGIN
+    DECLARE start_id INT;
+    DECLARE end_id INT;
+
+    SELECT MIN(feature_group_id), MAX(feature_group_id) INTO start_id, end_id FROM `hopsworks`.`feature_store_activity`;
+
+    CALL BatchProc('UpdateFeatureStoreActivity', 1000, start_id, end_id);
+    
+END //
+
+-- Procedure to update feature_store_activity
+CREATE PROCEDURE UpdateFeatureStoreActivityForTrainingDatset(start_id INT, end_id INT)
+BEGIN
+    UPDATE `hopsworks`.`feature_store_activity` SET training_dataset_statistics_id = statistics_id
+        WHERE statistics_id IS NOT NULL AND training_dataset_id IS NOT NULL AND training_dataset_id BETWEEN start_id AND end_id AND EXISTS(SELECT 1 FROM `hopsworks`.`training_dataset_statistics` WHERE id = statistics_id LIMIT 1);
+    
+    DELETE FROM `hopsworks`.`feature_store_activity` WHERE statistics_id IS NOT NULL AND training_dataset_id IS NOT NULL AND training_dataset_id BETWEEN start_id AND end_id AND training_dataset_statistics_id IS NULL;
+END //
+
+CREATE PROCEDURE BatchUpdateFeatureStoreActivityForTrainingDatset()
+BEGIN
+    DECLARE start_id INT;
+    DECLARE end_id INT;
+
+    SELECT MIN(training_dataset_id), MAX(training_dataset_id) INTO start_id, end_id FROM `hopsworks`.`feature_store_activity`;
+
+    CALL BatchProc('UpdateFeatureStoreActivityForTrainingDatset', 1000, start_id, end_id);
+    
+END //
+
+DELIMITER ;
+
+
+SET SQL_SAFE_UPDATES = 0;
+CALL BatchPopulateFeatureGroupStatistics();
+CALL BatchPopulateFeatureDescriptiveStatistics();
 SET SQL_SAFE_UPDATES = 1;
 
 -- -- training dataset statistics
@@ -136,25 +310,8 @@ CREATE TABLE IF NOT EXISTS `hopsworks`.`training_dataset_statistics` (
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 SET SQL_SAFE_UPDATES = 0;
--- -- insert new training_dataset_statistics. For the same training dataset, only insert the last computed statistics.
-INSERT INTO `training_dataset_statistics` (id, computation_time, training_dataset_id, before_transformation)
-  WITH sorted_feature_store_statistic AS (
-    SELECT `feature_store_statistic`.*, ROW_NUMBER() OVER (PARTITION BY training_dataset_id, for_transformation ORDER BY commit_time DESC) as rn -- group by td_id and before_transformation, and sort by commit_time --
-    FROM `feature_store_statistic` WHERE training_dataset_id IS NOT NULL
-  )
-  SELECT id, commit_time as computation_time, training_dataset_id, for_transformation
-  FROM sorted_feature_store_statistic WHERE rn = 1; -- last computed stats --
-
--- -- insert one feature descriptive statistic as a reference per training dataset statistics. These will be used in the expat to parse and create the corresponding feature descriptive statistics rows
-INSERT INTO `feature_descriptive_statistics` (id, feature_name, feature_type, count, num_non_null_values, num_null_values, extended_statistics_path)
-  SELECT id, 'for-migration', 'TRAINING_DATASET' as feature_type, training_dataset_id, UNIX_TIMESTAMP(commit_time)*1000, feature_group_commit_id, file_path
-  FROM `feature_store_statistic` WHERE id IN (SELECT id FROM training_dataset_statistics);
-  -- -- insert one feature descriptive statistic per orphan training dataset statistics file, to be deleted during the expat
-INSERT INTO `feature_descriptive_statistics` (id, feature_name, feature_type, count, extended_statistics_path)
-  SELECT id, 'to-be-deleted', 'TRAINING_DATASET' as feature_type, training_dataset_id, file_path
-  FROM `feature_store_statistic` WHERE training_dataset_id IS NOT NULL AND id NOT IN (SELECT id FROM `training_dataset_statistics`);
--- To be done in the expat: insert training_dataset_descriptive_statistics, test_dataset_descriptive_statistics, val_dataset_descriptive_statistics (many-to-many relationship), parse feature_descriptive_statistics
--- and delete orphan statistics files. Split statistics are handled in the expat migration as well
+CALL BatchPopulateTrainingDatasetStatistics();
+CALL BatchPopulateFeatureDescriptiveStatisticsForTrainingDataset();
 SET SQL_SAFE_UPDATES = 1;   
 
 -- -- Update feature_store_activity foreign keys
@@ -169,12 +326,8 @@ ALTER TABLE `hopsworks`.`feature_store_activity`
 
 SET SQL_SAFE_UPDATES = 0;
 -- -- update feature_store_activity statistics ids
-UPDATE `feature_store_activity` SET feature_group_statistics_id = statistics_id
-    WHERE statistics_id IS NOT NULL AND feature_group_id IS NOT NULL AND EXISTS(SELECT 1 FROM `feature_group_statistics` WHERE id = statistics_id LIMIT 1);
-DELETE FROM `feature_store_activity` WHERE statistics_id IS NOT NULL AND feature_group_id IS NOT NULL AND feature_group_statistics_id IS NULL;
-UPDATE `feature_store_activity` SET training_dataset_statistics_id = statistics_id
-    WHERE statistics_id IS NOT NULL AND training_dataset_id IS NOT NULL AND EXISTS(SELECT 1 FROM `training_dataset_statistics` WHERE id = statistics_id LIMIT 1);
-DELETE FROM `feature_store_activity` WHERE statistics_id IS NOT NULL AND training_dataset_id IS NOT NULL AND training_dataset_statistics_id IS NULL;
+CALL BatchUpdateFeatureStoreActivity();
+CALL BatchUpdateFeatureStoreActivityForTrainingDatset();
 SET SQL_SAFE_UPDATES = 1;
 
 ALTER TABLE `hopsworks`.`feature_store_activity`
