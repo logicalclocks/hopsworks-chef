dc8ef6550d9d76ec70c161484b11b99a39ea53ae
bubriks <bubriks@gmail.com>
2024-06-14 15:58:30 +0200

    init

--- files/default/sql/ddl/updates/3.8.0.sql
+++ files/default/sql/ddl/updates/3.8.0.sql
@@ -45,20 +45,16 @@ ALTER TABLE `hopsworks`.`feature_store_jdbc_connector`
 CREATE TABLE IF NOT EXISTS `hopsworks`.`hopsworks_action` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `parent_action_id` INT NULL,
-  `project_id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-  `status` VARCHAR(20) NOT NULL,
-  `task` BLOB,
+  `status` VARCHAR(50) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `className` VARCHAR(255) NOT NULL,
+  `methodName` VARCHAR(255) NOT NULL,
+  `parameters` BLOB,
   `start_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `end_time` TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `hopsworks_action_parent_action_id_idx` (`parent_action_id`),
-  KEY `hopsworks_action_project_id_idx` (`project_id`),
-  KEY `hopsworks_action_user_fkc` (`user_id`),
-  CONSTRAINT `hopsworks_action_parent_action_fkc` FOREIGN KEY (`parent_action_id`) REFERENCES `hopsworks`.`hopsworks_action` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `hopsworks_action_project_fkc` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `hopsworks_action_user_fkc` FOREIGN KEY (`user_id`) REFERENCES `hopsworks`.`users` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `hopsworks_action_parent_action_fkc` FOREIGN KEY (`parent_action_id`) REFERENCES `hopsworks`.`hopsworks_action` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=ndbcluster DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `hopsworks`.`hopsworks_action_attempt` (
